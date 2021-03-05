af0572f3eb037b179be03661add43f15794d4fc0
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-06 01:19:06 +0530

    Create A1. Print In Java.java

---
+++ Level 1 (Beginner)/Getting Started/A1. Print In Java.java
@@ -0,0 +1,33 @@
+package GettingStarted;
+
+public class printInJava {
+
+	public static void main(String[] args) {
+
+		//we have to print the triangle
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		
+		
+		//we can also print the same trianle using the single print statment, by usnig the back slash which also means newline
+		
+		System.out.println("*\n**\n***\n****\n*****");
+	}
+
+}
+
+Output:
+*
+**
+***
+****
+*****
+
+*
+**
+***
+****
+*****

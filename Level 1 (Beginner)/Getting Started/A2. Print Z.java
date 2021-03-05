97fafdebbd7fc11decba81d1710d2b050c6a9467
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-06 01:32:53 +0530

    Create A2. Print Z.java

---
+++ Level 1 (Beginner)/Getting Started/A2. Print Z.java
@@ -0,0 +1,15 @@
+package GettingStarted;
+
+public class printInJava {
+
+	public static void main(String[] args) {
+    
+    //To print Z using System.out.println();
+		System.out.println("*****"); //pehli line mai 0 space and 5 stars
+		System.out.println("   * "); //dusri line mai 3 spaces 
+		System.out.println("  *  ");
+		System.out.println(" *   "); 
+		System.out.println("*****"); //last line mai 5 spaces
+	}
+
+}

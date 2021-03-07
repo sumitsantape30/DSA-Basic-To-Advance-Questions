fe1727da2722eb8d08cd8e46c15265b8414ff135
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-08 01:22:58 +0530

    Create A7. Is A Number Prime.java

---
+++ Level 1 (Beginner)/Basics of Programming/Getting Started/A7. Is A Number Prime.java
@@ -0,0 +1,18 @@
+1. You've to check whether a given number is prime or not.
+2. Take a number "t" as input representing count of input numbers to be tested.
+3. Take a number "n" as input "t" number of times.
+4. For each input value of n, print "prime" if the number is prime and "not prime" otherwise.
+  
+Input Format
+A number t
+A number n
+A number n
+.. t number of times
+
+Output Format
+prime
+not prime
+not prime
+.. t number of times
+
+Code:

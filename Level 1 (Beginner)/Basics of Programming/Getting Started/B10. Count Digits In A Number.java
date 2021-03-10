c618b6651e9492481c2f21ed6cd0809051a350d2
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-10 12:45:30 +0530

    Create B10. Count Digits In A Number.java

---
+++ Level 1 (Beginner)/Basics of Programming/Getting Started/B10. Count Digits In A Number.java
@@ -0,0 +1,44 @@
+Count Digits In A Number
+
+1. You've to count the number of digits in a number.
+2. Take as input "n", the number for which the digits has to be counted.
+3. Print the digits in that number.
+  
+Input Format
+"n" where n is any integer.
+Output Format
+"d" where d is the number of digits in the number "n"
+  
+Constraints
+1 <= n < 10^9
+Sample Input
+65784383
+Sample Output
+8
+
+Code:
+package GettingStarted;
+
+import java.util.Scanner;
+
+public class CountDigitInNum {
+	
+	public static int countDigit( int n) {
+		
+		int count=0;
+		while( n >0) {
+			count++;
+			n= n/10;
+		}
+		return count;
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		int n = s.nextInt();
+		int count= countDigit( n);
+		System.out.println(count);
+	}
+
+}

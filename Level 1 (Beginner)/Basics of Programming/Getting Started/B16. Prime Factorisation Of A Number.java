436fd9fc4770f4f1b289d2839b9116ed8b6ba96d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-11 17:30:56 +0530

    Create B16. Prime Factorisation Of A Number.java

---
+++ Level 1 (Beginner)/Basics of Programming/Getting Started/B16. Prime Factorisation Of A Number.java
@@ -0,0 +1,57 @@
+Prime Factorisation Of A Number
+
+    1. You are required to display the prime factorization of a number.
+    2. Take as input a number n.
+    3. Print all its prime factors from smallest to largest.
+                               
+Input Format
+n, an integer
+Output Format
+p1  p2  p3  p4.. all prime factors of n
+
+Constraints
+2 <= n < 10 ^ 9
+
+Sample Input
+1440
+Sample Output
+2 2 2 2 2 3 3 5
+
+Code:
+
+package GettingStarted;
+
+import java.util.Scanner;
+
+public class primeFactorizationOfANumber {
+	
+	public static void printPrimeFactorization(int n) {
+		
+		//mai pehla divisor 2 choose karra aur jabtak div root n ke barabar na hojaye tabtak kam karenge
+		for( int div= 2; div*div <= n; div++) {
+			
+			//jabtak yeh n divide hota rehta hai tabtak divide krte rho means katate rho aur har ek bar apne divisor ko print krte rho
+			while( n % div == 0) {
+				n= n/ 10;
+				System.out.print(div+" ");
+			}
+		}
+		
+		//isme ek case chhut jata hai kabhi kabhi jaise 46 ki bta kare, 46 is 2*23, 2 se 46 ko divide kiye 23 bach gya ab 23 tak ki table chalani jarurat nhi hai hum quit kardenge lekin jab bahar ayenge n cut karke 1 nhi bana hai
+		//so agar apke n 1 nhi bana hai means n 1 se bda bach gya hai to yeh only number hai jo root n ke bad ata hai jo n ko divide kr skta hai *open book and see example of number 46*
+	   if( n != 1) {
+		   //agar kat kar 1 nhi kar paye to jo number bacha hua tha jaise 23 bacha tha woh khudme ek prime factor hai to usko print kardenge
+		   System.out.println(n);
+	   }
+	}
+	
+	//point to remeber: root n se aage loop na chalaye sirf root n tak divide kare, root n ke beyond agar root katke 1 nhi hua to jo number bacha hai woh last prime factor bacha hai jo root n ke beyond hai usko print kardenge.
+
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		printPrimeFactorization(n);
+	}
+
+}

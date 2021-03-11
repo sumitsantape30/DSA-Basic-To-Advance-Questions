8d57dcd99aca457ef48672724bc907cff3799f6f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-11 16:02:17 +0530

    Update B15. Gcd And Lcm.java

--- Level 1 (Beginner)/Basics of Programming/Getting Started/B15. Gcd And Lcm.java
+++ Level 1 (Beginner)/Basics of Programming/Getting Started/B15. Gcd And Lcm.java
@@ -25,3 +25,44 @@ Sample Output
 72
 
 Code:
+
+package GettingStarted;
+
+import java.util.Scanner;
+
+public class GCDAndLCM {
+	
+	public static void gcdandlcm(int n1, int n2) {
+		// you'll think ki isme se pehle chota number dhundlu coz hum divide karna chote number se start karenge but usse faraq nhi pdta
+		
+		//LCM keliye original values preserve kr rhe hai
+		int on1= n1;
+		int on2= n2;
+		
+		while( n1 % n2 != 0) { //jabtak remainder 0 nhi ata tabtak hum un dono number pe kam krte rahenge
+			
+			//yad karo: ek divisor tha ek dividend tha, divisor ne divide kiya to remainder aya agli division keliye yeh remainder divisor ban jayega aur divisor dividend ban jayega *book dekho*
+			int rem= n1 % n2;
+			n1= n2;
+			n2= rem; 
+		}
+		
+		//so jis number ne remainder 0 laya woh GCD hota hai means n2 nehi khud divide krke remainder 0 laya tabhi hum loop ke bahar aaye
+		int gcd= n2;	
+		//lcm keliye property hai, gcd*lcm= n1*n2
+		int lcm= (on1*on2)/gcd; //but yaha n1 and n2 woh number rhe hi nhi jo initially the kyuki woh GCD calculate krte krte chote hogye hai isliye do variables rakhenge jisme original value preserve krke rakhle
+		
+		System.out.println(gcd);
+		System.out.println(lcm);
+		
+	}
+
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int n1 = s.nextInt();
+		int n2= s.nextInt();
+		gcdandlcm( n1, n2);
+	}
+
+}

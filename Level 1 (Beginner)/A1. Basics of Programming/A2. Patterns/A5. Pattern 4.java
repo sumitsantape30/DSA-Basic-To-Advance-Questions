a4a09cc24a137f36c47020e7f80c43dfb395906f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-12 19:28:09 +0530

    Create A5. Pattern 4.java

---
+++ Level 1 (Beginner)/A1. Basics of Programming/A2. Patterns/A5. Pattern 4.java
@@ -0,0 +1,51 @@
+Constraints
+1 <= n <= 100
+ Also, n is odd.
+  
+Sample Input
+5
+Sample Output
+		*	
+	*	*	*	
+*	*	*	*	*	
+	*	*	*	
+		*	
+  
+Code:
+
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt(); //hume input humesha odd milega
+		
+		int sp= n/2; //starting mai spaces n/2 hai, 5 line keliye 2 space ek star
+		int st= 1;
+		
+		
+		for( int i=0; i<= n; i++) {
+			
+			for( int j=0; j< sp; j++) {
+				System.out.print("\t");
+			}
+			
+			for( int j=0; j< st; j++) {
+				System.out.println("*\t");
+			}
+			
+			if( i <= n/2) {
+				sp--;
+				st += 2; // pehle row mai 1 star that dusre row mai 2 star hai isliye +2 kiye
+			}else {
+				sp++;
+				st -= 2; //  n/2 ke bad wale mai starts kam ho rhe by 2
+			}
+		}
+	}
+
+}

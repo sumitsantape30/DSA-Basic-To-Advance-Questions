bb7813eb8c6bb3910abc38f47103ef2220e80040
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-12 13:03:43 +0530

    Update A1. Pattern 1.java

--- Level 1 (Beginner)/A1. Basics of Programming/A2. Patterns/A1. Pattern 1.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A2. Patterns/A1. Pattern 1.java
@@ -11,3 +11,23 @@ Sample Output
 
 Code:
 
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		
+		for( int i=1; i<= n; i++) {
+			for( int j= 1; j<= i; j++) { //yeh jitni i ki value hai utne starts print karega
+				System.out.print("*\t"); // \t se tab lag jayega taaki stars thode dur dur aaye humare
+			}
+			System.out.println();
+		}
+	}
+
+}

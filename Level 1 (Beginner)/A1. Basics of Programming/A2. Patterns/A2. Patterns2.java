c8bd4ae74e18951ac81d2c4512d604be5668faec
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-12 13:27:16 +0530

    Create A2. Patterns2.java

---
+++ Level 1 (Beginner)/A1. Basics of Programming/A2. Patterns/A2. Patterns2.java
@@ -0,0 +1,32 @@
+Constraints
+1 <= n <= 100
+Sample Input
+5
+Sample Output
+*	*	*	*	*	
+*	*	*	*	
+*	*	*	
+*	*	
+*
+
+Code:
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+
+		for (int i =n ; i >= 1; i--) { //maine i n se start kara hai so andar wala loop pehi iteration mai n stars print karega
+			for (int j = 1; j <= i; j++) { //andar wala loop utne stars print karta hai jitni i ki value hai
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+

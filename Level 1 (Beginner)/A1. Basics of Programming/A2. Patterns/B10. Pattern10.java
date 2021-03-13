a48a13c62a79c03a5895bf7e316aa64eea4c2d9a
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-13 13:11:04 +0530

    Create B10. Pattern10.java

---
+++ Level 1 (Beginner)/A1. Basics of Programming/A2. Patterns/B10. Pattern10.java
@@ -0,0 +1,67 @@
+Constraints
+1 <= n <= 100
+ Also, n is odd.
+  
+Sample Input
+5
+Sample Output
+		*	
+	*		*	
+*				*	
+	*		*	
+		*	
+
+  Code:
+
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern10 {
+
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		
+		int os= n/2; //jab n=7 wala pattern dekhe the usme outer space 7 keliye 3 the so n/2;
+		int is= -1; //inner space -1 lenge initially
+		
+		for( int i=1; i<= n; i++) {
+			//System.out.print(os+" "+is);
+			
+			//outer space print karenge
+			for( int j=1; j<= os; j++) {
+				System.out.print("\t");
+			}
+			
+			// outer space ke bad ab ek star ayega
+			System.out.print("*\t");
+			
+			//ab inner spaces ayenge
+			for( int j=1; j<= is; j++) {
+				System.out.print("\t");
+			}
+			
+			// inner space ke bad firse jo star tha woh har row mai nhi tha, yeh pehli and last row mai nhi hai baki bichke rows mai do stars hai
+			if( i >1 && i < n) { //jabtak i 1 se bda hai and i n se chota hai means woh first and last row nhi hai tabhi iss star ko print karenge
+				System.out.print("*\t");
+			}
+			
+			
+			//iske bad addhe tak kuch means n/2 tak kuch aur ho rha hai aur uske bad kuch ho rha hai
+			if(i <= n/2) { //addhe tak outer spaces badh rhe hai aur inner space 2 2 se badh rhe hai
+				os--;
+				is += 2;
+			}else { //aadhe ke bad outer space kam hojate hai and inner space 2 se ghatate hai
+				os ++;
+				is -= 2;
+			}
+			
+			System.out.println();
+			
+		}
+		
+	}
+
+}

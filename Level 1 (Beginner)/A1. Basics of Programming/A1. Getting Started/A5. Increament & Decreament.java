791fd652aeeff003fabd285c8cb33791676d1486
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-15 21:40:08 +0530

    Update A5. Increament & Decreament.java

--- Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/A5. Increament & Decreament.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/A5. Increament & Decreament.java
@@ -5,22 +5,25 @@ public class incAnddec {
 	public static void main(String[] args) {
 
 		int i = 10;
-	    if(i++ == i) { // yahape i 10 tha then i increament hogya to 11 aur 11 compare hua 10 ke sath so condition is false
-	 	 System.out.println(i + " is good");
+		//System.out.println(i++ +" "+i); o/p: 10 11
+		
+	    if(i++ == i) { // yahape i 10 tha then i increament hogya to 11 but i++ wale i mai 10 hi rahega aur == ke bad wale i mai 11 rahega, so 10 compare ho rha hai with 11 which is false
+		 System.out.println(i + " is good");
 	    }
 	    else {
 	 	 System.out.println(i + " is bad");
 	    }
 	 
 	    int j = 20;
-	    if(++j == j) //yahape pehlehi j increament hogya to 21 and then 21 compare hua 21 ke sath which is false
+	    if(++j == j) //yahape pehlehi j increament hogya to 21 and then 21 compare hua 21 ke sath coz == ke bad ke i ki value bhi 21hogyi hai so condition is false
 	 	 System.out.println(j + " is good");
 	    else
 	 	 System.out.println(j + " is bad");
 	}
-
 }
 
-Code:
+Output:
+
 11 is bad
 21 is good
+

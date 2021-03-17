f5272ccffa8905638293de03368c92631e94f2f1
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-17 21:57:58 +0530

    Create A3. Any Base To Decimal.java

---
+++ Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/A3. Any Base To Decimal.java
@@ -0,0 +1,56 @@
+Any Base To Decimal
+
+1. You are given a number n.
+2. You are given a base b. n is a number on base b.
+3. You are required to convert the number n into its corresponding value in decimal number system.
+  
+Input Format
+A number n
+A base b
+Output Format
+A decimal number representing corresponding value of n in base b.
+  
+Constraints
+0 <= d <= 1000000000
+2 <= b <= 10
+  
+Sample Input
+111001
+2
+Sample Output
+57
+  
+Code:
+package GettingStarted;
+
+import java.util.Scanner;
+
+public class AnyBaseToDecimal {
+	
+	public static int getValueIndecimal(int n, int b) {
+		int rv= 0;
+		int p=1;
+		
+		while( n >0 ) { //hume tabtak kam karna hai jabtak humara number 0 se bda hai
+			
+			int dig= n % 10; //aur hum 10 se divide krke digit nikalenge
+			//aur 10 se divide krke chota karenge
+			n= n /10;
+						
+			//and jo power hongi woh humare base ki hongi
+			rv +=  dig* p;
+			//humare base ki power hai to woh 20 se multiply hokar badhegi
+			p= p * b;
+			
+		}
+		return rv;
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int b = scn.nextInt();
+		int d = getValueIndecimal(n, b);
+		System.out.println(d);
+	}
+}

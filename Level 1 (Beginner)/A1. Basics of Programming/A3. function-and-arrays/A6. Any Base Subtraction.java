e5840b4b036c3366f79440740c3e66034c4ffb8c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-18 13:45:37 +0530

    Update A6. Any Base Subtraction.java

--- Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/A6. Any Base Subtraction.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/A6. Any Base Subtraction.java
@@ -22,3 +22,67 @@ Sample Output
 77
 
 Code:
+
+package FunctionsAndArrays;
+
+import java.util.Scanner;
+
+public class AnyBaseSubstraction {
+	
+	public static int getDifference(int b, int n1, int n2) {
+
+		int rv= 0;
+		int c= 0;
+		int p=1;
+		
+		//humara kam tabtak chalega jatbak bada number bacha hua hai
+		while( n2 > 0) {
+			
+			int d1= n1 % 10;
+			// n1 ko chota kardenge
+			n1= n1/10;
+			int d2= n2 % 10;
+			n2= n2/10;
+			
+			//ab hum digit d banana chahte hai so d ko 0 assign karenge
+			int d= 0;
+			//aur d2 digit ke andar carry add karlenge
+			d2= d2 + c;
+			
+			//ab hum dekhenge kya d2 mai dum hai d1 ko manage karneka
+			if( d2 >= d1){ // agar iss waqt d2 grater or equal hai d1 ke to hume carry ki jarurat nhi hai so carry hoga 0
+				c= 0;
+				// d digit hojayegi d2-d1
+				d= d2- d1;
+			}else {
+			   // agar d2 chota pd gya to carry hojayega 1 actually yeh carry borrow hai to muje -1 ka carry dena padega kyuki hum carry ko add kr rhe hai
+			   c = -1;
+			   d = d2 + b - d1;
+			}
+			
+			//ab rv ke andar add karenge d* power, power ka kam hai unko dhang se allign karna 
+			rv= rv + d*p;
+			p= p* 10;
+		}
+		
+		return rv;
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int b = scn.nextInt();
+		int n1 = scn.nextInt();
+		int n2 = scn.nextInt();
+
+		int d = getDifference(b, n1, n2);
+		System.out.println(d);
+	}
+
+}
+
+Input:
+8
+256
+1212
+Output:
+734

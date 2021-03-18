74614014a93fc51158d0d61b941c82f4544feb9f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-18 12:32:43 +0530

    Create A6. Any Base Subtraction.java

---
+++ Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/A6. Any Base Subtraction.java
@@ -0,0 +1,24 @@
+Any Base Subtraction
+
+1. You are given a base b.
+2. You are given two numbers n1 and n2 of base b.
+3. You are required to subtract n1 from n2 and print the value.
+Input Format
+A base b
+A number n1
+A number n2
+Output Format
+A number of base b equal in value to n2 - n1.
+  
+Constraints
+2 <= b <= 10
+0 <= n1 <= 256
+n1 <= n2 <= 256
+Sample Input
+8
+1
+100
+Sample Output
+77
+
+Code:

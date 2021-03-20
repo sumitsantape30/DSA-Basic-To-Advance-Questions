167fa0fd88ff5741a0f6d65e03770867d62ddda6
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-20 13:55:13 +0530

    Update B17. Subsets Of Array.java

--- Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/B17. Subsets Of Array.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/B17. Subsets Of Array.java
@@ -32,3 +32,61 @@ Sample Output
 10	20	-	
   
 Code:
+
+package FunctionsAndArrays;
+
+import java.util.Scanner;
+
+public class subsetOfArray {
+	
+	public static int[] takeInput() {
+		
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int arr[]= new int[n];
+		
+		for( int i=0; i< arr.length; i++) {
+			arr[i]= s.nextInt();
+		}
+		
+		return arr;
+	}
+	
+	public static void subset( int arr[]) {
+		
+		int limit= (int)Math.pow(2, arr.length); // kitne subsets honge means kahatk loop chalega: 2^n
+	
+		for( int i=0; i< limit; i++) { // 8 subsets honga to uske liye 0 se 7 tak loop chalta hai
+			String set= "";
+			
+			//i ko temp mai le lenge coz niche i=i/2 karunga to pura i hi change hojayega aur loop mai gadbad hojayegi
+			int temp= i;
+			
+			//convert i to binary and use 0's and 1's to know if to print the element or not
+			// so hume remainders binary nikalne mai help krte hai, muje mere i ko 2 se divide karna hoga bar bar
+			//hum binary mai convert karne ke bad niche se means ulte numbers print krte hai so yeh loop ulta chalao
+			for( int j= arr.length -1 ; j>= 0; j--) { //jitne array mai elements hai utin bar hum divide karenge
+				int r=  temp %2 ;// remainder
+				temp= temp /2; //temp ko chota kardenge
+				
+				//agar remainder 0 hai to muje element ko print nhi karna warna muje element ko print karna hai
+				if( r == 0) {
+					set= "\t"+ set; //agar yeh 0 aya hai to iss number ke nam ka dash hum apn set mai add karlenge
+				}else {
+					//aur agar yeh 1 aya hai to hum arr[j] ko apne set mai add karle
+					set= arr[j]+ "\t" + set;
+				}
+			}
+			
+			System.out.print(set);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int arr[]= takeInput();
+		subset(arr);
+	}
+
+}
+

6c9f7297d981bd92dd8342b7007e2d85ec1acaab
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-23 15:54:55 +0530

    Update A5. Exit Point Of A Matrix.java

--- Level 1 (Beginner)/A1. Basics of Programming/A4. 2D Arrays/A5. Exit Point Of A Matrix.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A4. 2D Arrays/A5. Exit Point Of A Matrix.java
@@ -46,3 +46,81 @@ Sample Output
 3
 
 Code:
+
+package TwoDArrays;
+
+import java.util.Scanner;
+
+public class ExitPoint {
+	
+	public static int[][] takeInput(){
+		
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int m= s.nextInt();
+		
+		int arr[][]= new int[n][m];
+		
+		for( int i=0; i< arr.length; i++) {
+			for( int j=0; j< arr[0].length; j++) {
+				arr[i][j]= s.nextInt();
+			}
+		}
+		return arr;
+	}
+	
+	public static void exitPoint( int arr[][]) {
+		
+		
+		//1. dir nam ka variable initialise karenge from 0
+		int dir=0; // dir mai 0 stands for e, 1=s, 2=w, 3=n
+		// 1 ko south kyu banaye? kyuki east se 90 degree right turn se south ata hai so east se agli direction south hai and 0 se agli value 1 hoti hai and east se agli direction south hoti hai so on
+		
+		//and woh banda i=0 se and j=0 se entry kr rha hai
+		int i=0;
+		int j=0;
+		
+		while( true) {
+			//3. yeh hume help karega, agar hum 0 pe aaye to humari direction change nhi hogi
+			dir= (dir+ arr[i][j]) % 4; //%4 hume help karega, agar dir 4 hua to modulus se woh 0 hojayega means first east ke taraf
+			
+			//2. 
+			if( dir == 0) { //agar direction 0 hai means banda east mai ja rha hai, east mai ja rha hai to i ko nhi j ko faraq pdta hai
+				j++;
+			}else if( dir == 1) { //agar direction 1 hai means south to i++
+			    i++;	
+			}else if( dir == 2) { //agar west ja rha hai to j--
+				j--;
+			}else if( dir == 3) { //agar dir 3 hai means north ko ja rha hai to i--;
+				i--;
+			}
+			
+			//hume exist point chahiye to yeh hume pta chalega bahar aane ke bad, so agar hum matrix ke kahi bahar aagye to break kar denge, yeh hume pta lagega ek step bahar aane ke bad
+			//hum dekhenge kya hum bahar aagye kya
+			if( i < 0) { //agar i negative hojaye
+				i++; //i negative hogya to i++ krke break kare
+				break;
+			}else if(j < 0) { //ya j negative hojaye
+				i++;
+				break; 
+			}else if(i == arr.length) { //ya humara i row ke length jitna hojaye, last possible value arr.length-1 hoti hai
+				i--; //i arr.length hogya to i-- karke break kare, mean i bahar nikal gya hai to i-- karke break kare
+				break;
+			}else if( j == arr[0].length ) { //ya humara j column ke length jitne hojaye
+				j--; // j bahar nikal gya hai to j-- karke break kare
+				break;
+			}//let's jab hum bahar nikal jayege to i and j ki value hogi 2,-1 but exit point hum 2,-1 nhi print karna chahte hum print karna chahte hai jo aakhri spot humne visit kiya tha to hum 2,0 hi print karenge
+		     // so jab hum exist ho chuke hai to ek step wapas leke jayiye aur fir btaye ki yahase exit hua tha 
+		}
+		//i and j ko print kardenge
+		System.out.println(i);
+		System.out.println(j);
+	}
+
+	public static void main(String[] args) {
+
+		int arr[][]= takeInput();
+		exitPoint(arr);
+	}
+
+}

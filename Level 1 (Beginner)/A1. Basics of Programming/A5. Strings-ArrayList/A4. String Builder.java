8a96dee557bb30fafb445cecbfc4b57dba9cda01
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-25 22:32:55 +0530

    Create A4. String Builder.java

---
+++ Level 1 (Beginner)/A1. Basics of Programming/A5. Strings-ArrayList/A4. String Builder.java
@@ -0,0 +1,35 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        StringBuilder sb= new StringBuilder("Hello");
+        System.out.println(sb);
+        
+        char ch= sb.charAt(0); //get
+        System.out.println(ch);
+        
+        sb.setCharAt(0,'d'); //update/replace
+        System.out.println(sb);
+        //So string builder mai hum string mai set bhi kr skte hai 
+        
+        //kahibichme character insert karne keliye setChar se uss index pe koi character replace kr skte hai
+        sb.insert(2, 'y'); // insert
+        System.out.println(sb);
+        
+        //ab koi index se koi character delete karne keliye
+        sb.deleteCharAt(2); //delete
+        System.out.println(sb);
+        
+        sb.append('g');// last mai add karne keliye
+        
+        System.out.println(sb.length());
+    }
+}
+
+Output:
+java -cp /tmp/TwPXJxGR5f HelloWorld
+Hello
+H
+dello
+deyllo
+dello
+6

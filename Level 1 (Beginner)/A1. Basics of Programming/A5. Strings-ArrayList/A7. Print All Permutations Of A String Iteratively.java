b078296a58b6fcebe39220f7917974b3832359cd
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-26 14:58:38 +0530

    Update A7. Print All Permutations Of A String Iteratively.java

--- Level 1 (Beginner)/A1. Basics of Programming/A5. Strings-ArrayList/A7. Print All Permutations Of A String Iteratively.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A5. Strings-ArrayList/A7. Print All Permutations Of A String Iteratively.java
@@ -20,3 +20,63 @@ bca
 cba
 
 Code:
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class HelloWorld {
+    
+    public static String takeInput() {
+		Scanner s= new Scanner(System.in);
+		String str= s.next();
+		return str;
+	}
+	
+	public static void solution( String str) {
+		
+		int n= str.length();
+		//sabse pehle muje factorial, jitna factorial hai utne permutations honga
+		int f= fact(n);
+		
+		for( int i=0; i< f; i++) { // 0 se loop chalu hoga and f se ek chote tak chalega
+			
+			//permutations keliye hum orignal string se shuruwat krte hai so string builder mai original string ko pakal lijiye kyuki hume remove karna padega isliye string builder use kare
+			StringBuilder sb= new StringBuilder(str);
+		   //i hume divide karne mai use hoga so usko temp mai lelo taki bahar wale loop ko takleef na ho
+		   int temp= i;
+		   
+			//ab humara divisor
+			// humari diving harbar string ke length se start hogi
+			for( int div= n; div >= 1; div--) {
+				int q= temp / div; //quotient 
+				int r= temp % div; //remainder 
+				
+				//ab remainder ke hisab se print hoga ki kya print hoga as jo remainder aya usi character ko print krte hai
+				
+				System.out.print(sb.charAt(r));
+				//ab usko delete bhi kar denge coz woh ab original string mai rehna nhi chahiye
+				sb.deleteCharAt(r);
+				
+				//so jo issbar ka quotient hota hai woh agli bar ka number hota hai so agli bar woh temp ban jayega jo issbar quotient aya tha
+				temp= q;
+			}
+			System.out.println();
+		}
+	}
+	
+	public static int fact(int n) {
+		int fact=1;
+		
+		for( int i=2; i<= n; i++) {
+			fact= fact*i;
+		}
+		return fact;
+	}
+
+	public static void main(String[] args) {
+     
+       String str= takeInput();
+       solution(str);
+       
+	}
+}

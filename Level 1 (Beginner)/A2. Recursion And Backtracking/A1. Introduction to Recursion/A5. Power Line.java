6860034d6210eadaac306795dd34b9b3427e2e2b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-27 19:05:34 +0530

    Create A5. Power Line.java

---
+++ Level 1 (Beginner)/A2. Recursion And Backtracking/A1. Introduction to Recursion/A5. Power Line.java
@@ -0,0 +1,20 @@
+Power-linear
+1. You are given a number x.
+2. You are given another number n.
+3. You are required to calculate x raised to the power n. Don't change the signature of power function .
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+Input Format
+A number x
+A number n
+Output Format
+x raised to the power n
+
+Constraints
+1 <= x <= 10
+0 <= n <= 9
+Sample Input
+2
+5
+Sample Output
+32

3b794043d57185f7cf3c4534243d7a5ef3d3707f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-03-27 15:26:53 +0530

    Update A1. Print Decreasing.java

--- Level 1 (Beginner)/A2. Recursion And Backtracking/A1. Introduction to Recursion/A1. Print Decreasing.java
+++ Level 1 (Beginner)/A2. Recursion And Backtracking/A1. Introduction to Recursion/A1. Print Decreasing.java
@@ -1,29 +1,30 @@
-Print Increasing
-
+Print Decreasing
+Easy  Prev Next  
 1. You are given a positive number n. 
-2. You are required to print the counting from 1 to n.
-3. You are required to not use any loops. Complete the body of print Increasing function to achieve it. Don't change the signature of the function.
+2. You are required to print the counting from n to 1.
+3. You are required to not use any loops. Complete the body of print Decreasing function to achieve it.
 
 Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
 Input Format
 A number n
 Output Format
-1
-2
-3
-..
 n
+n - 1
+n - 2
+.. 
+1
+
 
 Constraints
 1 <= n <= 1000
 Sample Input
 5
 Sample Output
-1
-2
-3
-4
 5
+4
+3
+2
+1
 
 Code:
 package Recursion;

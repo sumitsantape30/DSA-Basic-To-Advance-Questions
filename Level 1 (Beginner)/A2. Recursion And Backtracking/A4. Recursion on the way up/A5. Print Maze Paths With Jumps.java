73a89d7258beef00050f30db2370d6485e86988d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-04-03 00:55:35 +0530

    Create A5. Print Maze Paths With Jumps.java

---
+++ Level 1 (Beginner)/A2. Recursion And Backtracking/A4. Recursion on the way up/A5. Print Maze Paths With Jumps.java
@@ -0,0 +1,31 @@
+Print Maze Paths With Jumps
+1. You are given a number n and a number m representing number of rows and columns in a maze.
+2. You are standing in the top-left corner and have to reach the bottom-right corner. 
+3. In a single move you are allowed to jump 1 or more steps horizontally (as h1, h2, .. ), or 1 or more steps vertically (as v1, v2, ..) or 1 or more steps diagonally (as d1, d2, ..). 
+4. Complete the body of printMazePath function - without changing signature - to print the list of all paths that can be used to move from top-left to bottom-right.
+Use sample input and output to take idea about output.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+Input Format
+A number n
+A number m
+Output Format
+Print paths (one path in each line) in order hinted by Sample output
+
+Constraints
+0 <= n <= 5
+0 <= m <= 5
+Sample Input
+3
+3
+Sample Output
+h1h1v1v1
+h1h1v2
+h1v1h1v1
+h1v1v1h1
+h1v1d1
+h1v2h1
+h1d1v1
+
+Code:
+

4078212b7f4dcc8fff680e7514761962b5cb547a
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-04-02 09:32:41 +0530

    Create A1. Print Subsequence.java

---
+++ Level 1 (Beginner)/A2. Recursion And Backtracking/A4. Recursion on the way up/A1. Print Subsequence.java
@@ -0,0 +1,27 @@
+Print Subsequence
+
+1. You are given a string str.
+2. Complete the body of printSS function - without changing signature - to calculate and print all subsequences of str.
+Use sample input and output to take idea about subsequences.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+Input Format
+A string str
+Output Format
+Subsequences of str in order hinted by Sample output
+
+Constraints
+0 <= str.length <= 7
+Sample Input
+yvTA
+
+Sample Output
+yvTA
+yvT
+yvA
+yv
+yTA
+yT
+yA
+
+Code:

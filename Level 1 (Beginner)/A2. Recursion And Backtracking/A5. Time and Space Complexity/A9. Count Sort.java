aad9279f01aa50e6426a1e24944f143138c9f783
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-04-05 22:59:18 +0530

    Create A9. Count Sort.java

---
+++ Level 1 (Beginner)/A2. Recursion And Backtracking/A5. Time and Space Complexity/A9. Count Sort.java
@@ -0,0 +1,30 @@
+Count Sort
+
+1. You are given an array(arr) of integers.
+2. You have to sort the given array in increasing order using count sort.
+Input Format
+An Integer n 
+arr1
+arr2..
+n integers
+Output Format
+Check the sample ouput and question video.
+
+Constraints
+1 <= N <= 10000
+-10^8 <= arr[i] <= 10^8
+Sample Input
+5
+7 
+-2 
+4 
+1 
+3
+Sample Output
+-2
+1
+3
+4
+7
+  
+Code:

2edce01373feb0729f75ba751ed8821b29832ba1
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-04-08 22:36:57 +0530

    Create B11. Sot Dates.java

---
+++ Level 1 (Beginner)/A3. Dynamic Programming/A1. Time and Space Complexity/B11. Sot Dates.java
@@ -0,0 +1,29 @@
+Sort Dates
+1. You are given an array(arr) of different dates in format DD-MM-YYYY.
+2. You have to sort these dates in increasing order.
+Input Format
+An Integer N 
+arr1
+arr2..
+n integers
+Output Format
+Check the sample output and question video.
+
+Constraints
+1 <= N <= 10000
+All dates are between year 0 to year 2500
+Sample Input
+5
+12041996
+20101996
+05061997
+12041989
+11081987
+Sample Output
+11081987
+12041989
+12041996
+20101996
+05061997
+
+Code:

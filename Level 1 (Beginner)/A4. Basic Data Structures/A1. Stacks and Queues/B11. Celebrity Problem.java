3caee7c338bab6ef32b61fb65e96b12d01aa00a8
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-05-12 14:14:50 +0530

    Update B11. Celebrity Problem.java

--- Level 1 (Beginner)/A4. Basic Data Structures/A1. Stacks and Queues/B11. Celebrity Problem.java
+++ Level 1 (Beginner)/A4. Basic Data Structures/A1. Stacks and Queues/B11. Celebrity Problem.java
@@ -27,3 +27,73 @@ Sample Output
 
 Code:
 
+package Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class CelebrityProblem {
+	
+	public static int[][] takeInput(){
+		
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int m= s.nextInt();
+		
+		int arr[][]= new int[n][m];
+		
+		for( int i=0; i< arr.length; i++) {
+			for( int j=0; j< arr[0].length; i++) {
+				arr[i][j]= s.nextInt();
+			}
+		}
+		return arr;
+	}
+	
+	public static void findCelebrity( int arr[][]) {
+		
+		Stack<Integer> st= new Stack<>();
+		for(int i= 0; i < arr.length; i++) {
+			st.push(i); //stack mai sabko add kardenge
+		}
+		
+		while(st.size() >= 2) { //jabtak stak mai 2 ya 2 se jada log hai 
+			// to hum do pop karenge
+			int i= st.pop();
+			int j= st.pop();
+			
+			//aur check karenge ki kya i j ko janta hai ya nhi
+			if( arr[i][j] == 1) {
+				//agar i j ko janta hai means i celebrity nhi hai
+				// to jo celebrity nhi hai usko bahar rakhenge aur jo celebrity ho skta hai usko wapas dal denge
+				st.push(j); //i celebrity nhi hai to j ko dal denge
+				
+			}else {
+				//if i doesn't know j means j is not celebrity
+				st.push(i);// j celebrity nhi hai means i ko wapas dal denge
+			}
+		}
+		
+		//aise krte krte last mai aapke pas 2 se kam log reh jayenge, last mai 1 hi reh jayega to mai usko bolta hu potential
+		int pot= st.pop();
+		// ab apko potential ki puri and column check karna hai
+		for( int i=0; i< arr.length; i++) {
+			if( i !=  pot) { //hume check tab karna hai jabt i potential ke equal nhi hai
+				
+				if( arr[i][pot] == 0 || arr[pot][i] == 1) { //agar koi i aisa nikal aya jo iss potential celebrity ko nhi janta ya koi i aisa nikal aya jisko potenntial janta hai to celebrity nhi hoga so hum none print karenge
+					System.out.println("none");
+					return;
+				}
+			}
+		}
+		
+		//loop ke bahar aake hum potential print karenge
+		System.out.println(pot);
+	}
+
+	public static void main(String[] args) {
+
+		int arr[][]= takeInput();
+		findCelebrity(arr);
+	}
+}

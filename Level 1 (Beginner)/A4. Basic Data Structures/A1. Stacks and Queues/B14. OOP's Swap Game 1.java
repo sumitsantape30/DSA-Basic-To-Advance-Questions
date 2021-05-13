e6d79a4e4e6895a084553ff24575edf63b9210a8
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-05-13 22:42:45 +0530

    Create B14. OOP's Swap Game 1.java

---
+++ Level 1 (Beginner)/A4. Basic Data Structures/A1. Stacks and Queues/B14. OOP's Swap Game 1.java
@@ -0,0 +1,45 @@
+package Stack;
+
+
+public class SwapGame1 {
+	
+	public static class Person{
+		int age;
+		String name;
+		
+		void saysHi() {
+			System.out.print(name+"["+age+"] says hi ");
+		}
+	}
+	
+	public static void swap1(Person prsn1, Person prsn2) {
+		
+		Person temp= prsn1;
+		prsn1= prsn2;
+		prsn2= temp;
+	}
+
+	public static void main(String[] args) {
+
+		Person p1= new Person();
+		p1.age = 10;
+		p1.name= "A";
+		//p1.saysHi();
+		
+		Person p2= new Person();
+		p2.age= 20;
+		p2.name= "B";
+		//p2.saysHi();
+		
+		//swap karne se pehle aur swap karne ke bad dono ki property call karenge taki hume pta chale
+		p1.saysHi();
+		p2.saysHi();
+		swap1(p1, p2);
+		System.out.println();
+		p1.saysHi();
+		p2.saysHi();
+		
+		// swap function mai jo references swap hue the  hue but jab function khatam hua to mar gye isliye swap nhi hua.
+	}
+
+}

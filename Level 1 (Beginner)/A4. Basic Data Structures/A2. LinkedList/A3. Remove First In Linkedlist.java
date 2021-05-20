11951c4553b2f1b78f257ade63be8cdee9696c88
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-05-20 19:23:48 +0530

    Create A3. Remove First In Linkedlist.java

---
+++ Level 1 (Beginner)/A4. Basic Data Structures/A2. LinkedList/A3. Remove First In Linkedlist.java
@@ -0,0 +1,133 @@
+Remove First In Linkedlist
+
+1. You are given a partially written LinkedList class.
+2. Here is a list of existing functions:
+   2.1 addLast - adds a new element with given value to the end of Linked List
+   2.2. display - Prints the elements of linked list from front to end in a single line. All 
+     elements are separated by space
+     2.3. size - Returns the number of elements in the linked list.
+3. You are required to complete the body of removeFirst function 
+     3.1. removeFirst - This function is required to remove the first element from 
+          Linked List. Also, if there is only one element, this should set head and tail to 
+          null. If there are no elements, this should print "List is empty".
+4. Input and Output is managed for you.
+Input Format
+Input is managed for you
+Output Format
+Output is managed for you
+
+Constraints
+None
+Sample Input
+addLast 10
+addLast 20
+addLast 30
+display
+removeFirst
+size
+addLast 40
+addLast 50
+removeFirst
+display
+size
+removeFirst
+removeFirst
+removeFirst
+removeFirst
+quit
+Sample Output
+10 20 30 
+2
+30 40 50 
+3
+List is empty
+
+Code:
+
+package LinkedList;
+
+import java.util.Scanner;
+
+class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		next = null;
+	}
+
+	public Node() {
+         
+	}
+}
+
+class LinkedList {
+	Node head;
+	Node tail;
+	int size;
+	
+	public LinkedList(){
+		head= null;
+		tail= null;
+		size=0;
+	}
+
+	void addLast(int val) {
+		Node temp= new Node();
+		temp.data= val;
+		temp.next= null;
+		
+		if( size == 0) {
+			head= tail= temp; 
+		}else { 
+			tail.next= temp;
+			tail= tail.next;
+		}
+		size++;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public void display() {
+		
+		Node temp= head;
+		while( temp != null) {
+			System.out.print(temp.data+" ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+	
+	public void removeFirst() {
+		if( size == 0) {
+			//khali list hai to list is empty print karenge
+			System.out.println("List is Empty");
+		}else if( size() == 1) { //agar size 1 hai to head and tail ko null kardenge aur size ko 0 kr denge
+			head= tail= null;
+			size=0;
+		} else{ //agar jada items hai to
+			head= head.next;
+			size--;
+		}
+	}
+}
+
+public class LLQues {
+
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int val= s.nextInt();
+		LinkedList list1= new LinkedList();
+		list1.addLast(10);
+		list1.addLast(20);
+		list1.addLast(30);
+		list1.addLast(40);
+		list1.display();
+		list1.removeFirst();
+
+	}
+}

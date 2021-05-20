e6378f2706624c428a7a31211d9a783e1c35506e
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-05-20 23:37:05 +0530

    Update B11. Linked List To Stack Adapter.java

--- Level 1 (Beginner)/A4. Basic Data Structures/A2. LinkedList/B11. Linked List To Stack Adapter.java
+++ Level 1 (Beginner)/A4. Basic Data Structures/A2. LinkedList/B11. Linked List To Stack Adapter.java
@@ -74,3 +74,44 @@ Sample Output
 10
 
 Code:
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class LLToStackAdapt {
+
+	public static class LLToStackAdapter {
+		LinkedList<Integer> list; // data memeber
+
+		public LLToStackAdapter() {
+			list = new LinkedList<>();
+		}
+ 
+		int size() {
+			return list.size();
+		}
+
+		void push(int val) {
+			list.addFirst(val);
+		}
+
+		int pop() {
+			if (size() == 0) {
+				System.out.println("Stack Underflow");
+				return -1;
+
+			} else {
+				return list.removeFirst();
+			}
+		}
+
+		int top() {
+			if (size() == 0) {
+				return list.removeFirst();
+
+			} else {
+				return list.getFirst();
+			}
+		}
+	}
+}

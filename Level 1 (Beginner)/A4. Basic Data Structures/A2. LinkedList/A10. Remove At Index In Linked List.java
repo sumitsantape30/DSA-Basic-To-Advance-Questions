8b40a3ea0e5636e0eacdba3a913fe682fdce747c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-05-21 17:24:16 +0530

    Create A10. Remove At Index In Linked List.java

---
+++ Level 1 (Beginner)/A4. Basic Data Structures/A2. LinkedList/A10. Remove At Index In Linked List.java
@@ -0,0 +1,295 @@
+Remove At Index In Linked List
+
+1. You are given a partially written LinkedList class.
+2. Here is a list of existing functions:
+2.1 addLast - adds a new element with given value to the end of Linked List
+   2.2. display - Prints the elements of linked list from front to end in a single line. All 
+   elements are separated by space
+  2.3. size - Returns the number of elements in the linked list.
+  2.4. removeFirst - Removes the first element from Linked List. 
+  2.5. getFirst - Returns the data of first element. 
+  2.6. getLast - Returns the data of last element. 
+  2.7. getAt - Returns the data of element available at the index passed. 
+  2.8. addFirst - adds a new element with given value in front of linked list.
+  2.9. addAt - adds a new element at a given index.
+  2.10. removeLast - removes the last element of linked list.
+3. You are required to complete the body of removeAt function. The function should remove the element available at the index passed as parameter. If the size is 0, should print "List is empty". If the index is inappropriate print "Invalid arguments". Also consider the case when list has a single element.
+4. Input and Output is managed for you.
+Input Format
+Input is managed for you
+Output Format
+Output is managed for you
+
+Constraints
+None
+Sample Input
+addFirst 10
+getFirst
+addAt 0 20
+getFirst
+getLast
+display
+size
+addAt 2 40
+getLast
+addAt 1 50
+addFirst 30
+removeAt 2
+getFirst
+removeAt 0
+removeAt 1
+addAt 2 60
+display
+size
+removeAt 0
+removeAt 1
+getFirst
+quit
+Sample Output
+10
+20
+10
+20 10 
+2
+40
+30
+  
+Code:
+
+package LinkedList;
+
+import java.util.Scanner;
+
+class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		next = null;
+	}
+
+	public Node() {
+         
+	}
+}
+
+class LinkedList {
+	Node head;
+	Node tail;
+	int size;
+	
+	public LinkedList(){
+		head= null;
+		tail= null;
+		size=0;
+	}
+
+	void addLast(int val) {
+		Node temp= new Node();
+		temp.data= val;
+		temp.next= null;
+		
+		if( size == 0) {
+			head= tail= temp; 
+		}else { 
+			tail.next= temp;
+			tail= tail.next;
+		}
+		size++;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public void display() {
+		
+		Node temp= head;
+		while( temp != null) {
+			System.out.print(temp.data+" ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+	
+	public void removeFirst() {
+		if( size == 0) {
+			System.out.println("List is Empty");
+		}else if( size() == 1) { 
+			head= tail= null;
+			size=0;
+		} else{ 
+			head= head.next;
+			size--;
+		}
+	}
+	
+	public int getFirst() {
+		if(size == 0) { 
+			System.out.println("List is Empty");
+			return -1;
+		}
+		return head.data;
+	}
+	
+	public int getLast() {
+		if(size == 0) { 
+			System.out.println("List is Empty");
+			return -1;
+		}
+		return tail.data;
+	}
+	
+	public int getAt(int idx) {
+		if(size == 0) { 
+			System.out.println("List is Empty");
+			return -1;
+		}else if( idx < 0 || idx >= size()) { 
+			System.out.println("Invalid Argument");
+			return -1;
+		}else {
+			Node temp = head;
+			for( int i=0; i< idx ; i++) {
+				temp = temp.next;
+			} 
+			return temp.data;
+		}
+	}
+	
+	public void addFirst(int val) {
+		Node temp= new Node();
+		temp.data= val;
+		temp.next= head;
+		head= temp; 
+		
+		if( size == 0) { 
+			tail= temp;
+		}
+		size++;
+	}
+	
+	public void addAt(int idx, int val) {
+		
+		if(idx < 0 || idx > size) {
+			System.out.println("Invalid Arguments");
+		}else if( idx == 0) {
+			addFirst(val);
+		}else if( idx == size) {
+			addLast(val);
+		}else {
+			Node node= new Node();
+			node.data= val;
+		
+			Node temp= head;
+			for( int i=0; i< idx-1; i++) { 
+				temp= temp.next;
+			}
+			
+			node.next = temp.next; 
+			temp.next= node;
+			
+			size++; 
+		}
+	}
+	
+	public void removeLast() {
+		if( size == 0) { 
+			System.out.println("List is Empty");
+		}else if( size() == 1) {  
+			head= tail= null;
+			size=0;
+		} else{ 
+			
+			Node temp= head;
+			for( int i=0; i< size-2; i++) {
+				temp= temp.next;
+			}
+			
+			tail= temp;
+			temp.next= null;
+			size--;
+		}
+	}
+	
+	private Node getNodeAt(int idx) {
+		Node temp= head;
+		for( int i=0; i< idx; i++) {
+			temp= temp.next;
+		}
+		return temp;
+	}
+	
+	public void reverseDI() {
+
+		int li = 0;
+		int ri = size - 1;
+
+		while (li > ri) {
+			Node left = getNodeAt(li);
+			Node right= getNodeAt(ri);
+			
+			int temp= left.data;
+			left.data= right.data;
+			right.data= temp;
+			
+			li++;
+			ri--;
+		}
+	}
+	
+	public void reversePI() { 
+		Node prev= null;
+		Node curr= head;
+		
+		while( curr != null) { 
+			Node next = curr.next; 
+			
+			curr.next= prev; 
+			
+			prev= curr; 
+			curr= next; 
+		}
+		
+		Node temp= head;
+		head = tail;
+		tail= temp;
+	}
+	
+	public void removeAt( int idx) {
+		if( idx < 0 || idx >= size) { //agar index negative ya fir >= size de diya hai to hum bolenge invalid arguments
+			System.out.println("Invalid Arguments");
+		}else if( idx == 0) { //agar index 0 pass kiya to mai sidha removeFirst() krdunga
+			removeFirst();
+		}else if( idx == size -1){ // index last rha to mai removeLast() ko bolunga remove karne
+			removeLast();
+		}else {
+			//agar bich ka koi element remove karna hai to yeh mai handle karunga
+			Node temp= head;
+			for( int i=0; i< idx-1; i++) {
+				temp= temp.next;
+			}
+			
+			//mera temp ab uss index ke ek piche aagya hai
+			temp.next= temp.next.next;
+			size--; //size ko kam krdenge
+		}
+	}
+}
+
+public class LLQues {
+
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int val= s.nextInt();
+		LinkedList list1= new LinkedList();
+		list1.addLast(10);
+		list1.addLast(20);
+		list1.addLast(30);
+		list1.addLast(40);
+		list1.display();
+		list1.removeFirst();
+
+	}
+}

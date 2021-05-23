46721664610ba6c2230a60ab3cf60345a3173841
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-05-23 22:12:16 +0530

    Update A1. Generic Tree Constructor.java

--- Level 1 (Beginner)/A4. Basic Data Structures/A3. Generic Trees/A1. Generic Tree Constructor.java
+++ Level 1 (Beginner)/A4. Basic Data Structures/A3. Generic Trees/A1. Generic Tree Constructor.java
@@ -6,7 +6,7 @@ import java.util.Stack;
 
 public class GenericTreeConstructor {
 
-	private class Node {
+	private static class Node {
 		int data;
 		ArrayList<Node> children = new ArrayList<Node>();
 
@@ -17,7 +17,7 @@ public class GenericTreeConstructor {
 		int arr[] = { 10, 20, 50, -1, 60, -1, 60, -1, -1, 30, 70, -1, 30, 70, -1, 80, 110, -1, 80, 110, -1, 120, -1, -1,
 				90, -1, -1, 40, 100, -1, -1, -1 };
 
-		Node root; //intially root hi rahega badme root milega then update karenge
+		Node root= null; //intially root mai null hi rahega badme root milega then update karenge
 		Stack<Node> st = new Stack<>(); // nodes ki stack banayengee
 
 		for (int i = 0; i < arr.length; i++) {

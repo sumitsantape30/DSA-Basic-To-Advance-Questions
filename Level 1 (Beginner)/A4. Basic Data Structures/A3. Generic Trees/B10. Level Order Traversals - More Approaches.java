02eb3636258bc6a1e9e536c01cf6c056adae6c9a
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-05-27 13:33:21 +0530

    Create B10. Level Order Traversals - More Approaches.java

---
+++ Level 1 (Beginner)/A4. Basic Data Structures/A3. Generic Trees/B10. Level Order Traversals - More Approaches.java
@@ -0,0 +1,110 @@
+Approach 2 ============================================================================
+
+Code:
+
+private static class
+  
+  Node {
+		int data;
+		ArrayList<Node> children = new ArrayList<>();
+		
+		// main queue mai null nhi dal skte isliye ek constructor banayenge jo data leta hai
+		public Node(int data) {
+			this.data= data;
+		}
+		
+		public Node() { //default constructor bhi rakhenge
+			
+		}
+	}
+
+
+ //approach 2
+	 public static void levelOrderLinewise2(Node node) {
+		 Queue<Node> mq= new LinkedList<>() ; //isme koi child queue nhi hai
+			mq.add(node); 
+			mq.add(new Node(-1)); //node ke sath sath maine null bhi add kr skte the but woh nhi hoga so null ki jaga aisa new node dalunga jiska data -1 hoga
+
+						
+			while( mq.size() > 0){ 
+				node= mq.remove(); 
+				//jo remove kiya hai uska data agar woh -1 nhi hai to print kardo aur uska children add kardo
+				if( node.data != -1) {
+					
+				 System.out.println(node.data+" "); 
+				 
+				 for( Node child: node.children) {
+					 mq.add(child);
+				 }
+			
+				}else { // agar node ka data -1 hai to check kare kya main queue ka size
+					if( mq.size() > 0) {
+						//agar main queue ka size greater than 0 hai tabhi dubara -1 wala node add krde aur enter bhi lagayenge
+						mq.add(new Node(-1));
+						System.out.println();
+					}		
+				}	
+			}
+	 }
+
+Appraoch 3 (Count Approach)===========================================================================================
+   //approach 3 -- Count approach
+	public static void levelOrderLinewise3(Node node) {
+		Queue<Node> mq = new LinkedList<>(); // isme koi child queue nhi hai ek hi queue hai
+		mq.add(node);
+
+		while (mq.size() > 0) {
+			int cicl= mq.size(); //aatehi yeh dekhe children in current level aur woh main queue ka size aapko batayega
+
+			for( int i=0; i< cicl; i++) { //jitne children hai current level mai utni bar loop chalaye
+				//aur fir wahi rpa
+				mq.remove();
+				System.out.println(node.data +" ");
+				
+				for( Node child: node.children ) {
+					//hum wapas usko add kardenge
+					mq.add(child);
+				}
+			}
+			
+			//ab jab iss loop mese bahar niklenge to current level khatam ho chuka hai to enter mariya
+			System.out.println();
+		}
+	}
+
+Approach 4 (Pair) ===================================================================
+  
+   static class Pair { //pair class banayenge jisme node and level hogi
+		  Node node;
+		  int level;
+		  
+		  public Pair(Node node, int level) {  // constructor
+			  this.node= node;
+			  this.level= level;
+		  }
+	 }// yeh pair tayar 
+	 
+	public static void levelOrderLinewise3(Node node) {
+		Queue<Pair> mq = new LinkedList<>(); //isbar humari Queue<Node> ki nhi <Pair> ki hai
+		mq.add(new Pair(node, 1));// miane isme new Pair dali aur usme node pass ki aur pehle node ka level hoga 1
+		
+		int level = 1; //bahar level nam ka variable bna lenge
+		while( mq.size() > 0) {
+			Pair p= mq.remove();
+			// yeh remove hua
+			if( p.level > level) { //ab dekhe kya p ka level jo hai kya woh level se bda hai
+				//agar level se bda hai to pehle level ko set kardo aur ek enter laga do
+				level= p.level;
+				System.out.println();
+			}
+			
+			// agr aisa nhi hai woh usi level ka hai to simple print kare
+			System.out.println(p.node.data+ " ");
+			// aur uske children keliy loop chala de
+			for(Node child: p.node.children) {
+				Pair cp= new Pair(child, p.level +1); // child pair , aur uska level p.level se ek jada hoga
+				mq.add(cp); // aur isko main queue mai add karlo
+			}
+		}	
+	}
+  

7775736ededcf278d4ebfc5e2295b6e7ef2668f2
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-06-22 06:59:19 +0530

    Update A2. Print Z.java

--- Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/A2. Print Z.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/A2. Print Z.java
@@ -6,9 +6,9 @@ public class printInJava {
     
     //To print Z using System.out.println();
 		System.out.println("*****"); //pehli line mai 0 space and 5 stars
-		System.out.println("   * "); //dusri line mai 3 spaces 
-		System.out.println("  *  ");
-		System.out.println(" *   "); 
+		System.out.println("   *"); //dusri line mai 3 spaces 
+		System.out.println("  *");
+		System.out.println(" *"); 
 		System.out.println("*****"); //last line mai 5 spaces
 	}
 

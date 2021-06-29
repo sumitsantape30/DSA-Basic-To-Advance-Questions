4b85dfa2fbc51a64bfd40e1e7283d891a02347f9
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-06-29 14:04:30 +0530

    Update B16. Prime Factorisation Of A Number.java

--- Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/B16. Prime Factorisation Of A Number.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/B16. Prime Factorisation Of A Number.java
@@ -28,7 +28,8 @@ public class primeFactorizationOfANumber {
 	public static void printPrimeFactorization(int n) {
 		
 		//mai pehla divisor 2 choose karra aur jabtak div root n ke barabar na hojaye tabtak kam karenge
-		for( int div= 2; div*div <= n; div++) {
+		for( int div= 2; div*div <= n; div++) { // n keliye uske sare prime factors root n se niche niche lie karenge except 1, root n se bda prime factor bas 1 hai
+			//so kisi bhi number n keliye, root n se bade prime factor 2 nhi ho skte ek hi ho skta hai. n ke prime factors root n se bade 2 nhi ho skte hoga to ek hi hoga
 			
 			//jabtak yeh n divide hota rehta hai tabtak divide krte rho means katate rho aur har ek bar apne divisor ko print krte rho
 			while( n % div == 0) {
@@ -37,7 +38,7 @@ public class primeFactorizationOfANumber {
 			}
 		}
 		
-		//isme ek case chhut jata hai kabhi kabhi jaise 46 ki bta kare, 46 is 2*23, 2 se 46 ko divide kiye 23 bach gya ab 23 tak ki table chalani jarurat nhi hai hum quit kardenge lekin jab bahar ayenge n cut karke 1 nhi bana hai
+		//isme ek case chhut jata hai kabhi kabhi jaise 46 ki bat kare, 46 is 2*23, 2 se 46 ko divide kiye 23 bach gya ab 23 tak ki table chalani jarurat nhi hai hum quit kardenge lekin jab bahar ayenge n cut karke 1 nhi bana hai
 		//so agar apke n 1 nhi bana hai means n 1 se bda bach gya hai to yeh only number hai jo root n ke bad ata hai jo n ko divide kr skta hai *open book and see example of number 46*
 	   if( n != 1) {
 		   //agar kat kar 1 nhi kar paye to jo number bacha hua tha jaise 23 bacha tha woh khudme ek prime factor hai to usko print kardenge

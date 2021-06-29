1a25f25546ff191cbff4361f12afd7fa0b166414
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-06-29 11:27:36 +0530

    Update B14.2. Rotate a Number.java

--- Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/B14.2. Rotate a Number.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/B14.2. Rotate a Number.java
@@ -1,1 +1,41 @@
+package Restart;
+
+import java.util.Scanner;
+
+public class printZ {
+
+	public static void rotate( int n, int k) {
+		
+		int num=0; //number of digits
+		while( n != 0) {
+			num++;
+			n= n/10;
+		}
+		
+		k= k % num;
+		if( k < 0) {
+			k += num;
+		}
+		
+		//ab divisor aur multiplier ki value set karenge
+		int div=(int) Math.pow(10, k);
+		int mult= (int) Math.pow(10, num-k);
+		
+		//fir quotient aur remainder nikalo
+		
+		int q= n /div;
+		int r= n % div;
+		
+		int inv= (r*mult) + q;
+		System.out.println(inv);
+	}
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int k= s.nextInt();
+		rotate(n, k);
+	}
+}
 

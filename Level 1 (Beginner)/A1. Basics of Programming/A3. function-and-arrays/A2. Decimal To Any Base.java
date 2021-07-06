45cfd4fbdeaf032937c3a1ca0d8dc0abfecaf1cc
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-06 08:27:01 +0530

    Update A2. Decimal To Any Base.java

--- Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/A2. Decimal To Any Base.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/A2. Decimal To Any Base.java
@@ -35,10 +35,10 @@ public class DecimaltoanyBase {
 		int pow= 1; //10 ki sabse choti power hoti hai 10^0 that's actually 1
 		
 		while( n > 0) {
-			int rem= n % b;  //jis base mai convert karna hai usase divide karenge
+			int rem= n % b;  //jis base mai convert karna hai usase divide karenge, so sabse pehle uski last digit nikal lo
 			n= n/b; //number ko chota kardenge
 			
-			//so jo pehle digit se jo pehla remainder aya hai usko sabse right mai bhejna hai to uske 10 ki sabse choti power se mutlipy karenge
+			//so jo pehle digit se jo pehla remainder(last digit) aya hai usko sabse right mai bhejna hai to uske 10 ki sabse choti power se mutlipy karenge
 		    // 10 ki sabse choti power hoti hai 10^0 that's actually 1
 			ans= ans + (rem*pow); //ans ke purani value mai add karenge. Base ki power se multiply krte rahenge
 			//next ane wale remainder ko 10 ki badi power se multiply karenge taki wo thodase left mai aaye

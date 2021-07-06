0e1d82889bfe0ca1c1cb45edae3b864a0bc2fdc1
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-06 10:07:08 +0530

    Update A5. Any Base Addition.java

--- Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/A5. Any Base Addition.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/A5. Any Base Addition.java
@@ -30,16 +30,16 @@ public class AnyBaseAddition {
 	
 	 public static int getSum(int b, int n1, int n2){
 
-		 int rv=0;
-		 int c= 0; //initially mera carry 0 hai
-		 int p=1; //power hai yeh
+		 int ans=0;
+		 int carry= 0; //initially mera carry 0 hai
+		 int power=1; //power hai yeh
 		 //mera kam tabtak chalega jabtak n1 mai digit bachi hai ya fir n2 mai digit bachi hai ya carry bacha hai
 		 while( n1 > 0 || n2 > 0 || c > 0) {
 			 
 			 //ab n1 aur n2 ki aakhri digit nikalenge
 			 //ab pehli digit nikalenge
-			 int d1= n1 % 10;
-			 int d2= n2% 10; //dusri digit
+			 int l1= n1 % 10; //last digit of n1
+			 int l2= n2% 10; //last digit of n2
 			 
 			 //ab donoko chota karenge
 			 //ab n1 ko chota karunga
@@ -47,19 +47,24 @@ public class AnyBaseAddition {
 			 n2= n2/10; //n2 kobhi chota karunga
 			 
 			 //ab mai dono digit ka sum nikalta hu aur sathme carry bhi add hota hai
-			 int d= d1+ d2 + c;
+			 int sum= d1+ d2 + c;
 			 //agar inn teeno ko add karke jo number aya woh 8 se chota aya to thik hai, agar woh 8  se bada hai to woh carry chodega aur ayapr spare chodega
 			 // following is the way to do it
-			 c = d/ b; //agar woh 8 ya 8 se bda number chhod rha hai to carry apne aap 1 hojayega aur agar 7 tak ka  number chodata hai to 7/8 is zero
-			 d = d % b; //this is my remaining digit, remainder
+			 //ab quotient aur remainder nikalenge
+			 int q = sum/ b; //agar woh 8 ya 8 se bda number chhod rha hai to carry apne aap 1 hojayega aur agar 7 tak ka  number chodata hai to 7/8 is zero
+			 r = sum % b; //this is my remaining digit, remainder
 			 
-			 //ab yeh digit hume use krni hai yeh rv mai add hogi and yeh normal add nhi hogi iske sath ek power multiply hogi
-			 rv += d* p;
+			 //ab yeh remainder hume use krni hai yeh ans mai add hogi and yeh normal add nhi hogi iske sath ek power multiply hogi
+			 ans += (r* p);
+			 
+			 //ab agli bar keliye now update carry with the help of q
+			 carry = q;
+			 
 			 //aur power ko 10 se badhe dijiyega
 			 p= p * 10; //fir power kobhi update kardo
 			 
 		 }
-		 return rv;
+		 return ans;
 		 
 	 }
 

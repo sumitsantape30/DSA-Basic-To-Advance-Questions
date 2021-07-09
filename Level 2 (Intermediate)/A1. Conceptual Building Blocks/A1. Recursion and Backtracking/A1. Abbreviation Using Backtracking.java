b4f58570dc2ac01aa42ab25cd1119cb434e93e14
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-09 13:50:26 +0530

    Create A1. Abbreviation Using Backtracking.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/A1. Abbreviation Using Backtracking.java
@@ -0,0 +1,27 @@
+Abbreviation Using Backtracking
+
+1. You are given a word.
+2. You have to generate all abbrevations of that word.
+
+Use recursion as suggested in question video
+Input Format
+A string representing a word
+Output Format
+Check the sample ouput and question video.
+Question Video
+
+  COMMENTConstraints
+1 <= length of string <= 32
+Sample Input
+pep
+Sample Output
+pep
+pe1
+p1p
+p2
+1ep
+1e1
+2p
+3
+
+Code:

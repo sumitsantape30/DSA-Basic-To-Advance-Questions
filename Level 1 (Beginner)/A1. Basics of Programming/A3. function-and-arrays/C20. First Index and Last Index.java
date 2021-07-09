a9ce9165c0be730494b587366404d5bd0b89ec26
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-09 08:35:08 +0530

    Update C20. First Index and Last Index.java

--- Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/C20. First Index and Last Index.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A3. function-and-arrays/C20. First Index and Last Index.java
@@ -42,3 +42,77 @@ Sample Output
 9
 
 Code:
+
+package Restart;
+
+import java.util.Scanner;
+
+public class Problems {
+
+	public static int[] takeInput() {
+
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int arr[] = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = s.nextInt();
+		}
+		return arr;
+	}
+
+	public static void firstIndex(int arr[], int ele) {
+
+		int start = 0;
+		int end = arr.length - 1;
+		int fi = -1;
+
+		while (start <= end) {
+
+			int mid = (start + end) / 2;
+
+			if (arr[mid] == ele) {
+				fi = ele; // agar data barbar hai to answer mark kare, coz this may be an answer aur left ko jana na bhule
+				end = mid - 1;
+			} else if (ele > arr[mid]) {
+				start = mid + 1;
+			} else if (ele < arr[mid]) {
+				end = mid - 1;
+			}
+		}
+
+		System.out.println(fi);
+	}
+
+	public static void lastIndex(int arr[], int ele) {
+
+		int start = 0;
+		int end = arr.length - 1;
+		int li = -1;
+
+		while (start <= end) {
+
+			int mid = (start + end) / 2;
+
+			if (arr[mid] == ele) {
+				li = ele; // agar data barbar hai to answer mark kare, coz this may be an answer aur left ko jana na bhule
+				start= mid +1;
+			} else if (ele > arr[mid]) {
+				start = mid + 1;
+			} else if (ele < arr[mid]) {
+				end = mid - 1;
+			}
+		}
+
+		System.out.println(li);
+	}
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		int arr[] = takeInput();
+		int ele= s.nextInt();
+		firstIndex(arr, ele);
+		lastIndex(arr, ele);
+
+	}
+}

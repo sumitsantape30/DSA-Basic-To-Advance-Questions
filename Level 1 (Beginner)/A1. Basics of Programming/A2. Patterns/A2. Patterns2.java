a98848ca23ba73660fb29ecd57c7691e18396543
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-10 08:28:08 +0530

    Update A2. Patterns2.java

--- Level 1 (Beginner)/A1. Basics of Programming/A2. Patterns/A2. Patterns2.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A2. Patterns/A2. Patterns2.java
@@ -31,3 +31,25 @@ public class Pattern2 {
 	}
 }
 
+================================== Approach 2===================================
+	
+	package Restart;
+
+import java.util.Scanner;
+
+public class Problems {
+	
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		
+		for( int i=1; i<= n; i++) {
+			
+			for( int j=1; j<= n-i-1; j++) {
+				System.out.print("*\t");
+			}
+			System.out.println();
+		}
+	}
+}

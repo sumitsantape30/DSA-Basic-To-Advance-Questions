530d496426936f8a6e58d1e2e74088d7cc784b8b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-15 07:51:07 +0530

    Update A6. Rotate By 90 Degree.java

--- Level 1 (Beginner)/A1. Basics of Programming/A4. 2D Arrays/A6. Rotate By 90 Degree.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A4. 2D Arrays/A6. Rotate By 90 Degree.java
@@ -43,3 +43,75 @@ Sample Output
 44 34 24 14
 
 Code:
+
+package DSA_Cracker;
+
+import java.util.Scanner;
+
+public class RotateMatrix {
+	
+	public static int[][] takeInput(){
+		
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		
+		int arr[][]= new int[n][n];
+		
+		for( int i=0; i< arr.length; i++) {
+			for( int j=0; j< arr[0].length; j++) {
+				arr[i][j]= s.nextInt();
+			}
+		}
+		return arr;
+	}
+	
+	//ab hum matrix ka transpose lenge, arr[i][j] mai arr[j][i] dalna hota hai
+	public static int[][] transpose( int arr[][]){
+		
+		for( int i=0; i< arr.length; i++) {
+			for( int j=i; j< arr[0].length; j++) { //mai bas upper triangle mai move karunga  isliye j=i kiya taki triangularly move ho. j wala first loop 0 start hoga then next time j 1 se start hoga
+				int temp= arr[i][j];
+				arr[i][j]= arr[j][i];
+				arr[j][i]= temp;
+			}
+		}
+		return arr;
+	}
+	
+	//transpose hogya ab row by row saari line ko reverse karenge
+	//saari row ko reverse karenge jisse first column last column ban jaye
+	public static int[][] rowbyrowRev( int arr[][]){
+		
+		for( int i=0; i< arr.length; i++) {
+			int li=0; //left index
+			int ri= arr[i].length-1; //right index
+			
+			while( li > ri) {
+				//values swap karte jayenge
+				int temp= arr[i][li];
+				arr[i][li]= arr[i][ri];
+				arr[i][ri]= temp;
+				li++;
+				ri--;
+			}
+		}
+		return arr;
+	}
+	
+	public static void print( int arr[][]) {
+		
+		for( int i=0; i< arr.length; i++) {
+			for( int j=0; j< arr[0].length; j++) {
+				System.out.println(arr[i][j]);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int arr[][]= takeInput();
+		int ans[][]= transpose(arr);
+		print(ans);
+	}
+
+}

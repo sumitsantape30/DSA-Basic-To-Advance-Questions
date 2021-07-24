2b4d2a7353d77b0472906ff5f1b9202e8f7542e6
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-24 07:37:00 +0530

    Update A1. Display Array.java

--- Level 1 (Beginner)/A2. Recursion And Backtracking/A2. Recursion in Arrays/A1. Display Array.java
+++ Level 1 (Beginner)/A2. Recursion And Backtracking/A2. Recursion in Arrays/A1. Display Array.java
@@ -55,7 +55,7 @@ public class DisplayArray {
 	
 	// expectation yeh hai ki yeh idx se end tak sab print karde
 	// and faith yeh hai ki idx+1 se end tak print karna woh janta hai
-	public static void displayArray(int arr[], int idx) {
+	public static void displayArray(int arr[], int idx) { // agr input mai muje array mila to kam nhi hoga, sirf array se mai problem ko chota nhi kr pa rha so muje index bhi chahiye taki mai choti problem achieve kr saku
 		if( idx == arr.length) {
 			return;
 		}
@@ -69,7 +69,7 @@ public class DisplayArray {
 
 		int arr[] = takeInput();
 		int n= arr.length;
-		displayArray(arr,0);
+		displayArray(arr,0); 
 
 
 	}

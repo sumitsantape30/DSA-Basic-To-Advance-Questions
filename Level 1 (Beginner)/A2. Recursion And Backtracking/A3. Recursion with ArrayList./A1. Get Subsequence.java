a3ae67f4c33beb7e0837db9863acc0cd13f61952
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-25 08:06:53 +0530

    Update A1. Get Subsequence.java

--- Level 1 (Beginner)/A2. Recursion And Backtracking/A3. Recursion with ArrayList./A1. Get Subsequence.java
+++ Level 1 (Beginner)/A2. Recursion And Backtracking/A3. Recursion with ArrayList./A1. Get Subsequence.java
@@ -58,6 +58,34 @@ public class getSubsequence {
 		} //2 loop lagane se order alag ayega bass that's it
 		return myres;
 	}
+	
+	
+	public static ArrayList<String> gss(String str){
+		
+		if( str.length() == 0) {
+			ArrayList<String> bans= new ArrayList<String>();
+			bans.add("");
+			return bans;
+		}
+		
+		char ch= str.charAt(0);
+		String ros= str.substring(1);
+		
+		ArrayList<String> rr= gss(ros);
+		
+		ArrayList<String> mr= new ArrayList<String>();
+		
+		for( int i=0 ; i< rr.size(); i++) { //pehli bar usme 'a'(character) add nhi hoga
+			mr.add(rr.get(i));
+		}
+		
+		for( int i=0; i< rr.size(); i++) { //fir dusri bar woh 'a' (character) aajayega
+			mr.add(ch+ rr.get(i));
+		}
+		
+		return mr;
+		
+	}
 
 	public static void main(String[] args) {
 

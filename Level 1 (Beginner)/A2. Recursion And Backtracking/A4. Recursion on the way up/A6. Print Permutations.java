46e24813f136e8beff8b8e737901c34e292a4cef
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-07-29 08:10:10 +0530

    Update A6. Print Permutations.java

--- Level 1 (Beginner)/A2. Recursion And Backtracking/A4. Recursion on the way up/A6. Print Permutations.java
+++ Level 1 (Beginner)/A2. Recursion And Backtracking/A4. Recursion on the way up/A6. Print Permutations.java
@@ -53,6 +53,21 @@ public class PrintPermutations {
 			 printPermutations(roq, asf + ch); // answer so far mai jo character humne choose kiya hai woh jodkar call laga denge 
 		 } 
 	 }
+	
+	public static void printPermutation(String str, String asf) {
+		if( str.length() == 0) {
+			System.out.println(asf);
+			return;
+		}
+		
+		for( int i=0; i< str.length(); i++) { 
+			
+			char ch= str.charAt(i);
+			String ros= str.substring(0, i) + str.substring(i +1);
+			printPermutation(ros, asf + ch);
+			
+		}
+	}
 
 	public static void main(String[] args) {
 

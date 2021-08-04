3e9b0b080877f58d8a47b3a7fc3e724e809aa7bc
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-04 07:45:30 +0530

    Update A3. Insertion Sort.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A1. Time and Space Complexity/A3. Insertion Sort.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A1. Time and Space Complexity/A3. Insertion Sort.java
@@ -70,7 +70,7 @@ public class InsertionSort {
 				if( isGreater(arr, j, j+1)) {// j apne aage wale se bol rha hai ki agar mai terese bda hu to mere sath swap hoja
 					swap(arr, j, j+1);
 				}else { //aur agar mai terese chota hu to break hoja kyuki merese piche wale tumse chotehi honge to waha jake fayda nhi so break kr denge
-					break;
+					break; //left mai choti value dikhti hai to turant bahar aajao
 				}
 			}
 		}

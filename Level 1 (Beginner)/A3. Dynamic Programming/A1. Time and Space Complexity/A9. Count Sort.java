09a0976781a827bba854c78601c89f4abe131d18
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-06 06:25:28 +0530

    Update A9. Count Sort.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A1. Time and Space Complexity/A9. Count Sort.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A1. Time and Space Complexity/A9. Count Sort.java
@@ -42,23 +42,26 @@ public class CountSort {
 		int range= min - max +1;
 		int farr[]= new int[range];
 		
+		//1. create frequency array
 		//ab har value ki frequency banayenge ki kon kitni bar appear hua
-		
-		for( int i=0; i< arr.length; i++) {
+		for( int i=0; i< arr.length; i++) { //so b original array pe travel karke frequency array ko bharenge
 			//kis index pe woh value badhawayega? jo arr[i] hai usmese min minus hoga, uss index pe woh frequency badhwayega
 			int idx= arr[i]- min;
 			//ab frequency array mai index wale spot pe jayenge aur usko ++ karenge
 			farr[idx]++;
+			//farr[arr[i]-min]++;
 		}
 		//iss step ke bad humare pas frequency aa chuki hogi
 		
-		//ab iss farr ko pre sum mai convert kardenge
+		//2. convert frequency array into prefixsum array
+		//ab iss farr ko prefixsum mai convert kardenge
 		//ab hum usko simple frequency ke hisab se chipka nhi skte instead hum frequency ko convert karke prefix sum array create karlenge
 		for( int i=1; i< farr.length; i++) {
 			//frquency array pe loop lagake iske har position ke andar usse pichle wala add kardenge
-			farr[i]= farr[i] + farr[i-1];
+			farr[i]= farr[i] + farr[i-1]; 
 		} // so mera prefix sum array mai convert hogya
 		
+		//3. travel from right to left in original array and fill answer array
 		//ab mai array pe ulta loop lagaunga
 		//pehle ek ans array banayenge woh bhi array jitne hi banega
 		int ans[]= new int[arr.length];
@@ -66,8 +69,10 @@ public class CountSort {
 		for( int i=arr.length-1 ; i >=0 ; i--) {
 			//ab meri ayegi value
 			int val= arr[i]; //arr[i] mese value laayi
+	
 			// ab frequency array batayega usko kaha dala jaye, woh position batayega idex nhi batyaega
 			int pos= farr[val - min]; //freqeuncy array mai jab usko value ko use karunga but hume pta hai woh sidhi use hoti nhi hai uss value se humesha min minus karna pdta hai
+			//int pos= farr[arr[i]-min]; // varr[i] (last value) iss position pe aana chahta hai
 			
 			//so hume mil gyi position ab position pe nhi dump karenge answer ke andar, index se pos 1 minus hota hai
 			int idx = pos- 1;
@@ -77,7 +82,7 @@ public class CountSort {
 			//aur frequency array mai jo position hai woh kam karenge
 			farr[val - min]--; // iss wali spot pe -- karde, means agli bar keliye position update
 		}
-		
+		//hume orignal array ma hi change lana tha so usme wapas dal denge
 		//ab answer mai loop lagakr original array mai dal denge
 		for( int i=0; i< ans.length; i++) {
 			arr[i]= ans[i];

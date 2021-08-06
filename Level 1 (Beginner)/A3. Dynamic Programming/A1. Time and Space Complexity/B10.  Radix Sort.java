cda66c09c08e4008fb76f3cd265a61301855a086
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-06 07:35:43 +0530

    Update B10.  Radix Sort.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A1. Time and Space Complexity/B10.  Radix Sort.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A1. Time and Space Complexity/B10.  Radix Sort.java
@@ -33,12 +33,24 @@ public class RadixSort {
 
 	public static void radixSort(int[] arr) {
 		
+		int max=0;
+		for( int i=0; i< arr.length; i++) {
+			max= Math.max(max,  arr[i]);
+		}
+		
+		int counter=1;
+		while( max !=0) { //jabtak max 0 nhi hojata max ko 10 se divide krte rho aur countSort ko call krte rho so jitne digit hai utni bar call lagegi
+			max= max/10;
+			countSort(arr, (int)Math.pow(10, counter));
+			counter++;
+		} //jinte number of digits hai utni bar yeh loop chalega
+		
+/*		
 		//radix sort chalaya kitni bar jaye? ones place keliye tens place keliye aisa kabtak chalaye? jabtak sabse bade number ki sari digit puri na hojaye so max nikalna padega
-		int max= Integer.MIN_VALUE;
-		for( int val: arr) {
-			if( val > max) { //agar meri value max se badi hai to mai val ko update kar dunga
-				max= val;
-			}
+		//pehle max nikalna padega
+		int max= Integer.MIN_VALUE; //max=0 le skte ho
+		for( int i=0; i< arr.length; i++){
+			max= Math.max(max, arr[i]);
 		}
 		
 		int exp= 1; // exponent
@@ -47,7 +59,8 @@ public class RadixSort {
 			exp= exp * 10 ; // exponent * 10 kar denge
 			// means pehle ones place keliye sort kardo fir tens place keliye fir hundered place keliye so an so jabtak exponent max se chota hai
 		}
-
+*/
+		
 	}
 
 	
@@ -55,25 +68,24 @@ public class RadixSort {
 	// so countSort ke andar jo frequency array banana hoga woh 0  to 9 hi vary kr skti hai so hum range 10 size ki rakh skte hai coz comparison keliye jo use ho rha woh digit hai
 	// 0 to 9 vary kr rha hai to min humara humesha 0 hoga 
 	// aur freuqency array ke andar arr[i] use nhi kr skte 
-	// kisi bhi number ki digit kaise nikalte hai iske liye opeb book
+	// kisi bhi number ki digit kaise nikalte hai iske liye open book
 	public static void countSort(int arr[], int exp) {
 
-		int farr[] = new int[10];
+		int farr[] = new int[10]; //10 size ka array banayenge cox ek digit ki range 0 to 9 hoti hai
 
-
-		for (int i = 0; i < arr.length; i++) {
-			farr[arr[i] / exp % 10]++; // agar hume 1000 place wali digit nikalni hai to yeh karenge
+		for (int i = 0; i < arr.length; i++) { //ab freqency array bharna hai
+			farr[(arr[i] / exp) % 10]++; // frequency array mai jakar uss place ki frquency badha denge
 		}
 
-		for (int i = 1; i < farr.length; i++) {
-			farr[i] = farr[i] + farr[i - 1];
+		for (int i = 1; i < farr.length; i++) { //frequency array ko prefixsum array mai convert karenge
+			farr[i] = farr[i - 1] + farr[i] ;
 		} 
 		
 		int ans[] = new int[arr.length];
 		for (int i = arr.length - 1; i >= 0; i--) {
-			int pos = farr[arr[i] / exp % 10]-1 ; 
-			ans[pos]= arr[i];
-			farr[arr[i] / exp % 10]--; 
+			int pos = farr[arr[i] / exp % 10] ; 
+			ans[pos-1]= arr[i];
+			farr[arr[i] / exp % 10]--;  //fir agli bar keliye position update
 		}
 
 		for (int i = 0; i < ans.length; i++) {

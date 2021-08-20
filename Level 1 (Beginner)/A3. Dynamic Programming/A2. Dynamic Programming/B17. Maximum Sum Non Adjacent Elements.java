37e5a1a590b22803d4dec8ac7aba49da6f7d49e0
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-20 15:11:06 +0530

    Create B17. Maximum Sum Non Adjacent Elements.java

---
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/B17. Maximum Sum Non Adjacent Elements.java
@@ -0,0 +1,29 @@
+Maximum Sum Non Adjacent Elements
+
+1. You are given a number n, representing the count of elements.
+2. You are given n numbers, representing n elements.
+3. You are required to find the maximum sum of a subsequence with no adjacent elements.
+Input Format
+A number n
+n1
+n2
+.. n number of elements
+Output Format
+A number representing the maximum sum of a subsequence with no adjacent elements.
+Question Video
+
+  COMMENTConstraints
+1 <= n <= 1000
+-1000 <= n1, n2, .. n elements <= 1000
+Sample Input
+6
+5
+10
+10
+100
+5
+6
+Sample Output
+116
+
+Code:

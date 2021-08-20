437cf6cfa7092a462f7a02ac671016490e7533fc
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-20 07:11:25 +0530

    Update A7. Target Sum Subsets - Dp.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/A7. Target Sum Subsets - Dp.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/A7. Target Sum Subsets - Dp.java
@@ -38,6 +38,50 @@ import java.util.Scanner;
 
 public class targetSumSubset {
 	
+	//recursive
+	public static boolean  tss( int arr[], int idx, int target) { // index pass karenge ki iss current index faisla aap kr rhe ho
+		if( target == 0) { //agar target kahipe bhi 0 ho jata hai means hume subset mil gya to return true
+			return true;
+		}
+		
+		if( idx >= arr.length || target < 0) {
+			return false;
+		}
+		
+		boolean f1= tss(arr, idx+1, target); //current element nhi ana chahta, woh bol rha hai kya indx+1 se aage wale elements kya taret bana payenge
+		boolean f2= tss(arr, idx+1, target- arr[idx] ) ; //mai to ana chahta hu, aur kya idx+1 se aage wale elements kya tum updated bana paoge, updated target will be, target- value of current index
+		
+		boolean ans= f1 || f2 ; //donomese ek bhi true hai to return true
+		return ans;
+		
+	}
+	
+	//memoization: kitni size ki array? recursive mai index aur target dono vary kr rhe hai so ek dimension index ko sambhalega aur dusra dimension target ko sambhalega
+	// so 2D storage banegi, Boolean wala array pass karna coz isme by default null pda hua hota hai, aur boolean wale array mai by default false hota hai and kisika jawab true or false aata hai so muje pta nhi chalega ki yeh by default wala false hai ya mera answer wala false hai so Boolean wala mai dekh skte ki not null rha means calculated hai
+	//Boolean dp[][]= new dp[n][target+1] ; kitne size ka? vertical mai index rakhenge aur horizontal size "target" rakhenge so kitne size ka banega? n+1 aur target+1, n size ka bhi row le skte ho  
+	//2 chize vary kr rhi thi, index aur target. index 0 se array.length-1 tak vary karega so 0 to n-1 'n' bande hote hai isliye vertical wale ko n size ka banaya, dusra vary karega 0 to target tak let's say 0 to 19 tak, 0 to 19 '20' bande honge isliye target+1 ka banaya
+	public static boolean tssM( int arr[], int idx, int target, Boolean dp[][]) { 
+		if( target == 0) {
+			return true;
+		}
+		
+		if( idx >= arr.length || target < 0) { //target -ve hogya to out of bound ajayega so woh basecace bhi dalo
+			return false;
+		}
+		
+		if( dp[idx][target] != null) { //agar woh null nhi hai means calculated hai
+			return dp[idx][target];
+		}
+		
+		boolean f1= tss(arr, idx+1, target); 
+		boolean f2= tss(arr, idx+1, target- arr[idx] ) ; 
+		boolean ans= f1 || f2 ; 
+		
+		dp[idx][target]= ans;
+		return ans;
+		
+	}
+	
 	public static void targetSumSubset(int arr[], int tar) {
 
 		//true or false store karege isliye boolean array banayenge

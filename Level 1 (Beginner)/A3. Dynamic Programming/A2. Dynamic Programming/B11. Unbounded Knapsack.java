eb6d99588c94b99bb6ebbefd74551136894e5186
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-21 12:34:50 +0530

    Update B11. Unbounded Knapsack.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/B11. Unbounded Knapsack.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/B11. Unbounded Knapsack.java
@@ -29,3 +29,65 @@ Sample Input
 7
 Sample Output
 100
+
+Code:
+
+package DP;
+
+import java.util.Scanner;
+
+public class unboundedKnapsach {
+	
+	public static int unboundedKnapsach(int vals[], int wts[], int cap) {
+		int n= vals.length;
+		
+		int dp[]= new int[cap+1]; // capacity se ek bana array banayenge, capacity 7 thi to 0 to 7 index lagega so 8 size ka banayenge
+		dp[0]= 0; //0 kilo ki bag mai kuch nhi ho skta
+		
+		for( int bagc= 1; bagc <= cap; bagc++) { //cap or dp.length likh skte ho
+			//ab hum sabhi items ko try karneki koshish karenge
+			int max=0; //max rkhenge ek
+			for( int i=0; i<n ; i++) {
+				if( wts[i] <= bagc) { //agar weight bag capacity se kam hai to hum isko use kr skte bag mai
+					
+					int rbagc= bagc- wts[i];// remaining bag capacity
+					int rbagv= dp[rbagc]; // reamining bag value
+					int tbagv= rbagv + vals[i];// total bag value
+					
+					if( tbagv > max) { //agar yeh total bag value tumhare max ko beat krti hai to max ko update krlo
+						max= tbagv;
+					}
+				} 
+			}
+			
+			dp[bagc]= max; //dp of bagcapacity mai yeh max dal denge
+			
+		}
+		
+		return dp[cap]; //last index humara capacity hai so woh return kar denge
+		
+	}
+
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		
+		int vals[]= new int[n];
+		
+		for(int i=0; i< vals.length; i++) {
+			vals[i]= s.nextInt();
+		}
+		
+		int wts[]= new int[n];
+		
+		for( int i=0; i< wts.length; i++) {
+			wts[i]= s.nextInt();
+		}
+		
+		int cap= s.nextInt();
+		int ans= unboundedKnapsach(vals, wts, cap);
+		System.out.println(ans);
+		
+	}
+}

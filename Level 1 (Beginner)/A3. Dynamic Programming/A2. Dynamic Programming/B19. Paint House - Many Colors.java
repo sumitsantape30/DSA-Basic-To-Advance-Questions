fc0bd76a3dca3fac361611439433961c4a6951e6
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-21 20:57:57 +0530

    Update B19. Paint House - Many Colors.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/B19. Paint House - Many Colors.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/B19. Paint House - Many Colors.java
@@ -24,3 +24,65 @@ Sample Input
 1 2 4
 Sample Output
 8
+
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int arr[][] = new int[n][m];
+        for(int i = 0; i < n; i++) {
+        	for(int j = 0; j < m; j++) {
+        		arr[i][j] = sc.nextInt();
+        	}
+        }
+        paint_house_many_colors_optimal(n,m,arr);
+        sc.close();
+    }
+
+private static void paint_house_many_colors_optimal(int n, int m, int[][] arr) {
+		int dp[][] = new int[n][m];
+		
+		int least = Integer.MAX_VALUE;
+		int sleast = Integer.MAX_VALUE;
+		
+		for(int j = 0; j < m; j++) {
+			dp[0][j] = arr[0][j];
+			
+			if(arr[0][j] <= least) {
+				sleast = least;
+				least = arr[0][j];
+			} else if(arr[0][j] <= sleast) {
+				sleast = arr[0][j];
+			}
+		}
+		
+		for(int i = 1; i < dp.length; i++) {
+			int nleast = Integer.MAX_VALUE;
+			int nsleast = Integer.MAX_VALUE;
+			for(int j = 0; j < dp[0].length; j++) {
+				if(least == dp[i-1][j]) {
+					dp[i][j] = sleast + arr[i][j];
+				} else {
+					dp[i][j] = least + arr[i][j];
+				}
+//				update least and sleast
+				if(dp[i][j] <= nleast) {
+					nsleast = nleast;
+					nleast = dp[i][j];
+				} else if(dp[i][j] <= nsleast) {
+					nsleast = dp[i][j];
+				}
+			}
+			least = nleast;
+			sleast = nsleast;
+		}
+		System.out.println(least);
+	}
+}

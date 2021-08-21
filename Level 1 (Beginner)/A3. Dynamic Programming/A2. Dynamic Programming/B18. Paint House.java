26736b965bfdf4a5cdde5864362552bd40c091e0
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-21 20:57:13 +0530

    Update B18. Paint House.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/B18. Paint House.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/B18. Paint House.java
@@ -23,3 +23,42 @@ Sample Input
 1 2 4
 Sample Output
 8
+
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		long[][] arr = new long[n][3];
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		paint_house(n, arr);
+		sc.close();
+	}
+
+	private static void paint_house(int n, long[][] arr) {
+		long dp[][] = new long[n][3];
+		
+		dp[0][0] = arr[0][0];
+		dp[0][1] = arr[0][1];
+		dp[0][2] = arr[0][2];
+		
+		for(int i = 1; i < n; i++) {
+			dp[i][0] = arr[i][0] + Math.min(dp[i-1][1],dp[i-1][2]);
+			dp[i][1] = arr[i][1] + Math.min(dp[i-1][0],dp[i-1][2]);
+			dp[i][2] = arr[i][2] + Math.min(dp[i-1][0],dp[i-1][1]);
+		}
+		
+		long ans = Math.min(Math.min(dp[n-1][0], dp[n-1][1]), dp[n-1][2]);
+		System.out.println(ans);
+	}
+}

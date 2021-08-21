8e53306e2e459a4f6a1b7b9f990c32ec0962a37d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-21 20:54:46 +0530

    Create C22. Tiling With M * 1 Tiles.java

---
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/C22. Tiling With M * 1 Tiles.java
@@ -0,0 +1,20 @@
+Tiling With M * 1 Tiles
+
+1. You are given a number n and a number m separated by line-break representing the length and breadth of a m * n floor.
+2. You've an infinite supply of m * 1 tiles.
+3. You are required to calculate and print the number of ways floor can be tiled using tiles.
+Input Format
+A number n
+A number m
+Output Format
+A number representing the number of ways in which the number of ways floor can be tiled using tiles.
+Question Video
+
+  COMMENTConstraints
+1 <= n <= 100
+1 <= m <= 50
+Sample Input
+39
+16
+Sample Output
+61

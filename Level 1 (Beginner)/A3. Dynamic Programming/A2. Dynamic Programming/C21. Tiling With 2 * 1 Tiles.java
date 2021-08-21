d3c8ddcccdfe700711938643f930f3f54b419b74
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-21 20:54:03 +0530

    Create C21. Tiling With 2 * 1 Tiles.java

---
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/C21. Tiling With 2 * 1 Tiles.java
@@ -0,0 +1,17 @@
+Tiling With 2 * 1 Tiles
+
+1. You are given a number n representing the length of a floor space which is 2m wide. It's a 2 * n board.
+2. You've an infinite supply of 2 * 1 tiles.
+3. You are required to calculate and print the number of ways floor can be tiled using tiles.
+Input Format
+A number n
+Output Format
+A number representing the number of ways in which the number of ways floor can be tiled using tiles.
+Question Video
+
+  COMMENTConstraints
+1 <= n <= 100
+Sample Input
+8
+Sample Output
+34

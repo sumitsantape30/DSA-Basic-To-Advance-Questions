029b8d5db4ec11ec328d258a901205461e4eca03
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-21 20:59:41 +0530

    Update C22. Tiling With M * 1 Tiles.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/C22. Tiling With M * 1 Tiles.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/C22. Tiling With M * 1 Tiles.java
@@ -18,3 +18,31 @@ Sample Input
 16
 Sample Output
 61
+
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+           Scanner sc = new Scanner(System.in);
+           int n = sc.nextInt();
+           int m = sc.nextInt();
+           
+           int dp[] = new int[n + 1];
+           
+           for(int i = 1; i <= n; i++) {
+        	   if(i < m) {
+        		   dp[i] = 1;
+        	   } else if(i == m) {
+        		   dp[i] = 2;
+        	   } else {
+        		   dp[i] = dp[i - 1] + dp[i - m];
+        	   }
+           }
+           System.out.println(dp[n]);
+           sc.close();
+    }
+}

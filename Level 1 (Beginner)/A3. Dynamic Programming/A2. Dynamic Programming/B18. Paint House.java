aa243614289f96aa6f83e38889de92280b105dc3
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-23 13:44:33 +0530

    Update B18. Paint House.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/B18. Paint House.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/B18. Paint House.java
@@ -25,40 +25,51 @@ Sample Output
 8
 
 Code:
+package DP;
 
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
+public class paintHouse {
+	
+	public static long paintHouse( long arr[][]) {
+		int n= arr.length;
+		
+		long dp[][]= new long[arr.length][3]; // 3 ki jagah arr[0].length bhi le skte hai
+		
+		dp[0][0]= arr[0][0];// first gharpe red color lagaya to 
+		dp[0][1]= arr[0][1] ;// first gharpe blue color lagaya to
+		dp[0][2]= arr[0][2]; //green lagaya to
+		
+		//ab hum 2nd gharse loop lagayenge aakhri ghar tak
+		for( int i=1; i< arr.length; i++) {
+			dp[i][0]= arr[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);// iss ith gharke andar agar red color lagana hai, iss gharka red wala color utha lo aur pichle gharke blue aur green mese jo minimum hoga 
+			dp[i][1]= arr[i][1] + Math.min(dp[i-1][0], dp[i-1][2]); // ab 1 pe hai to pichle ghar ka 0 or 2 lena hoga
+			dp[i][2]= arr[i][2] + Math.min(dp[i-1][0], dp[i-1][1]); //iss ghar se 2th color le rhe ho ab pichle ghar ke 0 or 1 uthaiye aur unka minimum lo
+		}
+		
+		//jo last wali row hai uska minimum answer hoga
+		return Math.min(dp[n-1][0], Math.min(dp[n-1][1], dp[n-1][2]));
+		
+	}
 
-    public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-
-		long[][] arr = new long[n][3];
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[0].length; j++) {
-				arr[i][j] = sc.nextInt();
+	public static void main(String[] args) {
+//long lenge coz constraints mai jada badi value de rakhi hai
+		
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt(); // number of houses
+		
+		long arr[][]= new long[n][3]; // 3 colors hai so 3
+		
+		for( int i=0 ;i < arr.length; i++) {
+			for( int j=0; j< arr[0].length; j++) {
+				arr[i][j]= s.nextInt();
 			}
 		}
-		paint_house(n, arr);
-		sc.close();
+		
+		long ans= paintHouse(arr);
+		System.out.println(ans);
+		
+		
 	}
 
-	private static void paint_house(int n, long[][] arr) {
-		long dp[][] = new long[n][3];
-		
-		dp[0][0] = arr[0][0];
-		dp[0][1] = arr[0][1];
-		dp[0][2] = arr[0][2];
-		
-		for(int i = 1; i < n; i++) {
-			dp[i][0] = arr[i][0] + Math.min(dp[i-1][1],dp[i-1][2]);
-			dp[i][1] = arr[i][1] + Math.min(dp[i-1][0],dp[i-1][2]);
-			dp[i][2] = arr[i][2] + Math.min(dp[i-1][0],dp[i-1][1]);
-		}
-		
-		long ans = Math.min(Math.min(dp[n-1][0], dp[n-1][1]), dp[n-1][2]);
-		System.out.println(ans);
-	}
 }

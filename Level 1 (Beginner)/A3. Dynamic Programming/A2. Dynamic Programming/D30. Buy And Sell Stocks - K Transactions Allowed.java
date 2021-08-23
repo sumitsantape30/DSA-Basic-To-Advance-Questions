fad1ff2b1678ac7413559b9a63c889d40abf9b18
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-23 19:06:23 +0530

    Update and rename D30. to D30. Buy And Sell Stocks - K Transactions Allowed.java

---
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/D30. Buy And Sell Stocks - K Transactions Allowed.java
@@ -0,0 +1,33 @@
+Buy And Sell Stocks - K Transactions Allowed
+
+1. You are given a number n, representing the number of days.
+2. You are given n numbers, where ith number represents price of stock on ith day.
+3. You are given a number k, representing the number of transactions allowed.
+3. You are required to print the maximum profit you can make if you are allowed k transactions at-most.
+Note - There can be no overlapping transaction. One transaction needs to be closed (a buy followed by a sell) before opening another transaction (another buy).
+Input Format
+A number n
+.. n more elements
+A number k
+Output Format
+A number representing the maximum profit you can make if you are allowed a single transaction.
+Question Video
+
+  COMMENTConstraints
+0 <= n <= 20
+0 <= n1, n2, .. <= 10
+0 <= k <= n / 2
+Sample Input
+6
+9
+6
+7
+6
+3
+8
+1
+
+Sample Output
+5
+
+Code:

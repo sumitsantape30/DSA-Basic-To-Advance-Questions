1fd54ac415ef02a68f0f438e8128eace4cae47e5
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-23 19:03:27 +0530

    Create C27. Buy And Sell Stocks With Transaction Fee - Infinite Transactions Allowed.java

---
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/C27. Buy And Sell Stocks With Transaction Fee - Infinite Transactions Allowed.java
@@ -0,0 +1,36 @@
+Buy And Sell Stocks With Transaction Fee - Infinite Transactions Allowed
+
+1. You are given a number n, representing the number of days.
+2. You are given n numbers, where ith number represents price of stock on ith day.
+3. You are give a number fee, representing the transaction fee for every transaction.
+4. You are required to print the maximum profit you can make if you are allowed infinite transactions, but has to pay "fee" for every closed transaction.
+Note - There can be no overlapping transaction. One transaction needs to be closed (a buy followed by a sell) before opening another transaction (another buy).
+Input Format
+A number n
+.. n more elements
+A number fee
+Output Format
+A number representing the maximum profit you can make if you are allowed infinite transactions with transaction fee.
+Question Video
+
+  COMMENTConstraints
+0 <= n <= 20
+0 <= n1, n2, .. <= 10
+0 <= fee <= 5
+Sample Input
+12
+10
+15
+17
+20
+16
+18
+22
+20
+22
+20
+23
+25
+3
+Sample Output
+13

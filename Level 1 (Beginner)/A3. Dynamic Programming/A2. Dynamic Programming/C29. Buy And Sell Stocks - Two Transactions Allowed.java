d4e33483316803564bea29dc9c6c262885731532
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-25 08:49:37 +0530

    Update C29. Buy And Sell Stocks - Two Transactions Allowed.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/C29. Buy And Sell Stocks - Two Transactions Allowed.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/C29. Buy And Sell Stocks - Two Transactions Allowed.java
@@ -29,3 +29,56 @@ Sample Output
 30
 
 Code:
+
+package DP;
+
+import java.util.Scanner;
+
+public class BuyAndSellStocksTwoTransactionsAllowed {
+	
+	//Hint: single transaction ke 2 methods sikhe, left se min banate chalo ki aaj ke din bechenge. Ya fir right se max ki aaj ke din kharidenge. So inn dono method ka combination hai
+	//dusri approch mai mai har ek point ko buying point man rha hu aur pehle approach mai selling point man rhe hai ki aaj ke din bechenge
+	
+	public static int buy_and_sell_2_transaction(int n, int arr[]) {
+		int minsf = arr[0]; // min so far ko array ke 0th element se initialise kr diya
+		int maxsf = arr[n-1]; 
+		
+		int dp1[] = new int[n];
+		int dp2[] = new int[n];
+		
+		//dp1[i]: max profit by doing at most one transaction till ith index
+		for(int i = 1; i < n; i++) { //left to right fill karenge isko 
+			minsf = Math.min(minsf, arr[i]); //aatehi sabse pehle min ko update krlo
+			dp1[i] = Math.max(dp1[i - 1], arr[i] - minsf); //fir dp mai dal denge: aaj agar humne isko bach diya to aajke pehle din kitna profit tha aur aaj kitna profit hota, max wala dal denge
+		}
+		
+		// dp2[i] : max profit by doing at most one transaction from ith index to last index 
+		//2nd last se start krdo
+		for(int i = n-2; i >= 0; i--) { //right to left
+			maxsf = Math.max(maxsf, arr[i]);
+			dp2[i] = Math.max(dp2[i + 1], maxsf - arr[i]  ); // maxsf pe bechenge aur arr[i] pe means aaj kharidenge
+		}
+		
+		int maxprofit = 0;
+		for (int i = 0; i < n; i++) {
+			maxprofit = Math.max(maxprofit, dp1[i] + dp2[i]);
+		}
+		
+		return maxprofit;
+	}
+	
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int arr[]= new int[n];
+		
+		for( int i=0; i< arr.length; i++) {
+			arr[i]= s.nextInt();
+		}
+		
+		System.out.println(buy_and_sell_2_transaction(n, arr));
+		
+	}
+
+}

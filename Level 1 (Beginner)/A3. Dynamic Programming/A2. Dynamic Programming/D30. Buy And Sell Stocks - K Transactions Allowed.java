b0077d683b57163ddc974985da2053bcaa387798
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-25 10:08:44 +0530

    Update D30. Buy And Sell Stocks - K Transactions Allowed.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/D30. Buy And Sell Stocks - K Transactions Allowed.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/D30. Buy And Sell Stocks - K Transactions Allowed.java
@@ -31,3 +31,50 @@ Sample Output
 5
 
 Code:
+
+package DP;
+
+import java.util.Scanner;
+
+public class BuyAndSellStocksKTransactionsAllowed {
+	
+	private static int ktransactionsAllowedOptimized(int n, int[] arr, int k) {
+	    int dp[][] = new int[k + 1][n];
+	    for (int t = 1 ; t <= k ; t++ ) {
+	      int max = Integer.MIN_VALUE;
+	      for (int d = 1 ; d < n ; d++ ) {
+
+	        if ( dp[t - 1][d - 1] - arr[d - 1] > max ) {
+	          max = dp[t - 1][d - 1] - arr[d - 1];
+	        }
+
+	        if ( max + arr[d] > dp[t][d - 1]) {
+	          dp[t][d] =  max + arr[d];
+	        } else {
+	          dp[t][d] =  dp[t][d - 1];
+	        }
+
+	      }
+	    }
+	    return dp[k][n - 1];
+	  }
+	
+	//T.C- > n.k.n -> o(n2k) hogi iski
+
+	public static void main(String[] args) {
+		
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		int arr[]= new int[n];
+		
+		for( int i=0; i< arr.length; i++) {
+			arr[i]= s.nextInt();
+		}
+
+		int k= s.nextInt();
+		
+		System.out.println(ktransactionsAllowedOptimized(n, arr, k));
+		
+	}
+
+}

4e37abdc377ec0f60992b0c746f870afd0b91f7d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-08-26 08:05:48 +0530

    Update C28. Buy And Sell Stocks With Cooldown - Infinite Transaction Allowed.java

--- Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/C28. Buy And Sell Stocks With Cooldown - Infinite Transaction Allowed.java
+++ Level 1 (Beginner)/A3. Dynamic Programming/A2. Dynamic Programming/C28. Buy And Sell Stocks With Cooldown - Infinite Transaction Allowed.java
@@ -33,3 +33,48 @@ Sample Output
 19
 
 Code:
+
+package DP;
+
+import java.util.Scanner;
+
+public class BuyAndSellStocksWithCooldownInfiniteTransactionAllowed {
+	
+	public static int BuyAndSellStocksWithCooldownInfiniteTransactionAllowed(int arr[] ) {
+		
+		int buy[]= new int[arr.length];
+		int sell[]= new int[arr.length];
+		
+		buy[0]= -arr[0];
+		sell[0]= 0 ;
+		
+		for( int i=1; i< arr.length; i++) {
+			if( i == 1) {//aagr i ki value 1 hai to buy[i] pe -arr[i] aur bouy[0] ka max store kardo
+				buy[i]= Math.max(buy[0], -arr[i]);
+			}else {
+				buy[i]= Math.max(buy[i-1], sell[i-2] - arr[i]);
+			}
+			
+			sell[i]= Math.max(sell[i-1], buy[i-1] + arr[i]);
+		}
+		return sell[arr.length-1];
+	}
+	
+
+	public static void main(String[] args) {
+
+		Scanner s= new Scanner(System.in);
+		int n= s.nextInt();
+		
+		int arr[]= new int[n];
+		
+		for( int i=0; i< arr.length; i++) {
+			arr[i]= s.nextInt();
+		}
+		
+		int ans= BuyAndSellStocksWithCooldownInfiniteTransactionAllowed(arr);
+		System.out.println(ans);
+		
+	}
+
+}

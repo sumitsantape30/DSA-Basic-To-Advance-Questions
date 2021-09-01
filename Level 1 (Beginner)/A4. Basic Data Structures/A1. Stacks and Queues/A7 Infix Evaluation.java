8493b67e3760aa2aff98992b36d477de0c5fff7b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-01 08:02:04 +0530

    Update A7 Infix Evaluation.java

--- Level 1 (Beginner)/A4. Basic Data Structures/A1. Stacks and Queues/A7 Infix Evaluation.java
+++ Level 1 (Beginner)/A4. Basic Data Structures/A1. Stacks and Queues/A7 Infix Evaluation.java
@@ -31,7 +31,7 @@ public class InfixEvaluation {
 	public static int precedence( char optor) { //yeh function operator ki precedence dega
 		if( optor== '+' || optor== '-') {
 			return 1;
-		}else if( optor == '*' || '/') {
+		}else if( optor == '*' || optor== '/') {
 			return 2;
 		}else { 
 			return 0;
@@ -54,72 +54,74 @@ public class InfixEvaluation {
 	public static void infixEva( String exp) {
 		
 		//2 stacks banani padegi operands and operators ki
-		Stack<Integer> opnds= new Stack<Integer>();
-		Stack<Character> optors= new Stack<Character>();
+		Stack<Integer> st1= new Stack<Integer>();
+		Stack<Character> st2= new Stack<Character>();
 		
+		//fir string ke upar loop lagayenge
 		for( int i=0; i< exp.length(); i++) {
 			char ch= exp.charAt(i);
 			
-			//woh character ab kya kya ho skta hai
-			if( ch == '(') { //woh character opening bracket h skta hai
-				//agar woh opening bracket hai to operators stack mai mai push kardo
-				optors.push(ch);
-				
-			}else if(Character.isDigit(ch)) { //yafir woh character koi digit ho skta hai, digit ko aise check krte hai
+			 if(Character.isDigit(ch)) { //yafir woh character koi digit ho skta hai, digit ko aise check krte hai. // if( ch >= '0' && ch <= '9') yeh bhi condition use kr skte ho
 				//agar woh digit hai to operands mai push kardo but usko character se number mai convert karna
-				opnds.push(ch - '0');
-				
-			}else if( ch == ')') { //yafir yeh ek closing bracket bhi ho skta hai
-				//closing brack hai to tabtak pop hoga jabtak ek opening brack nazar na ajaye aur fir ek aur pop karenge coz opening bracket kobhi nikalna hai
-				while( optors.peek() != '(') {
-					//jab operators mai ek pop krte hai to operands mai do pop krte hai
-					char optor= optors.pop();
-					int v2= opnds.pop(); // jo pehle niikala uska nam v2 rakha
-					int v1= opnds.pop(); //jo badme nikala uska nam v1 rakha
-					
-					//ab hum operation karke value mangayenge
-					int opv = operation(v1, v2, optor); // v1 v2 and opertor pass karenge
-					//aur operands mai push karenge operation ki value
-					opnds.push(opv);
-				}
-				
-				//ek aur pop karenge coz opening bracket kobhi nikalna hai
-				optors.pop();
+				st1.push(ch - '0');
+			
+			 //ab woh character ab kya kya ho skta hai
+			}else if( ch == '(') { //woh character opening bracket ho skta hai
+				//agar woh opening bracket hai to operators stack mai mai push kardo
+				st2.push(ch);
 				
 			}else if(ch == '+' || ch== '-' || ch =='/' || ch=='*') { // yafir yeh ek operator ho skta hai
 				//ch is wanting higher priority operators to solve first
-				
+					
+				      // agar stack ki top pe merese equal or jada precidence ka banda hai to usko solve karunga tabhi mai jaunga
 					//isme tabtak pop hota hai jabtak size grater than 0 hai aur peek pr opening bracket nhi hai, yafir humko unko pop krte hai jo precedence mai greater than equal hai
-					while(optors.size() > 0 && optors.peek() != '(' && precedence(ch) <= precedence(optors.peek())) { //peek wale ki priority badi ya barbar aapse(ch) hai to pop hota rahega
+					while(st2.size() > 0 && precedence(st2.peek()) >= precedence(ch)) { //peek wale ki priority badi ya barbar aapse(ch) hai to pop hota rahega
 						
 						// aur bichme ab wahi kam karenge jo upar kiye the
-						if( optors.peek() != '(') {
-							char optor= optors.pop();
-							int v2= opnds.pop(); 
-							int v1= opnds.pop(); 
+							char optor= st2.pop();
+							int v2= st1.pop(); 
+							int v1= st1.pop(); 
 							
-							int opv = operation(v1, v2, optor); 
-							opnds.push(opv);
+							int res = operation(v1, v2, optor); 
+							st1.push(res);
 						}
 						//aur finally jo aapka apna operator hai usko push karna na bhule, woh keh rha ki mujse badi priority walo ko pehle process karadu then ch will push itself
-						optors.push(ch);
+						st2.push(ch);
+			}else if( ch == ')') { //yafir yeh ek closing bracket bhi ho skta hai, closing bracket mila to tabtak solve karenge jabtak opening bracket nhi mil jata
+				//closing brack hai to tabtak pop hoga jabtak ek opening brack nazar na ajaye aur fir ek aur pop karenge coz opening bracket kobhi nikalna hai
+				while( st2.peek() != '(') { // jatab stack2 ke top pe opening nhi ajata
+					//jab operators mai ek pop krte hai to operands mai do pop krte hai
+					char optor= st2.pop();
+					int v2= st1.pop(); // jo pehle niikala uska nam v2 rakha
+					int v1= st1.pop(); //jo badme nikala uska nam v1 rakha
+					
+					//ab hum operation karke value mangayenge
+					int res = operation(v1, v2, optor); // v1 v2 and opertor pass karenge
+					//aur operands mai push karenge operation ki value
+					st1.push(res);
 				}
-			}
-			
-			//yeh sab hoone ke bad bhi kuch operator bach skte hai to unko bhi handle karna padega
-			while( optors.size() != 0) {
-				char optor= optors.pop();
-				int v2= opnds.pop(); 
-				int v1= opnds.pop(); 
 				
-				int opv = operation(v1, v2, optor); 
-				opnds.push(opv);
+				//ek aur pop karenge coz opening bracket kobhi nikalna hai
+				st2.pop(); //pop the opening bracket
+				
+			}
+		}
+			
+			//last mai ho skta hai ki kuch operators stack mai reh jaye
+			//yeh sab hoone ke bad bhi kuch operator bach skte hai to unko bhi handle karna padega
+			while( st2.size() != 0) {
+				char optor= st2.pop();
+				int v2= st1.pop(); 
+				int v1= st1.pop(); 
+				
+				int res = operation(v1, v2, optor); 
+				st1.push(res);
 			}
 			
 			//aur finally jo operands stack mai aakhri calculated banda bachega woh humara answer hoga
-			System.out.println(opnds.peek());
-		}
-	}
+			System.out.println(st1.peek());
+			//System.out.println(st1.pop()); bhi use kr skte ho
+	       }
 
 	public static void main(String[] args) {
 

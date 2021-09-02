00f5ba07665725b62ea8fee946d34f5e1286535d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-02 09:29:03 +0530

    Update B18. Dynamic Stack.java

--- Level 1 (Beginner)/A4. Basic Data Structures/A1. Stacks and Queues/B18. Dynamic Stack.java
+++ Level 1 (Beginner)/A4. Basic Data Structures/A1. Stacks and Queues/B18. Dynamic Stack.java
@@ -107,10 +107,9 @@ public class DynamicStack {
 		    void push(int val) {
 		      if (tos == data.length - 1) {
 		    	  doubleCapacity();
-		      } else {
+		      } //array ka size badh jayega fir index increase hoke array mai value store hojayegi isliye else hata do
 		        tos++;
 		        data[tos] = val;
-		      }
 		    }
 		    
 		    public void doubleCapacity() {
@@ -171,3 +170,102 @@ public class DynamicStack {
 		    }
 		  }
 		}
+
+//===========================Submitted Code===============================================================
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class CustomStack {
+    int[] data;
+    int tos;
+
+    public CustomStack(int cap) {
+      data = new int[cap];
+      tos = -1;
+    }
+
+    int size() {
+      // write ur code here
+      	return tos+1;
+      	
+    }
+
+    void display() {
+      // write ur code here
+      for(int i= tos; i>= 0; i--) {
+		    		System.out.print(data[i]+" ");
+		 }
+		    	
+	  System.out.println();
+    }
+
+    void push(int val) {
+      // write ur code here
+//       if( size()== data.length) {
+// 		    		System.out.println("Stack Overflow");
+// 		}
+		if( tos == data.length-1 ) {
+		    		int oa[]= data;
+		    		data= new int[2*oa.length];
+		    		for( int i=0; i< oa.length; i++){
+		    		    data[i]= oa[i];
+		    		}
+		}
+		    	tos++;
+		    	data[tos]= val;
+    }
+
+    int pop() {
+      // write ur code here
+      if( size() == 0) {
+		    		System.out.println("Stack underflow");
+		    		return -1;
+		    	}
+		    	
+		    	int temp= data[tos];
+		    	tos--;
+		    	return temp;
+    }
+
+    int top() {
+       // write ur code here
+       if( size() == 0) {
+		    		System.out.println("Stack underflow");
+		    		return -1;
+		    	}
+		    return data[tos];
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    CustomStack st = new CustomStack(n);
+
+    String str = br.readLine();
+    while(str.equals("quit") == false){
+      if(str.startsWith("push")){
+        int val = Integer.parseInt(str.split(" ")[1]);
+        st.push(val);
+      } else if(str.startsWith("pop")){
+        int val = st.pop();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("top")){
+        int val = st.top();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("size")){
+        System.out.println(st.size());
+      } else if(str.startsWith("display")){
+        st.display();
+      }
+      str = br.readLine();
+    }
+  }
+}

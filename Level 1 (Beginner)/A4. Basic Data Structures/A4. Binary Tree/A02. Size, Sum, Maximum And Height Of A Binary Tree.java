9f216dc0e29761daef588d80582441c83c958978
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-24 11:47:30 +0530

    Update A02. Size, Sum, Maximum And Height Of A Binary Tree.java

--- Level 1 (Beginner)/A4. Basic Data Structures/A4. Binary Tree/A02. Size, Sum, Maximum And Height Of A Binary Tree.java
+++ Level 1 (Beginner)/A4. Basic Data Structures/A4. Binary Tree/A02. Size, Sum, Maximum And Height Of A Binary Tree.java
@@ -25,4 +25,94 @@ Sample Output
 
 Code:
 
+import java.io.*;
+import java.util.*;
 
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+public static int size(Node node) {
+      if(node == null){
+          return 0;
+     }
+    
+    int leftSize= size(node.left);
+    int rightSize= size(node.right);
+    
+    int ans = leftSize + rightSize + 1;
+    return ans;
+  }
+
+  public static int sum(Node node) {
+      if( node == null){
+          return 0;
+      }
+    int lsum= sum(node.left);
+    int rsum= sum(node.right);
+    
+    int sum= lsum + rsum + node.data;
+    
+    return sum;
+    
+  }
+
+  public static int max(Node node) {
+      if( node == null){ //agar node null hai to, agar 0 return karoge to galat hoga agar sari values -ve rhi to isliye -infinity return karna hoga
+          return Integer.MIN_VALUE;
+      }
+      
+      int lMax= max(node.left);
+      int rMax= max(node.right);
+      
+      int max= Math.max(node.data, Math.max(lMax, rMax));
+      return max;
+  }
+
+  public static int height(Node node) { //height chahiye in terms of edges
+      if( node  == null){
+          return -1; //-1 return karenge
+      }
+    int lH= height(node.left);
+    int rH= height(node.right);
+    
+    int height= Math.max(lH, rH) + 1;
+    return height;
+    
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    Node root = construct(arr);
+
+    int size = size(root);
+    int sum = sum(root);
+    int max = max(root);
+    int ht = height(root);
+    System.out.println(size);
+    System.out.println(sum);
+    System.out.println(max);
+    System.out.println(ht);
+  }
+
+}

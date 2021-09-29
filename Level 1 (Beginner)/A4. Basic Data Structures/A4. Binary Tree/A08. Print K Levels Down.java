e9c57df7db280429df93af2cfc93ef5459b1fd3b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-09-29 09:06:15 +0530

    Update A08. Print K Levels Down.java

--- Level 1 (Beginner)/A4. Basic Data Structures/A4. Binary Tree/A08. Print K Levels Down.java
+++ Level 1 (Beginner)/A4. Basic Data Structures/A4. Binary Tree/A08. Print K Levels Down.java
@@ -21,3 +21,119 @@ Sample Output
 70
 
 Code:
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+  public static void printKLevelsDown(Node node, int k){
+      if( node == null || k < 0){ //agar node null hojaye or k negative ho to return
+        return;
+      }
+      
+      if( k == 0){
+          System.out.println(node.data); //agar k==0 hai to 0th level pe root hi hai to usko print kardenge
+      }
+    
+    printKLevelsDown(node.left, k-1); //root se k levels deep hai to left wale node se k-1 levels deep hoga
+    printKLevelsDown(node.right, k-1); //similarly right wale node sebhi k-1 levels deep hoga
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    int k = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    printKLevelsDown(root, k);
+  }
+
+}

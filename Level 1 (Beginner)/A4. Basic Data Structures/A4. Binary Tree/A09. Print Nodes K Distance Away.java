bcb7e78eb1cdc619337cc4af066666ce22b88d46
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-01 08:22:02 +0530

    Update A09. Print Nodes K Distance Away.java

--- Level 1 (Beginner)/A4. Basic Data Structures/A4. Binary Tree/A09. Print Nodes K Distance Away.java
+++ Level 1 (Beginner)/A4. Basic Data Structures/A4. Binary Tree/A09. Print Nodes K Distance Away.java
@@ -140,12 +140,171 @@ public class Main {
   }
   
   public static void printKLevelsDown(Node node, int k, int blocker){
-      if( node == null || k < 0 || node == blocker){ 
+      if( node == null || k < 0 || node == blocker){  //so maine bola node agar null hai to return aur agar node blocker node hai tobhi return
         return;
       }
       
       if( k == 0){
           System.out.println(node.data); 
+          return;
+      }
+    
+     printKLevelsDown(node.left, k-1, blocker); 
+     printKLevelsDown(node.right, k-1, blocker); 
+     
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    Integer[] arr = new Integer[n];
+    String[] values = br.readLine().split(" ");
+    for (int i = 0; i < n; i++) {
+      if (values[i].equals("n") == false) {
+        arr[i] = Integer.parseInt(values[i]);
+      } else {
+        arr[i] = null;
+      }
+    }
+
+    int data = Integer.parseInt(br.readLine());
+    int k = Integer.parseInt(br.readLine());
+
+    Node root = construct(arr);
+    printKNodesFar(root, data, k);
+  }
+
+}
+
+///=====================================================JB's====================================
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data, Node left, Node right) {
+      this.data = data;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public static class Pair {
+    Node node;
+    int state;
+
+    Pair(Node node, int state) {
+      this.node = node;
+      this.state = state;
+    }
+  }
+
+  public static Node construct(Integer[] arr) {
+    Node root = new Node(arr[0], null, null);
+    Pair rtp = new Pair(root, 1);
+
+    Stack<Pair> st = new Stack<>();
+    st.push(rtp);
+
+    int idx = 0;
+    while (st.size() > 0) {
+      Pair top = st.peek();
+      if (top.state == 1) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.left = new Node(arr[idx], null, null);
+          Pair lp = new Pair(top.node.left, 1);
+          st.push(lp);
+        } else {
+          top.node.left = null;
+        }
+
+        top.state++;
+      } else if (top.state == 2) {
+        idx++;
+        if (arr[idx] != null) {
+          top.node.right = new Node(arr[idx], null, null);
+          Pair rp = new Pair(top.node.right, 1);
+          st.push(rp);
+        } else {
+          top.node.right = null;
+        }
+
+        top.state++;
+      } else {
+        st.pop();
+      }
+    }
+
+    return root;
+  }
+
+  public static void display(Node node) {
+    if (node == null) {
+      return;
+    }
+
+    String str = "";
+    str += node.left == null ? "." : node.left.data + "";
+    str += " <- " + node.data + " -> ";
+    str += node.right == null ? "." : node.right.data + "";
+    System.out.println(str);
+
+    display(node.left);
+    display(node.right);
+  }
+
+  public static void printKNodesFar(Node node, int data, int k) {
+      ArrayList<Integer> al= nodeToRootPath(node, data); //data ka root to root path ajayega
+      
+      for( int i=0; i< al.size(); i++){
+          Node cr = al.get(i); // current node
+          Node blocker = i == 0 ? null : al.get(i-1) ; // current node keliye yeh blocker node banegi. jab i ==0 rahegi tab ustime blocker indexoutofbound dega isliye jab i ==0 hoga to blocker null lelo.
+
+          printKLevelsDown(cr, k - i , blocker);
+      }
+  }
+  
+   public static ArrayList<Node> nodeToRootPath(Node node, int data){
+      
+      if( node == null){ 
+          return new ArrayList<Node>();
+      }
+      
+      if(node.data == data){
+          ArrayList<Node> bans= new ArrayList<Node>();
+          bans.add(node);
+          return bans;
+      }
+      
+      ArrayList<Node> lans= nodeToRootPath(node.left, data);
+      if(lans.size()> 0){ 
+         lans.add(node);
+         return lans;
+      }
+      
+      ArrayList<Integer> rans= nodeToRootPath(node.right, data);
+      if(rans.size()> 0){
+          rans.add(node);
+          return rans;
+      }
+      
+      return new ArrayList<>();
+  }
+  
+  public static void printKLevelsDown(Node node, int k, int blocker){
+      if( node == null || node == blocker){ 
+        return;
+      }
+      
+      if( k == 0){
+          System.out.println(node.data); 
+          return;
       }
     
      printKLevelsDown(node.left, k-1, blocker); 

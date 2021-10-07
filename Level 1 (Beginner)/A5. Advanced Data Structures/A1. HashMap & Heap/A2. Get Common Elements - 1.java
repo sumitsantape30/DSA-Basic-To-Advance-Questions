27ce393ebb5ef4e8812ec8b31cb09fa9529de296
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-07 08:02:23 +0530

    Update A2. Get Common Elements - 1.java

--- Level 1 (Beginner)/A5. Advanced Data Structures/A1. HashMap & Heap/A2. Get Common Elements - 1.java
+++ Level 1 (Beginner)/A5. Advanced Data Structures/A1. HashMap & Heap/A2. Get Common Elements - 1.java
@@ -55,4 +55,45 @@ Sample Output
 5
 8
 
-CodeL
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+
+    Scanner s= new Scanner(System.in);
+    int n1=s.nextInt();
+    int a1[]= new int[n1];
+    for( int i=0; i< n1; i++){
+        a1[i]= s.nextInt();
+    }
+
+    int n2= s.nextInt();
+    int a2[]= new int[n2];
+    
+    for( int i=0 ; i< n2; i++){
+        a2[i]= s.nextInt();
+    }
+    
+    HashMap<Integer, Integer> map= new HashMap<>();
+    for( int i=0; i< n1; i++){
+        if( map.containsKey(a1[i])){
+            map.put(a1[i], map.get(a1[i]) + 1);
+        }else{
+            map.put(a1[i], 1);
+        }
+    }
+    
+    for( int i=0; i< n2; i++){
+        if( map.containsKey(a2[i])){
+            System.out.println(a2[i]);
+            map.remove(a2[i]);
+        }
+    }
+
+ }
+
+}

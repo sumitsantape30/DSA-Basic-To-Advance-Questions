e4ae4f56c2e1e0e0ad595b8900b9d2e6750ed902
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-07 07:16:47 +0530

    Update A1. Highest Frequency Character.java

--- Level 1 (Beginner)/A5. Advanced Data Structures/A1. HashMap & Heap/A1. Highest Frequency Character.java
+++ Level 1 (Beginner)/A5. Advanced Data Structures/A1. HashMap & Heap/A1. Highest Frequency Character.java
@@ -16,3 +16,36 @@ Sample Output
 q
 
 Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn= new Scanner(System.in);
+        String str= scn.nextLine();
+        HashMap<Character, Integer> map= new HashMap<>();
+        int maxfreq=0; //max frquency ko maine pehle 0 leliya
+        char mfc= str.charAt(0); //max frquency character ko maine 0 wala leliye
+        
+        for( int i=0; i< str.length(); i++){
+            char ch= str.charAt(i);
+            
+            if( map.containsKey(ch) == true){ //agar woh character already present hai to bas uski value badhayenge
+                map.put(ch, map.get(ch)+1 );
+            }else{
+                //agar woh pehli bar aya hai to uske value 1 rakhdo
+                map.put(ch, 1);
+            }
+            
+            if( map.get(ch) >  maxfreq){ //agar uss character ki frequency max frequency se badi hojati hai to maxfrequency ko update kardo aur sathmne maxfrquency character bhi update krdo
+             maxfreq= map.get(ch);
+             mfc= ch;
+            }
+        }
+        System.out.println(mfc);
+    }
+    //TC: O(n), n is the length of the string
+
+}

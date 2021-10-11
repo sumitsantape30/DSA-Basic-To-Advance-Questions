abb0c88a651f4fe8286af3aea5819ff85198c5fa
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-11 16:28:55 +0530

    Update B15. Gcd And Lcm.java

--- Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/B15. Gcd And Lcm.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/B15. Gcd And Lcm.java
@@ -59,6 +59,29 @@ public class GCDAndLCM {
 		System.out.println(lcm);
 		
 	}
+	
+	//accepted solution
+	 Scanner s= new Scanner(System.in);
+		int n1= s.nextInt();
+		int n2= s.nextInt();
+		
+	
+		
+		int div= n1;
+		int dvd= n2;
+		
+		while( dvd % div != 0) {
+			int rem= dvd % div;
+			dvd= div;
+			div= rem;
+		}
+		
+		int gcd= div;
+		int lcm= (n1 * n2)/gcd;
+		
+		System.out.println(gcd);
+		System.out.println(lcm);
+     }
 
 	public static void main(String[] args) {
 

18a1a50fff3b4111349411f2ccc5b69be583ccf8
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-22 08:47:28 +0530

    Update A09. Breadth First Traversal.java

--- Level 1 (Beginner)/A5. Advanced Data Structures/A2. Graphs/A09. Breadth First Traversal.java
+++ Level 1 (Beginner)/A5. Advanced Data Structures/A2. Graphs/A09. Breadth First Traversal.java
@@ -34,3 +34,79 @@ Sample Output
 6@2346
 
 Code:
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+
+      Edge(int src, int nbr) {
+         this.src = src;
+         this.nbr = nbr;
+      }
+   }
+   
+   public static class Pair{
+       int vtx;
+       String psf;
+      //isme weight ki jarurat nhi hai 
+       
+       public Pair(int vtx, String psf){
+           this.vtx= vtx;
+           this.psf= psf;
+       }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         graph[v1].add(new Edge(v1, v2));
+         graph[v2].add(new Edge(v2, v1));
+      }
+
+      int src = Integer.parseInt(br.readLine());
+
+      Queue<Pair> q= new ArrayDeque<>(); // hume ek pairs ki queue chahiye
+      q.add(new Pair(src, src + "")); // hume ek pair add karenge jiska vertex hoga 0 aur pathsofar mai add hoga source
+      boolean visited[]= new boolean[graph.length];
+      
+      while( q.size() != 0){
+          
+          Pair rp= q.remove();
+          
+          //remove karne ke bad agar removed pair ka vertex already visited nhi hai to kuch kam karna hai
+          if(visited[rp.vtx] == false){
+              //so sabse pehle usko visited mark krdo aur print kardo
+              visited[rp.vtx] = true;
+              System.out.println(rp.vtx + "@" + rp.psf);
+              
+              //ab uske unvisited neighbour ko queue mai add karna hai
+              for( Edge e: graph[rp.vtx]){
+                  int nbr = e.nbr; //removed pair ke vertex nikale
+                   
+                   if( visited[nbr] == false){ //agar woh neighbour unvisited hai to usko queue mai add kardo
+                       q.add(new Pair(nbr, rp.psf + nbr)); // removed pair ke psf mai iss neighbour ko add kar denge
+                   }
+                  
+              }
+          }
+          
+      }
+       
+   }
+}

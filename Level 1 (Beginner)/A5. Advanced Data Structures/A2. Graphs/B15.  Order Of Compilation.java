7f50d3a9428ac38b38b296a0bddca79d5c12745d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-23 10:01:12 +0530

    Create B15.  Order Of Compilation.java

---
+++ Level 1 (Beginner)/A5. Advanced Data Structures/A2. Graphs/B15.  Order Of Compilation.java
@@ -0,0 +1,102 @@
+Order Of Compilation
+
+1. You are given a directed acyclic graph. The vertices represent tasks and edges represent 
+    dependencies between tasks.
+2. You are required to find and print the order in which tasks could be done. The task that should be 
+    done at last should be printed first and the task which should be done first should be printed last. 
+    This is called topological sort. Check out the question video for details.
+
+Topological sort -> A permutation of vertices for a directed acyclic graph is called topological sort if 
+                                    for all directed edges uv, u appears before v in the graph
+
+Note -> For output, check the sample output and question video.
+Input Format
+Input has been managed for you
+Output Format
+Check the sample output
+
+Constraints
+None
+Sample Input
+7
+7
+0 1
+1 2
+2 3
+0 3
+4 5
+5 6
+4 6
+Sample Output
+4
+5
+6
+0
+1
+2
+3
+
+
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+
+      Edge(int src, int nbr) {
+         this.src = src;
+         this.nbr = nbr;
+      }
+   }
+
+   public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int vtces = Integer.parseInt(br.readLine());
+    ArrayList< Edge>[] graph = new ArrayList[vtces];
+    for (int i = 0; i < vtces; i++) {
+      graph[i] = new ArrayList<>();
+    }
+
+    int edges = Integer.parseInt(br.readLine());
+    for (int i = 0; i < edges; i++) {
+      String[] parts = br.readLine().split(" ");
+      int v1 = Integer.parseInt(parts[0]);
+      int v2 = Integer.parseInt(parts[1]);
+      graph[v1].add(new Edge(v1, v2));
+    }
+    
+    //DFS se stack bharwake lani hai
+    
+    boolean[] visited = new boolean[graph.length];
+    Stack<Integer> st = new Stack<>();
+    
+    for (int i = 0; i< visited.length; i++) {
+      if(visited[i] == false) { 
+        dfs(graph, i, visited, st); // is function se puri stack bharke ajayegi
+      }
+    }
+
+    while (st.size() > 0) { //jabtak stack khali nhi hota remove karke print karte rahenge
+      System.out.println(st.pop());
+    }
+  }
+
+  public static void dfs(ArrayList<Edge>[] graph, int src, boolean[] visited, Stack<Integer> st) {
+    
+    //aatehi visited mark krdo
+    visited[src] = true;
+    for (Edge e : graph[src]) {
+        int nbr= e.nbr;
+        if (visited[nbr] == false) { //agar woh unvisited nbr hai to usko call lagni chahiye
+          dfs(graph, nbr, visited, st);
+      }
+    }
+    //wapas jane se pehle usko stack mai push krdo
+    st.push(src);
+  }
+}

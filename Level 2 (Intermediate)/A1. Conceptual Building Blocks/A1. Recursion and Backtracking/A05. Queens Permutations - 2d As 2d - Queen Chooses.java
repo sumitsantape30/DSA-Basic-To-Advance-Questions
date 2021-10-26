7bca9f8c57c2da2693736bf066f0c8ab5558f696
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-26 20:58:41 +0530

    Create A05. Queens Permutations - 2d As 2d - Queen Chooses.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/A05. Queens Permutations - 2d As 2d - Queen Chooses.java
@@ -0,0 +1,53 @@
+Queens Permutations - 2d As 2d - Queen Chooses
+
+1. You are given a number n, representing the size of a n * n chess board.
+2. You are required to calculate and print the permutations in which n queens can be placed on the 
+     n * n chess-board. 
+
+Note -> Use the code snippet and follow the algorithm discussed in question video. The judge can't 
+               force you but the intention is to teach a concept. Play in spirit of the question.
+Input Format
+A number n
+Output Format
+Check the sample output and question video
+
+Constraints
+1 <= n <= 5
+Sample Input
+2
+Sample Output
+q1	q2	
+-	-	
+
+q1	-	
+q2	-	
+
+q1	-	
+-	q2	
+
+q2	q1	
+-	-	
+
+-	q1	
+q2	-	
+
+-	q1	
+-	q2	
+
+q2	-	
+q1	-	
+
+-	q2	
+q1	-	
+
+-	-	
+q1	q2	
+
+q2	-	
+-	q1	
+
+-	q2	
+-	q1	
+
+-	-	
+q2	q1	

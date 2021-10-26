8444929e3a3e7877f02701952418111406417fc1
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-26 14:19:51 +0530

    Update A02. Permutations - 2.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/A02. Permutations - 2.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/A02. Permutations - 2.java
@@ -1,9 +1,9 @@
-Permutations - 2
+Combinations - 1
 
-1. You are give a number of boxes (nboxes) and number of non-identical items (ritems).
+1. You are give a number of boxes (nboxes) and number of identical items (ritems).
 2. You are required to place the items in those boxes and print all such configurations possible.
 
-Items are numbered from 1 to ritems.
+Items are identical and all of them are named 'i'.
 Note 1 -> Number of boxes is greater than number of items, hence some of the boxes may remain 
                    empty.
 Note 2 -> Check out the question video and write the recursive code as it is intended without 
@@ -11,7 +11,7 @@ Note 2 -> Check out the question video and write the recursive code as it is int
 Input Format
 Input is managed for you
 Output Format
-Check the sample ouput and question video. 0 means empty box.
+Check the sample ouput and question video. - means empty box, i means occupied by item.
 
 Constraints
 0 < nboxes <= 10
@@ -20,66 +20,18 @@ Sample Input
 5
 3
 Sample Output
-12300
-12030
-12003
-13200
-13020
-13002
-10230
-10203
-10320
-10302
-10023
-10032
-21300
-21030
-21003
-23100
-23010
-23001
-20130
-20103
-20310
-20301
-20013
-20031
-31200
-31020
-31002
-32100
-32010
-32001
-30120
-30102
-30210
-30201
-30012
-30021
-01230
-01203
-01320
-01302
-01023
-01032
-02130
-02103
-02310
-02301
-02013
-02031
-03120
-03102
-03210
-03201
-03012
-03021
-00123
-00132
-00213
-00231
-00312
-00321
+iii--
+ii-i-
+ii--i
+i-ii-
+i-i-i
+i--ii
+-iii-
+-ii-i
+-i-ii
+--iii
+
+
 
 Code:
 

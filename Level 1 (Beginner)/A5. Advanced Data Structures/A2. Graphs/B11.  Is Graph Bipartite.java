a2e17533bbef05fdefd399caf6a415f4e51f4009
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-27 17:45:32 +0530

    Update B11.  Is Graph Bipartite.java

--- Level 1 (Beginner)/A5. Advanced Data Structures/A2. Graphs/B11.  Is Graph Bipartite.java
+++ Level 1 (Beginner)/A5. Advanced Data Structures/A2. Graphs/B11.  Is Graph Bipartite.java
@@ -128,3 +128,105 @@ public class Main {
      
    } 
 }
+
+//=========================SS's code============================================
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+         this.src = src;
+         this.nbr = nbr;
+         this.wt = wt;
+      }
+   }
+   
+    static class Pair{
+       int v;
+       String psf;
+       int level;
+       
+        public Pair(int v, String psf, int level){
+           this.v= v;
+           this.psf= psf;
+           this.level= level;
+       }
+   }
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int vtces = Integer.parseInt(br.readLine());
+      ArrayList<Edge>[] graph = new ArrayList[vtces];
+      for (int i = 0; i < vtces; i++) {
+         graph[i] = new ArrayList<>();
+      }
+
+      int edges = Integer.parseInt(br.readLine());
+      for (int i = 0; i < edges; i++) {
+         String[] parts = br.readLine().split(" ");
+         int v1 = Integer.parseInt(parts[0]);
+         int v2 = Integer.parseInt(parts[1]);
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
+      }
+      
+      int visited[]= new int[vtces]; // hum boolean nhi integer wla rakhenge aur sabme -1 dal denge
+      Arrays.fill( visited, -1); // -1 represent kr rha hai ki woh kabhi visit hi nhi hue hai, agar visit hue to uske samne woh level dal denge ki iss level pe visit hua hai 
+      
+      for( int i=0; i< graph.length; i++){
+          if( visited[i] == -1){ // not equal to -1 hai mtlab woh visit ho chuka hai so humne unke liye call laggani hai jo visite nhi hue hai
+              boolean  isCompBipartite= checkComponentForBipartiteness(graph, i, visited);
+              if( isCompBipartite == false){
+                  System.out.println(false);
+                  return;
+              }
+          }
+      }
+      //agar sare component check karke ek bhi component bibatite false nhi mila to true print kardenge ki sare hi bipartite hai
+      System.out.println(true);
+      
+   }
+   
+   public static boolean checkComponentForBipartiteness(ArrayList<Edge>[] graph, int src, int visited[]){
+       
+       ArrayDeque<Pair> q= new ArrayDeque<>();
+       
+       q.add(new Pair(src, src+"", 0)); //starting ki level 0
+       
+       while(q.size() > 0){ // jabtak queue khali nhi hoti tabtak kam karenge, remove, mark star, work, add star.
+        
+        //pehle remove pair
+        Pair rp= q.removeFirst();
+        
+        //ab mark star
+        if( visited[rp.v] != -1){ //agar already visited hai to we need to check yeh dubara visit ho rha hai, to yeh usi level pe visit ho rha hai ya naye level pe visit ho rha hai. agar purane wale level pehi visit ho rha hai to koi dikkat nhi hai even cycle hai , agar pichle bar yeh nth level pe visit hua aur ab n+1th level pe visit hua hai means yeh odd cycle hai means bipartite nhi hai
+        
+            if( rp.level != visited[rp.v] ){ //agar removed wale ka level agar nhi hai woh equal uske purane level ke sath, purana level visited mai pda hua hai jo pichli bar visit hua tha
+           //aajka aur purana level barbr nhi hai matlab yeh odd cycle hai mean bipartite nhi hai so return false
+               return false;
+          }
+            
+        }else{
+            //agar alreay visited nhi hai to visited mark kardenge iska level dal ke
+            visited[rp.v]= rp.level;
+        }
+        
+        // ab iske neighbours
+        for(Edge e: graph[rp.v]){
+            if( visited[e.nbr] == -1){
+                //unvisted hai to queue mai add kare
+                q.add(new Pair(e.nbr, rp.psf + e.nbr, rp.level + 1)); // univisited neighbours ko dala aur unka level removed wale se ek se jada dala
+            }
+         }
+       }
+       return true;
+   }
+}

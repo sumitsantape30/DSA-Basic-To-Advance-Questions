0062b20e37f59383dc00f629bec519d3e23e8071
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-28 08:39:23 +0530

    Update A5. Last Index.java

--- Level 1 (Beginner)/A2. Recursion And Backtracking/A2. Recursion in Arrays/A5. Last Index.java
+++ Level 1 (Beginner)/A2. Recursion And Backtracking/A2. Recursion in Arrays/A5. Last Index.java
@@ -65,4 +65,12 @@ public static int li2(int arr[], int idx, int data) {
 		}
 		
 	}
-	
+Analysis
+Time Complexity :
+O(n)
+As n (array"s length) calls are made and work is done corresponding to these n calls therefore the time complexity becomes O(n).
+
+SPACE COMPLEXITY :
+O(1)
+Since no extra space is used, therefore space complexity is constant, however you should know that if the recursion call stack is taken into account, then space complexity will be O(n) as there are n recursive calls.
+

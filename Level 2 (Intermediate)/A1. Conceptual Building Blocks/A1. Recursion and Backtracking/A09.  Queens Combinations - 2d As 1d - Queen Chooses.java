891af019d10ce443ff947f5ab8e6aead9505a121
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-10-31 18:35:49 +0530

    Create A09.  Queens Combinations - 2d As 1d - Queen Chooses.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/A09.  Queens Combinations - 2d As 1d - Queen Chooses.java
@@ -0,0 +1,83 @@
+Queens Combinations - 2d As 1d - Queen Chooses
+
+1. You are given a number n, representing the size of a n * n chess board.
+2. You are required to calculate and print the combinations in which n queens can be placed on the 
+     n * n chess-board. 
+
+Note -> Use the code snippet and follow the algorithm discussed in question video. The judge can't 
+               force you but the intention is to teach a concept. Play in spirit of the question.
+Input Format
+A number n
+Output Format
+Check the sample output and question video
+
+Constraints
+1 <= n <= 5
+Sample Input
+2
+Sample Output
+q	q	
+-	-	
+
+q	-	
+q	-	
+
+q	-	
+-	q	
+
+-	q	
+q	-	
+
+-	q	
+-	q	
+
+-	-	
+q	q	
+
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void queensCombinations(int qpsf, int tq, boolean[][] chess, int lcno) {
+        if(qpsf == tq){ //sari queens place hogyi to print and return
+          
+          for(int row = 0; row < chess.length; row++){
+              for( int col=0; col< chess.length; col++){
+                  if( chess[row][col] == true){
+                      System.out.print("q\t");
+                  }else{ //agar queen nhi hai khali hai badda to
+                      System.out.print("-\t");
+                  }
+              }
+              System.out.println();
+          }
+           System.out.println();
+           return;
+        }
+        
+        for( int cell= lcno+1 ; cell < chess.length*chess.length; cell++){ // jaha last queen baithayi thi uske aage loop chalayenge end tak
+            
+            //ab aap ith cell mai baithana chahte ho to apko wapas row no banani padegi 
+            int row= cell / chess.length;
+            int col= cell % chess.length;
+            //apko pass kiya gya 1d ki tarah, loop bhi humne 1d ki tarah lagaya aur ab humne usko row col means 2d mai convert karliya kyuki chess mai row col mai hi dal payenge na wog to 2d hai
+            chess[row][col] = true; //pehle check karneki jarurat nhi hai ki true hai ki nhi coz hum humesha aage wale dabbe mai dalte hai 
+            
+            queensCombinations(qpsf+1, tq, chess, cell); //ek aur queen place hogyi hai to +1 aur abhi cell th dabbe mai dala hai to cell pass kiye
+            
+            chess[row][col]= false;
+            
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] chess = new boolean[n][n];
+
+        queensCombinations(0, n, chess, -1); //last cell no -1 pass kiye ki pichli rani -1 pe baithi thi
+    }
+}

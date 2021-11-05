c7fb4573531f738ff37e0738b35641521bcaf2f2
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-05 13:44:05 +0530

    Create B16. Words - K Selection - 1.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/B16. Words - K Selection - 1.java
@@ -0,0 +1,32 @@
+Words - K Selection - 1
+
+1. You are given a word (may have one character repeat more than once).
+2. You are given an integer k.
+2. You are required to generate and print all ways you can select k distinct characters out of the 
+     word.
+
+Note -> Use the code snippet and follow the algorithm discussed in question video. The judge can't 
+               force you but the intention is to teach a concept. Play in spirit of the question.
+Input Format
+Input is managed for you
+Output Format
+Check the sample ouput and question video. 
+
+Constraints
+0 < str.length() < 15
+0 < k <= str.length()
+Sample Input
+aabbbccdde
+3
+Sample Output
+abc
+abd
+abe
+acd
+ace
+ade
+bcd
+bce
+bde
+cde
+

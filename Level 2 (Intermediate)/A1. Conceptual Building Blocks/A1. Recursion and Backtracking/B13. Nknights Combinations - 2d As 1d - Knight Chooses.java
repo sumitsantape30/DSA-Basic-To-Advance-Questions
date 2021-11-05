cfec1241700deca25813abaa21096ccef773f0c5
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-05 08:44:51 +0530

    Update B13. Nknights Combinations - 2d As 1d - Knight Chooses.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/B13. Nknights Combinations - 2d As 1d - Knight Chooses.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/B13. Nknights Combinations - 2d As 1d - Knight Chooses.java
@@ -37,3 +37,61 @@ k	k
 
 Code:
 
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static boolean IsKnightSafe(boolean[][] chess, int i, int j) {
+        // so muje 4 dabbe check karne hai
+        if( i - 1 >= 0 && j-2 >= 0 && chess[i-1][j-2]){ // woh spot range mai hona chahiye aur iss spot pe pehlese knight baitha hai to you are not safe
+            return false;
+        }
+        
+        if( i - 2 >= 0 && j-1 >= 0 && chess[i-2][j-1]){ //range mai hona chahiye aur iss spot pe pehlese knight baitha hai to you are not safe
+            return false;
+        }
+        
+        if( i - 2 >= 0 && j+1 < chess.length && chess[i-2][j+1]){ // iss spot pe pehlese knight baitha hai to you are not safe
+            return false;
+        }
+        
+        if( i - 1 >= 0 && j+2 < chess.length && chess[i-1][j+2]){ // iss spot pe pehlese knight baitha hai to you are not safe
+            return false;
+        }
+        
+        return true; // agar waha kahi nhi pda to aap bilkul safe hai
+    }
+
+    public static void nknights(int kpsf, int tk, boolean[][] chess, int lcno) {
+        if (kpsf == tk) {
+            for (int row = 0; row < chess.length; row++) {
+                for (int col = 0; col < chess.length; col++) {
+                    System.out.print(chess[row][col] ? "k\t" : "-\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = lcno + 1; i < chess.length * chess.length; i++) {
+            int row = i / chess.length;
+            int col = i % chess.length;
+
+            if (chess[row][col] == false && IsKnightSafe(chess, row, col)) { // IsKnightSafe call kiya ki aagar knight safe hai tabhi bithaye
+                chess[row][col] = true;
+                nknights(kpsf + 1, tk, chess, row * chess.length + col);
+                chess[row][col] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] chess = new boolean[n][n];
+
+        nknights(0, n, chess, -1);
+    }
+}

fb53b68a87cef79edbb99fb55f9fadf746bcba24
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-07 15:18:48 +0530

    Update B18.  Words - K Selection - 3.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/B18.  Words - K Selection - 3.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/B18.  Words - K Selection - 3.java
@@ -47,3 +47,55 @@ dde
 
 Code:
 
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void fun(String str, HashMap<Character, Integer> fmap, int idx, String asf, int k){
+      
+      if( k < 0){ //agar k negative hojata hai, jitne select karne the use jada select karliye to wahase apko answer nhi milne wala laut jao
+        return;
+      }
+      
+      if( idx == str.length()){ // jab aap sare characters ka faisla krlete ho to uss time i'll check 
+        if( k == 0){ //basecase: agar k ki value 0 hojati hai to print kardenge
+         System.out.println(asf);
+       }
+       return;
+      }
+      
+      //maine pehle option diya ki 'a' ki kitni frequency hai, lets say 4 hai to 'a' 4 bar banke aa skta hai then 3,2,1 aur fir ho skta hai ki 'a' select hi na ho means 0 times aaye
+      char ch= str.charAt(idx);
+      
+      for( int i= fmap.get(ch); i >=0 ; i--){ //iss character ki jitni frquency hogi max utni times yeh character aa skta hai aur minimum 0 times
+        String s = "";
+        for( int j=0; j< i; j++){
+            s += ch; // iss string mai mai uss character ko bar babr add krta hu, agar frequnecy 5 thi to maine 'a' ko 5 times add karliya
+        } // string merepas tayar hogyi now i'll make a call
+        
+        fun(str, fmap, idx + 1, asf + s, k - i); //current bande ka faisla ho chuka hai to index ko +1 karke pass kiya. fir jo string aapne tayar kithi usko asf mai add kardiya. aur k ki value ko i se decrease kardenge kyuki i number of times aapne character add kiya hai
+          
+      }
+  }
+  
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+    int k = Integer.parseInt(br.readLine());
+
+    HashMap<Character, Integer> unique = new HashMap<>(); // unique characters ka hashmap jiske andar frequency stored karenge
+    String ustr = ""; // unique string bana rakhi hai 
+    for (char ch : str.toCharArray()) {
+      if (unique.containsKey(ch) == false) {
+        unique.put(ch, 1);
+        ustr += ch;
+      } else {
+        unique.put(ch, unique.get(ch) + 1);
+      }
+    } // so humne frequency store karli
+
+     fun(ustr, unique, 0, "", k); // idx is abhi hum konse character ka faisla hum kr rhe hai uske liye index rakhliya. and asf will be blank
+  }
+
+}

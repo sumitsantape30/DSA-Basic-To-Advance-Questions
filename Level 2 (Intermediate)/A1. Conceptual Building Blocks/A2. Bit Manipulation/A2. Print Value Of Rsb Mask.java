9f47ecb9a7cba6f67b68816660c72e9804e45cdf
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-09 21:06:00 +0530

    Create A2. Print Value Of Rsb Mask.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/A2. Print Value Of Rsb Mask.java
@@ -0,0 +1,34 @@
+Print Value Of Rsb Mask
+
+1. You are given a number n.
+2. You have to print the right-most set bit mask.
+Input Format
+A number n
+Output Format
+A number
+
+  COMMENTConstraints
+1 <= n <= 10^9
+Sample Input
+58
+Sample Output
+10
+
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+
+    int rsbm = n & -n ; // rightmost set bit mask 
+    
+    //hum isko binary print karna chahte hai
+    System.out.println(Integer.toBinaryString(rsbm));
+  }
+
+}

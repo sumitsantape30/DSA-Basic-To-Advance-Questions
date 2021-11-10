30a259e5ff1b9e4ccb6d75d4216d25873dc60d2c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-10 16:10:56 +0530

    Update A3. Kernighans Algorithm.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/A3. Kernighans Algorithm.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/A3. Kernighans Algorithm.java
@@ -41,3 +41,25 @@ public class Main {
   }
 
 }
+
+//------------------------JB's--------------------------------------------
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    
+    int count=0;
+    
+    while( n!= 0){
+        n ^= (n & ((~n) + 1));
+        count++;
+    }
+    System.out.println(count);
+  }
+
+}

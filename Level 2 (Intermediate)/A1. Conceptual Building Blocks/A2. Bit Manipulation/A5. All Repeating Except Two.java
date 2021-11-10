c80a2ab257b6887d822e3db90fe4c11f392e3990
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-10 20:50:45 +0530

    Create A5. All Repeating Except Two.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/A5. All Repeating Except Two.java
@@ -0,0 +1,24 @@
+All Repeating Except Two
+
+1. You are given an array of numbers.
+2. You have to find 2 non-repeating numbers in an array.
+3. All repeating numbers are repeating even number of times.
+Input Format
+A number n
+a1
+a2..
+n numbers
+Output Format
+2 Non-repeating number
+
+Constraints
+1 <= n <= 10^9
+1 <= a1,a2.. <= 10^9 
+Sample Input
+6
+23 27 23 17 17 37
+Sample Output
+27
+37
+
+Code:

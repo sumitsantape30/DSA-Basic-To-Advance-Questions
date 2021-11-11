39b4949e7740d49167c8631ed7a5f7c0bee8ddfa
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-11 17:45:38 +0530

    Create A7. ll Repeating Three Times Except One.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/A7. ll Repeating Three Times Except One.java
@@ -0,0 +1,25 @@
+ll Repeating Three Times Except One
+
+1. You are given an array of numbers.
+2. All numbers occur thrice in the array except one.
+3. You have to find the element that occurs once.
+Input Format
+A number n
+a1
+a2..
+n numbers
+Output Format
+A number
+
+Constraints
+1 <= n <= 10^9
+1 <= a1,a2.. <= 10^9
+Sample Input
+4
+1 1 1 2 
+Sample Output
+2
+
+Code:
+
+

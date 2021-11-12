72773076729b60b3acbc69506c9b20ee6f189c25
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-12 07:16:29 +0530

    Create A9. Reduce N To 1.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/A9. Reduce N To 1.java
@@ -0,0 +1,20 @@
+Reduce N To 1
+
+1. You are given a positive number N.
+2. You have to find the minimum number of operations required to convert N into 1.
+3. Operations allowed :
+     (i)  If n is even, you have to replace n with n/2.
+     (ii) If n is odd, you can replace n with either n-1 or n+1.
+Input Format
+A number N
+Output Format
+Check the sample ouput and question video.
+
+  COMMENTConstraints
+1 <= N <= 2147483647
+Sample Input
+8
+Sample Output
+3
+
+Code:

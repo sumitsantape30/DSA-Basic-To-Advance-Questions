e86efb3e5a3e3d7e0bc83bf0940e190c687d6e2c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-13 17:32:46 +0530

    Update B11. Xor Of Sum Of All Pairs.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/B11. Xor Of Sum Of All Pairs.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/B11. Xor Of Sum Of All Pairs.java
@@ -26,6 +26,7 @@ Sample Output
 
 Code:
 
+//============================SS's 2================================
 import java.io.*;
 import java.util.*;
 
@@ -49,4 +50,32 @@ public class Main {
         }
         System.out.println(solution(arr));
     }
+}
+
+//================= SS's 2==========================================
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int solution(int[] arr){
+       int ans=0;
+       
+       for(int val: arr){ 
+           ans = ans ^ val;
+       }
+       
+       return 2 * ans;
+    }
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+    
+    
 }

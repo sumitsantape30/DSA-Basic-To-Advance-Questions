30f5f932bb84ac9e808b5a039b0b130d03354d1f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-14 21:01:13 +0530

    Update B13. Minimum Number Of Software Developers.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/B13. Minimum Number Of Software Developers.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/B13. Minimum Number Of Software Developers.java
@@ -36,3 +36,60 @@ Sample Output
 [0, 2]
 
 Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  static ArrayList<Integer> sol = new ArrayList<>(); // yeh sare solutions ka static hai, sare solutions ko compare karayega
+
+  public static void solution(int[] people, int nskills, int cp, ArrayList<Integer> onesol, int smask) { // cp: current person abhi kiska failsa ho rha hai. people: woh jiske andar har person ke skill ke mask hai  smask: skills mask, nskill: number of skills
+     
+     if( cp == people.length){ // aisa krte krte jab sare log khatam hojayenge to ho skta hai yaha answer ho 
+         // but hume sare answers mese wahi consider krne hai jisme sari bits set hai 
+         if( smask == (1 << nskills) - 1){ // 1 >> 2 hota hai 100 usmese ek minus krdo
+             if( sol.size() == 0 || onesol.size() < sol.size()){ // agar solution ka size 0 ho means abhitak ek bhi mila nhi hai. yafir one solution ka size apse chota hai. so yeh pehle solution or humare pas better solution hai 
+                sol = new ArrayList<>(onesol); // solution ko upgrade kardo
+             }
+         }
+         return;
+     }
+     
+     //har bande ke pas means current person ke pas 2 choices hai ki woh aa skta hai or woh nhi bhi aa skta
+     //agar humara banda aanese mana krta hai 
+     solution(people, nskills, cp+1, onesol, smask); // current person badhega, one solution mai kuch add nhi hoga aur smask bhi waisahi rahega kyuki bande ne NO kaha hai
+     
+    //agar banda yes kehta hai to pehle one solutin ke andar upar jate jate iss bande ka index add hona chahiye 
+    onesol.add(cp);
+    
+    //aur call lagado
+    solution(people, nskills, cp+1, onesol, (smask | people[cp]) ) ; //about last argument: upar jate hue mai pass karunga purana mask(smask) or people of current peroson, skills of current person. jo current person hai uske mask ko purane mask ke sath OR kardiya. current person ke pas jo skills thi woh smask iss mask mai set hogyi 
+    
+    //aur wapas aate aate onesol mese remove bhi hona chahiye
+    onesol.remove(onesol.size()-1);
+  }
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    HashMap<String, Integer> smap = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      smap.put(scn.next(), i); // yaha input ke andar map banaya hai har skill ke samne uska index, ki 1st skill is for 0, 2nd skill is for 1, 3rd skill is for 2 so on.
+    }
+
+    int np = scn.nextInt();
+    int[] people = new int[np]; // yaha number of persons ka array banaya
+    for (int i = 0; i < np; i++) {
+      int personSkills = scn.nextInt();
+      for (int j = 0; j < personSkills; j++) { // fir uss person ki skills mai loop lagaya
+        String skill = scn.next();
+        int snum = smap.get(skill); // skill ka number nikala map mese jo pehle dal diya tha ki yeh skill 0th hai, 'a' aya to 'a' ko 0 mai convert karliya
+        people[i] = people[i] | (1 << snum); // aur fir apne andar woh 0th bit on karli, basically yeh people mask bana hua hai
+      } // itne kam ne hume mask banake dediye
+    }
+
+    solution(people, n, 0, new ArrayList<>(), 0); // so humne people pass kiye hai, total number of skills pass kiye hai , and current person, then ek solution jo hum bana rhe hai, aur skills mai unhone pass kiya hai ki yeh skill woh hai jab aap kisi bande ko unlcude krte ho aur hokar ajati hai basically yeh skills ka mask hai yeh woh chiz hai jispe humne dry run mai yes no lagaya tha
+    System.out.println(sol);
+
+  }
+}

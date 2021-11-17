b61cd8aa76a7b20c3480db3bac3b26edfb8b372b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-17 16:39:08 +0530

    Update A02. Combinations - 1.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/A02. Combinations - 1.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/A02. Combinations - 1.java
@@ -65,3 +65,36 @@ public class Main {
   }
 
 }
+
+//================================================================JB's=========================================================
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void combinations(int cb, int tb, int ssf, int ts, String asf){ 
+      // ha aur na ki call se woh sare basecases generate honge 
+      if( cb > tb){ // jab sare boxes ka faisla hojaye means jab cb>tb
+        if( ssf == ts){ // jab selection so far total selection ke equal hoga unke print karna hai
+           System.out.println(asf);
+        }
+        return;
+      }
+
+      //given output mai dekhe to pehle badde bhare hue hai matlab ha ki call lagegi
+      combinations(cb + 1, tb, ssf + 1, ts, asf + "i"); //ssf ek se badhega kyuki humne ek box ke select karliya, asf mai i add karlenge
+      //na ki call
+      combinations(cb + 1, tb, ssf, ts, asf + "-"); // ssf nhi badhega kyuki yahape uss box ko select nhi kara item ko place karwane keliye, aur answer so far mai - add kardenge
+
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int nboxes = Integer.parseInt(br.readLine());
+    int ritems = Integer.parseInt(br.readLine());
+    combinations(1, nboxes, 0, ritems, "");
+  }
+
+}

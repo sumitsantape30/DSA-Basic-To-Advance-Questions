7367c1e41bdf785595205d301b04b03fec9618af
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-19 08:08:51 +0530

    Update A09.  Queens Combinations - 2d As 1d - Queen Chooses.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/A09.  Queens Combinations - 2d As 1d - Queen Chooses.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/A09.  Queens Combinations - 2d As 1d - Queen Chooses.java
@@ -81,3 +81,47 @@ public class Main {
         queensCombinations(0, n, chess, -1); //last cell no -1 pass kiye ki pichli rani -1 pe baithi thi
     }
 }
+
+//=================================================================JB's============================================================
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void queensCombinations(int qpsf, int tq, boolean[][] chess, int lbu) {
+        if(qpsf == tq){ // basecase pichle question jaisahi hai wahi copy karo
+          
+          for(int row = 0; row < chess.length; row++){
+              for( int col=0; col< chess.length; col++){
+                  if( chess[row][col] == true){
+                      System.out.print("q\t");
+                  }else{ 
+                      System.out.print("-\t");
+                  }
+              }
+              System.out.println();
+          }
+           System.out.println();
+           return;
+        }
+
+        for( int i= lbu +1; i < chess.length * chess.length; i++){
+            // fir row aur column nikalne padenge aur waha queen ko place kardenge
+            int row = i / chess.length;
+            int col = i % chess.length;
+            //so iss row aur col pe queen place krdo
+            chess[row][col] = true;
+            queensCombinations(qpsf +1, tq, chess, i); // i will be the last box used
+            chess[row][col] = false;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] chess = new boolean[n][n];
+
+        queensCombinations(0, n, chess, -1);
+    }
+}

05dee19b11956a8b1dc3b05ec9a5eac7afc6fdda
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-20 20:37:56 +0530

    Create B19. Abbreviation 1 - Using Bits.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/B19. Abbreviation 1 - Using Bits.java
@@ -0,0 +1,25 @@
+Abbreviation 1 - Using Bits
+
+1. You are given a word.
+2. You have to generate all abbrevations of that word.
+Note - Use bit manipulation
+Input Format
+A string representing a word
+Output Format
+Check the sample ouput and question video.
+
+  COMMENTConstraints
+1 <= length of string <= 32
+Sample Input
+pep
+Sample Output
+pep
+pe1
+p1p
+p2
+1ep
+1e1
+2p
+3
+
+Code:

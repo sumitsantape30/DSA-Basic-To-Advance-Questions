38215275d97ecb1bba83b26bc9963b2db29b83a6
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-23 17:54:06 +0530

    Create C27. Sum Of Bit Differences Of All Pairs.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/C27. Sum Of Bit Differences Of All Pairs.java
@@ -0,0 +1,24 @@
+Sum Of Bit Differences Of All Pairs
+
+1. You are given an array of n numbers.
+2. You have to find the sum of bit differences in all pairs that can be formed from n numbers.
+3. Bit difference of two numbers is defined as the count of different bits at same positions in binary representations of two numbers.
+Input Format
+A number n
+a1
+a2..
+n numbers
+Output Format
+Check the sample ouput and question video.
+Question Video
+
+Constraints
+1 <= n <= 10^9
+1 <= arr[i] <= 10^9
+Sample Input
+3
+1 2 3
+Sample Output
+8
+
+Code:

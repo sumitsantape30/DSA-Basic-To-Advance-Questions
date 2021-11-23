23c8a0cef30e587c518e041c5209a5a723750da4
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-23 13:35:18 +0530

    Create C26. Swap All Odd And Even Bits.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/C26. Swap All Odd And Even Bits.java
@@ -0,0 +1,20 @@
+Swap All Odd And Even Bits
+
+1. You are given a number n.
+2. You have to swap all odd position bits with even position bits.
+3. Every odd position bit is swapped with adjacent bit on left side.
+4. Every even position bit is swapped with adjacent bit on right side.
+5. Print the number formed after swapping.
+Input Format
+A number n
+Output Format
+Check the sample ouput and question video.
+
+Constraints
+1 <= n <= 10^9
+Sample Input
+10
+Sample Output
+5
+
+Code:

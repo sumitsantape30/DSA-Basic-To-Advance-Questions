b4a3074ca15b4f13da65cf1e9b1377940b7c34ae
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-23 10:50:14 +0530

    Create C25. Solve 7n By 8.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A2. Bit Manipulation/C25. Solve 7n By 8.java
@@ -0,0 +1,17 @@
+Solve 7n By 8
+
+1. You are given a number n.
+2. You have to calculate the value of 7n/8 without using division and multiplication.
+Input Format
+A number n
+Output Format
+Check the sample ouput and question video.
+
+Constraints
+1 <= n <= 10^9
+Sample Input
+15
+Sample Output
+13
+
+Code:

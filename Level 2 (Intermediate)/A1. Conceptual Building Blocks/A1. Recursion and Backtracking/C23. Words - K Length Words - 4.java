35d3c9f798be38c92a60fbf52b402cb55be12bf3
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-24 23:33:41 +0530

    Update C23. Words - K Length Words - 4.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/C23. Words - K Length Words - 4.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/C23. Words - K Length Words - 4.java
@@ -26,3 +26,52 @@ bab
 bba
 
 Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void fun( String str, HashMap<Character, Integer> fmap, int cs, int ts, String asf){ // cs: current spot, ts: total spots, asf: answer so far
+  
+      if( cs == ts + 1){ // agar current spot total spot+1 ke equal hojata hai, kyuki humne main function mai current spot 1 pass kiya hai, 3 spot then aur tum 4 pe gye means humara kam hochuka hai
+        System.out.println(asf);
+        return;
+      }
+      
+      //pehle spot keliye sare options hote hai, jitne bhi unique string ke andar characters hote hai woh sare options honge
+      for( int i=0; i< str.length(); i++){
+          char ch= str.charAt(i);
+          //ab hume hashmap mai check karna hoga iski frequency bachi hui hai humare pas, agar iski frequency grater than 0 hai matlab hum isko use kr skte hai
+          if( fmap.get(ch) > 0 ){
+              fmap.put(ch, fmap.get(ch) - 1); // frequency ko 1 se kam krdo
+              fun(str, fmap, cs + 1, ts, asf + ch); // current spot ka faila hogya so cs+1
+              fmap.put(ch, fmap.get(ch) + 1); // wapas aate hue uss frequency ko dubara thik kardenge. freuquency 1 se kam ki thi isliye wapas aate hue badha di
+          }
+      }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+    int k = Integer.parseInt(br.readLine());
+    
+    HashMap<Character, Integer> fmap = new HashMap<>();
+    String ustr= "";
+    
+    for( int i=0; i< str.length(); i++){
+        char ch= str.charAt(i);
+        
+        if( fmap.containsKey(ch) == true){ // agar map mai character apko mil jata hai to uski frequency update krdenge
+          fmap.put(ch, fmap.get(ch) + 1);
+        }else{ // yaha aaye means pehli bar aa rha hai to uski frquency rakhdo 1 aur usko unique string mai bhi add krdo
+           fmap.put(ch, 1);
+           ustr += ch;
+        }
+    }
+    
+    fun(ustr, fmap, 1, k, ""); // current spot is 1 means pehle spot ka faisla kr rhe hai, and total number of spots will be k
+
+  }
+
+}

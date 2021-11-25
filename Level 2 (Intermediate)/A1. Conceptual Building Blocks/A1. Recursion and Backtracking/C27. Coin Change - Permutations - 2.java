d3122792b990d7d6da4b203e69b67b3928c8d9ac
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-11-25 19:04:48 +0530

    Update C27. Coin Change - Permutations - 2.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/C27. Coin Change - Permutations - 2.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/C27. Coin Change - Permutations - 2.java
@@ -33,3 +33,33 @@ Sample Output
 5-2-.
 
 Code:
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void coinChange(int[] coins, int amtsf, int tamt, String asf) {
+        if( amtsf > tamt){ // aisa hua to aage call mat lagao yahise return hojao
+          return;
+        }else if( amtsf == tamt){ // agar amount so far equal hota hai total amount ke to hume answer mil gya to print kardo
+          System.out.println(asf + ".");
+          return;
+        }
+        
+        //mere pas sare options hai mai sare coins ko use kr skta hu
+        for( int i=0; i< coins.length; i++){
+            coinChange( coins, amtsf + coins[i], tamt, asf + coins[i] + "-"); // amout so far will get incremented by coins[i]
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        int amt = Integer.parseInt(br.readLine());
+        coinChange(coins, 0, amt, "");
+    }
+}

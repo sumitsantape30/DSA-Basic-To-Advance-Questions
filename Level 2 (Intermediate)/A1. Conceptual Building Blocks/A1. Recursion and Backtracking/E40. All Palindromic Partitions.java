d6e46b16b5b168a0472d2283b250d83deb014572
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-12-01 21:36:34 +0530

    Create E40. All Palindromic Partitions.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/E40. All Palindromic Partitions.java
@@ -0,0 +1,21 @@
+All Palindromic Partitions
+
+1. You are given a string of length n.
+2. You have to partition the given string in such a way that every partition is a palindrome.
+
+Note -> Check out the question video and write the recursive code as it is intended without 
+               changing signature. The judge can't force you but intends you to teach a concept.
+Input Format
+A String of length n
+Output Format
+Check the sample ouput and question video.
+Question Video
+
+Constraints
+1 <= length of string <= 15
+Sample Input
+pep
+Sample Output
+(p) (e) (p) 
+(pep) 
+

34184d2533b4673be9cd8865bc2671271f86c75f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-12-02 21:54:19 +0530

    Update E40. All Palindromic Partitions.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/E40. All Palindromic Partitions.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/E40. All Palindromic Partitions.java
@@ -19,3 +19,56 @@ Sample Output
 (p) (e) (p) 
 (pep) 
 
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static boolean isPalindrome(String str){
+        int li = 0;
+        int ri = str.length()-1;
+        
+        while( li < ri){
+            char right = str.charAt(ri);
+            char left = str.charAt(li);
+            
+            if(left != right){
+                return false;
+            }
+            
+            li++;
+            ri--;
+        }
+        return true;
+    }
+
+	public static void solution(String str, String asf) {
+	    if( str.length() == 0){
+	        System.out.println(asf);
+	        return;
+	    }
+	    
+	    //string ke prefixes ko analyse karna hai 
+	    for( int i=0; i< str.length(); i++){
+	        //shuruwat mai i 0 pe hai to 0th character tak ka prefix muje nikalna hai 
+	        String prefix = str.substring(0, i + 1); // iss o to i tak ke characters milenge i+1th character nhi milega
+	        String ros = str.substring(i+1) ;// ros: rest of the string, 0 to i to apne kha liye to i+1 se end tak rest of the string hogi
+	       
+	        // ab tumhe check karna hoga ki kya yeh prefix palindromic hai ya nhi hai
+	        if( isPalindrome(prefix)){
+	            //agar yeh palindrome mila to hum ise call lagayenge nhito hum ise call nhi lagayenge 
+	            solution(ros, asf + "(" + prefix + ") ");
+	        }
+	    }
+
+
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.nextLine();
+		solution(str, "");
+	}
+
+}

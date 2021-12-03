9da56afc9d17bed5ce1ef695ab34b313a3514ab6
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-12-03 21:30:37 +0530

    Create E43. Remove Invalid Parenthesis.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/E43. Remove Invalid Parenthesis.java
@@ -0,0 +1,22 @@
+Remove Invalid Parenthesis
+
+1. You are given a string, which represents an expression having only opening and closing parenthesis.
+2. You have to remove minimum number of parenthesis to make the given expression valid.
+3. If there are multiple answers, you have to print all of them.
+
+Note -> Check out the question video and write the recursive code as it is intended without changing signature. The judge can't 
+               force you but intends you to teach a concept.
+Input Format
+A string containing only opening and closing parenthesis
+Output Format
+Print all the Valid expressions.
+Check the sample ouput and question video.
+
+Constraints
+1 <= length of string <= 20
+Sample Input
+()())()
+Sample Output
+(())()
+()()()
+

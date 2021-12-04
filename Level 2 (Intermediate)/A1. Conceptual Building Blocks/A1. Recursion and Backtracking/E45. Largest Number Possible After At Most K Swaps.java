7cac20cbbf8b591bba73fb4935e37447ce56008b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-12-04 21:02:13 +0530

    Create E45. Largest Number Possible After At Most K Swaps.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/E45. Largest Number Possible After At Most K Swaps.java
@@ -0,0 +1,21 @@
+Largest Number Possible After At Most K Swaps
+
+1. You are given a string which represents digits of a number.
+2. You have to create the maximum number by performing at-most k swap operations on its digits.
+
+Note -> Check out the question video and write the recursive code as it is intended without 
+               changing signature. The judge can't force you but intends you to teach a concept.
+Input Format
+A string S and a number K
+Output Format
+A number
+
+Constraints
+1 <= length of S <= 30
+1 <= K <= 10
+Sample Input
+1234567
+4
+Sample Output
+7654321
+

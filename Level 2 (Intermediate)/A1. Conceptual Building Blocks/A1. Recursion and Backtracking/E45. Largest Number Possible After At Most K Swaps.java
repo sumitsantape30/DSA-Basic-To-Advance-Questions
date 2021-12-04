429930840da2936341d47d94efb5394419f01e8c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-12-04 22:08:46 +0530

    Update E45. Largest Number Possible After At Most K Swaps.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/E45. Largest Number Possible After At Most K Swaps.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/E45. Largest Number Possible After At Most K Swaps.java
@@ -19,3 +19,66 @@ Sample Input
 Sample Output
 7654321
 
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static String swap(String str, int i, int j){
+        
+        char ith = str.charAt(i); 
+        char jth = str.charAt(j);
+        
+        String left = str.substring(0, i); //isme i include nhi hoga
+        String middle = str.substring(i + 1, j); //middle part i ke bad start ho rha hai aur j se pehle khatam hai
+        String right = str.substring(j + 1); // j+1 se end tak
+        
+        return left + jth + middle + ith + right; 
+    }
+
+	static String max;
+	public static void findMaximum(String str, int k) {
+        
+        if(Integer.parseInt(str) > Integer.parseInt(max)){ //yeh jo nayi string aap leke aaye ho agar woh max se bada hai to max mai yeh string pakdle
+            max = str;
+        }
+        
+        if(k == 0){ // k = 0 ka matlab hai there are no more swaps allowed
+            return;
+        }
+        
+        //yeh hai mere options hai 
+        for(int i = 0; i < str.length() - 1; i++){ // sare characters pe jaunga, last character pr nhi jaunga kyuki j humesha i se aage hone chahiye
+          for(int j = i + 1; j < str.length(); j++){ // j i+1 se start hoga, j is always to the right of i.
+              if(str.charAt(j) > str.charAt(i)){ // agar j wala char bada hua i wale char se (char apne ascii pe compare honge) to hum swap karenge
+                String swapped = swap(str, i, j); // swap karke ek nayi string ayegi
+                //swap kiya ab call laga denge
+                findMaximum(swapped, k - 1);// swapped string pass karenge aur k-1 ki ab itne swaps apne pas possible hai
+                //iss case mai backtrack karke wapas string ko thik karne ki jarurat nhi hai jo change aya tha woh maine swapped mai pkda tha aur wahi call krte waqt pass kiya tha, string mai koi change hua nhi actually
+              }
+          }
+        }
+    
+  /*      for(int i = 0; i < str.length() - 1; i++){ 
+          for(int j = i + 1; j < str.length(); j++){ 
+              if(str.charAt(j) > str.charAt(i)){ 
+                str = swap(arr, i, j); 
+                findMaximum(str, k - 1);
+                str = swap(arr, i, j); // yahape string ko wapas pehle jaise banao, to dubara se i,j ko swap karlo. 2 bar swap honge to dubara wahi ban jayenge jo the
+              }
+          }
+        }       */
+        
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		int k = scn.nextInt();
+		 max = str;
+		findMaximum(str, k);
+		System.out.println(max);
+	}
+
+}

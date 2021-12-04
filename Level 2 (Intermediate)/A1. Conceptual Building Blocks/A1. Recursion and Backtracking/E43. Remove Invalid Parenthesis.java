e6396169e6c13e6295bb41ebf37f2123f2eedf6f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-12-04 18:26:15 +0530

    Update E43. Remove Invalid Parenthesis.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/E43. Remove Invalid Parenthesis.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A1. Recursion and Backtracking/E43. Remove Invalid Parenthesis.java
@@ -20,3 +20,65 @@ Sample Output
 (())()
 ()()()
 
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void solution(String str, int mra, HashSet<String> ans) { // mra = minimum removals allowed
+    if (mra == 0) { // aisa krte krte minimum removals allowed 0 hogyi ho to return karna hai. jitni removals allowed thi utni karli iske bad call lagaoge to extra removal hojayegi, yahapr print karna nhi karna woh uss chiz pe depend krta hai jo aap string leke aaye ho woh valid hai ya nhi
+
+      int mrnow = getMin(str); //yahapr apke pas jo string pdi hai uske liye minimum removals allowed nikal lo ki jo aap string leke aaye ho uske liye kitne removals chahiye
+      if (mrnow == 0) { // agar removal 0 hai to aap already balanaced hai
+        //ab koi removal nhi chahiye to isko print kiya ja skta hai
+        if(!ans.contains(str)){ // agar answer isko already contain krta hai to yeh dublicate hai hai so ans nhi contain krta hai tohi print kare
+            System.out.println(str);
+            ans.add(str); //aur ans mai add karle
+        }
+      }
+      return;
+    }
+
+    //har character ko pehle level pr pehle removal karenge, har character ko remove hone ki option denge (tu remove hota to kya hota)
+    for(int i = 0; i < str.length(); i++) {
+      // i ko remove karna hai to i ke pehle ka content chahiye
+      String left = str.substring(0, i);// i se pehle ka content
+      String right = str.substring(i + 1);
+      solution(left + right, mra - 1, ans); // left + right aage pass kiya to apka character chut gya hai aur minimum allowed removals mese ek kam krdo kyuki ek removal apne use karli hai
+    }
+  }
+
+  public static int getMin(String str) { // expression mese kitne brackets remove kiya jaye ki woh balanced hojaye
+
+    Stack<Character> st = new Stack<>();
+
+    for(int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+
+      if(ch == '(') { // agar character opening ho to sidha push hota hai
+        st.push(ch);
+      } else if (ch == ')') { //agar character closing bracket hai
+         
+         if(st.size() == 0) { // closing brcket hai aur stack ka size 0 hai, iss closing ko opening mila nhi to push krdenge
+          st.push(ch);
+        }else if (st.peek() == ')') { // agar closing aya hai aur stack ki top pebhi closing hi pda hai tobhi invalid hai to isko bhi push krdende
+          st.push(ch);
+        }else if (st.peek() == '(') { // agar stack ki top pe pda hai opening to pop krdenge
+          st.pop();
+        }
+      }
+    }
+    return st.size(); // jitne stack mai end mai reh gye hai woh sare invalid hai
+  }
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    String str = scn.next();
+    int mr = getMin(str);// minimum removals
+    solution(str, mr, new HashSet<>()); // actual remove krke print karna hai ki kitna removal ho skta hai
+    //dublicates generate na ho isliye hashet banaya hai
+  }
+
+}

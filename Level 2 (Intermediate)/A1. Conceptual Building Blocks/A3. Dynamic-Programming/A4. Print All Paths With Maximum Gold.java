d42f9962fa521bb7b925120683ef9db4c496b69e
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-12-13 17:41:43 +0530

    Update A4. Print All Paths With Maximum Gold.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A3. Dynamic-Programming/A4. Print All Paths With Maximum Gold.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A3. Dynamic-Programming/A4. Print All Paths With Maximum Gold.java
@@ -37,3 +37,128 @@ Sample Output
 4 d3 d1 d2 d3 d1 
 
 Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+   private static class Pair {
+      String psf;
+      int i;
+      int j;
+
+      public Pair(String psf, int i, int j) {
+         this.psf = psf;
+         this.i = i;
+         this.j = j;
+      }
+   }
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      int m = Integer.parseInt(br.readLine());
+      int[][] arr = new int[n][m];
+
+      for (int i = 0; i < n; i++) {
+         String str = br.readLine();
+         for (int j = 0; j < m; j++) {
+            arr[i][j] = Integer.parseInt(str.split(" ")[j]);
+         }
+      }
+
+      int dp[][] = new int[arr.length][arr[0].length];
+      
+      //yeh column by column solve hota hai, j pehle extreme pe hoga then i niche ki taraf increase hoga
+      for( int j = arr[0].length - 1; j >= 0; j--){
+          for( int i = 0; i < arr.length; i++){
+              if( j == arr[0].length - 1){ //aap extreme column pe ho to jo values hai wahi ayegi coz aage kuch nhi hai dig krne keliye
+               dp[i][j] = arr[i][j];
+                  
+              }else if(i == 0){ //yeh apki first row hai to agle column mai jane keliye 2 options hai apke pas. app d2 yafir d3 ke taraf ja skte ho, donomese jo maximum hoga uss taraf jayenge
+               dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i + 1][j + 1]); // aap same row mai aage ja skte ho or apnese niche wali row mai diagonally ja skte ho
+                  
+              }else if( i == arr.length - 1){ //ya apki last row hai tabbhi aap 2 direction mai ja skte ho, samne or upar ki taraf
+              
+               dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i - 1][j + 1]);
+                  
+              }else{//yafir aap bichme kahipe ho to 3 direction mai ja skte ho
+              
+               dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], Math.max(dp[i - 1][j + 1], dp[i + 1][j + 1]));
+               
+              }
+          }
+      }
+      
+      int max = Integer.MIN_VALUE;
+      //first column mai jo max hoga woh mere answer hoga
+      for( int i = 0; i< dp.length; i++){
+          if( dp[i][0] > max){ //row badal rhi hai column 0 hi rahega
+            max = dp[i][0];
+          }
+      }
+      
+      System.out.println(max);
+      
+      //agar first column mai yeh max jitni bar milega means agar same value first column mai multiple rows mai hongi to shuruwat mai apni arraydeque mai yeh dono dal denge
+      
+      ArrayDeque<Pair> que = new ArrayDeque<>();
+      for( int i = 0; i< dp.length; i++){
+          if( dp[i][0] == max){ //jaha jaha max ke equal value milegi mai usko pair banake queue mai add karlunga
+            que.add(new Pair(i + "", i, 0)); //psf mai shuruwat mai row(i) number bhi dalunga. aur j 0 hai sabka
+          }
+      }
+      
+      while( que.size() > 0){ //ab jabtak meri queue khali nhi hojati 
+          Pair rem = que.removeFirst(); //remove karenge
+          
+          if( rem.j == arr[0].length - 1){ //agar yeh last column hai to kuch nhi karenge simply print karenge
+            System.out.println(rem.psf);
+          }
+          //agar yeh last column nhi hai to iske children dalenge agle teeno ko check karke unn teen mai jo ke equal hai usko dalenge. ho skta hai yeh first or last row mai bhi ho
+          else if( rem.i == 0){ // agar hum first row mai hai to 2 hi jagah ja skte hai, to jaha jaha ja skte hai uska max nikal lo
+            
+            int g = Math.max(dp[rem.i][rem.j + 1], dp[rem.i + 1][rem.j + 1]); //donoka greater(g)
+            
+            //sare child mese jo g ke equal hai unko dal do agar dono same hai to dono daldo
+            
+            if( g == dp[rem.i][rem.j + 1]){ //agar row same hai matlab d2 wala motion
+                que.add(new Pair(rem.psf + " d2", rem.i, rem.j + 1));// naya pair dalenge. samne gye ho to yeh d2 hai 
+            }
+            
+            if( g == dp[rem.i + 1][rem.j + 1]){ //agar row same hai matlab d2 wala motion
+                que.add(new Pair(rem.psf + " d3", rem.i + 1, rem.j + 1)); //isme tum niche gye to yeh d3 hai
+            }
+            
+          }else if(rem.i == arr.length - 1){ //hum last row mai hai, upar jaisa hi logic ayega so copy n paste
+            
+            int g = Math.max(dp[rem.i][rem.j + 1], dp[rem.i - 1][rem.j + 1]); 
+            
+            if( g == dp[rem.i - 1][rem.j + 1]){ 
+                que.add(new Pair(rem.psf + " d1", rem.i - 1, rem.j + 1));  //d1 hoga ab yaha
+            }
+
+            if( g == dp[rem.i][rem.j + 1]){ 
+                que.add(new Pair(rem.psf + " d2", rem.i, rem.j + 1)); 
+            }
+            
+          }else{ //bicheme kahipe hai to 3 options hote hai
+              
+            int g = Math.max(dp[rem.i][rem.j + 1], Math.max(dp[rem.i - 1][rem.j + 1], dp[rem.i + 1][rem.j + 1])); 
+            
+            if( g == dp[rem.i - 1][rem.j + 1]){ 
+                que.add(new Pair(rem.psf + " d1", rem.i + 1, rem.j + 1)); 
+            }
+
+            if( g == dp[rem.i][rem.j + 1]){ 
+                que.add(new Pair(rem.psf + " d2", rem.i, rem.j + 1));
+            }
+            
+            if( g == dp[rem.i + 1][rem.j + 1]){ 
+                que.add(new Pair(rem.psf + " d3", rem.i + 1, rem.j + 1)); 
+            }
+
+          }
+      }  
+   }
+}

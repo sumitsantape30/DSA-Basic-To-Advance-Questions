5b2631f327221f1dcc4963ac91dc1e77047e5341
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-12-14 09:35:29 +0530

    Create A5. Print All Paths With Target Sum Subset.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A3. Dynamic-Programming/A5. Print All Paths With Target Sum Subset.java
@@ -0,0 +1,36 @@
+Print All Paths With Target Sum Subset
+
+1. You are given a number n, representing the count of elements.
+2. You are given n numbers.
+3. You are given a number "tar".
+4. You are required to calculate and print true or false, if there is a subset the elements of which add up to "tar" or not.
+5. Also, you have to print the indices of elements that should be selected to achieve the given target.
+6. You have to print all such configurations.
+Input Format
+A number n
+n1
+n2
+.. n number of elements
+A number tar
+Output Format
+Check the sample output and question video.
+
+Constraints
+1 <= n <= 30
+0 <= n1, n2, .. n elements <= 20
+0 <= tar <= 50
+Sample Input
+5
+4
+2
+7
+1
+3
+10
+Sample Output
+true
+2 4 
+1 2 3 
+0 1 3 4 
+
+Code:

9213ea90ddd2387eb8eaaf8b0983d3f23ed78261
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-12-26 11:14:36 +0530

    Update A8. Remove Primes.java

--- Level 1 (Beginner)/A1. Basics of Programming/A5. Strings-ArrayList/A8. Remove Primes.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A5. Strings-ArrayList/A8. Remove Primes.java
@@ -22,6 +22,49 @@ Sample Output
 
 code:
 
+//==================== Arraylist Concepts (question niche hai)================================================================
+//==============Remember: getsetaddre get, set, add, remove
+
+import java.io.*;
+import java.util.*;
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    ArrayList< Integer> al = new ArrayList< >(); //declares an integer arraylist
+    Arraylist< String> a2 = new Arraylist < >(); //declares an arraylist of string
+    System.out.println(a1.size());
+
+    a1.add(10);
+    a1.add(20);
+    a1.add(30);
+    a1.add(1, 1000); // adds 1000 at index 1
+
+    System.out.print(a1);  //prints the entire list at one go
+
+    for (int x : a1)
+    {
+      System.out.println(x); //each element belonging to a1 will get printed
+    }
+
+    int val = a1.get(1); //gets the value at index 1
+    val = 10;
+
+    for (int i = 0; i < a1.size(); i++)
+    {
+      System.out.println(a1.get(i));  //printing using get()
+    }
+
+    a1.set(1, 2000); //changes the value at index 1 to 2000;
+
+    a1.remove(1) //removes the value at index 1
+
+  }
+}
+
+//===================================================================================================================
+
 package Strings;
 
 import java.util.ArrayList;
@@ -74,8 +117,6 @@ public class removePrimeFromArrayList {
 
 		ArrayList<Integer> a1= takeInput();
 		ArrayList<Integer> result= removePrimes(a1);
-		System.out.println(result);
-		
+		System.out.println(result);	
 	}
-
 }

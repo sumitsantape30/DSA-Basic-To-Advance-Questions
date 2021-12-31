26b025db92904ea9e4277d687909c24e69164753
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-12-31 09:15:49 +0530

    Update C26. Stack To Queue Adapter - Remove Efficient.java

--- Level 1 (Beginner)/A4. Basic Data Structures/A1. Stacks and Queues/C26. Stack To Queue Adapter - Remove Efficient.java
+++ Level 1 (Beginner)/A4. Basic Data Structures/A1. Stacks and Queues/C26. Stack To Queue Adapter - Remove Efficient.java
@@ -57,3 +57,87 @@ Sample Output
 Queue underflow
 Queue underflow
 
+COde:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class StackToQueueAdapter {
+    Stack<Integer> mainS;
+    Stack<Integer> helperS;
+
+    public StackToQueueAdapter() {
+      mainS = new Stack<>();
+      helperS = new Stack<>();
+    }
+
+    //4. main stack ke andar hi aap sara content rakhte hai so uska hi size return karo
+    int size() {
+        return mainS.size();
+    }
+
+   //1.
+    void add(int val) {
+      
+      while( mainS.size() > 0){ //jabtak main stack mai elements hai
+          helperS.push(mainS.pop()) ; // main stack se remove karke helper stack mai dalte rhe
+      }
+      //ab main stack khaliho chuki hai to nayi value ko push krde
+      mainS.push(val);
+      
+      //ab helper stack se wapas apni values lekar aaye
+      while( helperS.size() > 0){
+          //aur ab helper stack se pop hokar main stack mai add hoga
+          mainS.push(helperS.pop());
+      } //jo sabse purani value hogi woh sabse upar hogi so iska fayda aap remove mai kr skte ho
+    }
+    
+    //2.
+    int remove() {
+        if(size()== 0 ){
+            System.out.println("Queue underflow");
+            return -1;
+        }
+      return mainS.pop(); //main stack ke top pe sabse purani value pdi hai to usko pop krdenge
+    }
+    
+    //3. remove jaisahi same kam karenge but yaha pop karneki jarurat nhi hai peek hi karo and return it
+    int peek() {
+        if(size()== 0 ){
+            System.out.println("Queue underflow");
+            return -1;
+        }
+      return mainS.peek(); 
+      
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StackToQueueAdapter qu = new StackToQueueAdapter();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("add")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        qu.add(val);
+      } else if (str.startsWith("remove")) {
+        int val = qu.remove();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("peek")) {
+        int val = qu.peek();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("size")) {
+        System.out.println(qu.size());
+      }
+      str = br.readLine();
+    }
+  }
+}
+

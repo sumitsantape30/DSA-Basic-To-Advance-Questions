a4a59224120e9c96ca8d98c488927b8cf0fbfdde
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-09 07:54:34 +0530

    Create A8. 4 Key Keyboard.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A3. Dynamic-Programming/A8. 4 Key Keyboard.java
@@ -0,0 +1,27 @@
+4 Key Keyboard
+
+1. Imagine you have a special keyboard with the following keys - 
+   Key 1 : Print 'X' on the screen.
+   key 2 : Select screen.
+   Key 3 : Copy selection to buffer.
+   Key 4 : Print buffer on screen appending to what has already been printed.
+2. You are given a number N, which represents the number of times the keyboard is pressed.
+3. You have to find the maximum number of 'X' that can be produced by pressing the N number of keys.  
+
+Constraints
+1 <= N <= 100
+
+Format
+Input
+A number N
+
+Output
+Check the sample output and question video.
+
+Example
+Sample Input
+3
+
+Sample Output
+3
+

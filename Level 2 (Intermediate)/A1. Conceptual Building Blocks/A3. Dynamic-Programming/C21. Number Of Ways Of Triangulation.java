5f970851111784ce551d613ae407019c2f84efd1
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-15 23:19:05 +0530

    Create C21. Number Of Ways Of Triangulation.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A3. Dynamic-Programming/C21. Number Of Ways Of Triangulation.java
@@ -0,0 +1,17 @@
+Number Of Ways Of Triangulation
+
+1. You are given a number N, which represents the number of sides in a polygon.
+2. You have to find the total number of ways in which the given polygon can be triangulated.
+Input Format
+A number N
+Output Format
+A number representing number of ways of traingulating a N-sided polygon.
+
+Constraints
+1 <= N <= 15
+Sample Input
+5
+Sample Output
+5
+
+Code:

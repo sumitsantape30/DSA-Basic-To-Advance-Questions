4840dcb2e02edbb4372347e97cc8537b502ef695
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-17 22:54:34 +0530

    Update C21. Number Of Ways Of Triangulation.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A3. Dynamic-Programming/C21. Number Of Ways Of Triangulation.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A3. Dynamic-Programming/C21. Number Of Ways Of Triangulation.java
@@ -15,3 +15,39 @@ Sample Output
 5
 
 Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    //so n vertices ka polygon hai to usko catalan of n-2 tarikese traingulate kr skte hai
+    //hume n number of sides ke terms mai diya hai but usse koi faraq nhi pdega, pentagon mai 5 side hoti hai aur 5 vertices hoti hai so on
+	public static int solution(int n){
+        n = n - 2; // n ko 2 se isliye ghata diya kyuki traingulation of n is catalan of n-2
+        
+        int dp[] = new int[n+1];
+        dp[0] =1;
+        dp[1] = 1;
+        
+        for( int i=2; i<= n; i++){
+            int l = i-1; //agar aap i ka catalan ni nikal rhe ho to left state kiya i-1 se
+            int r = 0;
+            
+            while( l >= 0){ //jabtak left 0 nhi hojata
+              dp[i] += dp[l]*dp[r];
+              
+              l--;
+              r++;
+            }
+        }
+        return dp[n];
+	 }
+	 
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		System.out.println(solution(n));
+	}
+
+}

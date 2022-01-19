bddfccbd52a9cdea8691bd08e6f039f9c367ff23
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-20 00:04:23 +0530

    Create D24. Minimum Score Of Triangulation.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A3. Dynamic-Programming/D24. Minimum Score Of Triangulation.java
@@ -0,0 +1,26 @@
+Minimum Score Of Triangulation
+
+1. You are given an array of integers, which represents the vertices of an N-sided convex polygon in clockwise order.
+2. You have to triangulate the given polygon into N-2 triangles.
+3. The value of a triangle is the product of the labels of vertices of that triangle.
+4. The total score of the triangulation is the sum of the value of all the triangles.
+5. You have to find the minimum score of the triangulation of the given polygon.
+Input Format
+A number N
+a1
+a2.. N integers
+Output Format
+Check the sample output and question video.
+
+Constraints
+1 <= N <= 1000
+1 <= arr[i] <= 100
+Sample Input
+3
+1
+2
+3
+Sample Output
+6
+
+Code:

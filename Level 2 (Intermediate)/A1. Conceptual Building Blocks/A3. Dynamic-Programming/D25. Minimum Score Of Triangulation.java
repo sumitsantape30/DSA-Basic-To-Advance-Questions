dece51077a8c0cde3a48af8b03c05bad3ed77216
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-20 16:55:29 +0530

    Update D25. Minimum Score Of Triangulation.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A3. Dynamic-Programming/D25. Minimum Score Of Triangulation.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A3. Dynamic-Programming/D25. Minimum Score Of Triangulation.java
@@ -23,4 +23,54 @@ Sample Input
 Sample Output
 6
 
-COde:
+Code:
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static int minScoreTriangulation(int[] arr) {
+    int[][] dp = new int[arr.length][arr.length];
+
+    for ( int g = 0; g < dp.length; g++) { //gap asal mai hota hai ki start aur end mai kitne ki duri hai
+
+      for ( int i = 0, j = g; j < dp[0].length; i++, j++) { //koi bhi diagonal humesha i=0 aur j=g se start krta hai, tabtak chalta hai jo j hai column sabke end mai khatam hote hai. first diagnoal ke upar 0 ka gap hota hai, second diagnoal ke upar 1 ka gap hota hai so on. 4 ka gap hai matlab pehle aur akhri wale mai 4 ka gap hai. sare diagonals apke aakhri column mai khatam hote hai
+
+        //gap 0 or 1 hai to special case hai
+        if ( g == 0) { //gap 0 matlab ek point hai, ek point ka triangle nhi banega
+          dp[i][j] = 0;
+        } else if ( g == 1) { //gap 1 matlab 2 point hai triagle nhi banega
+          dp[i][j] = 0;
+        } else if ( g == 2) { // start aur end mai 2 ka gap hai
+          dp[i][j] = arr[i] * arr[i + 1] * arr[i + 2];
+        } else {
+          int min = Integer.MAX_VALUE;
+            //original array ki bat karun mai
+          for ( int k = i + 1; k <= j - 1; k++) {
+            int tri = arr[i] * arr[j] * arr[k];// immediate triangle jo apne kata hai
+            int left = dp[i][k];//triangle ke left mai kya hai
+            int right = dp[k][j]; //triagle ke right mai kya hai
+
+            int total = tri + left + right;
+            if ( total < min) { //jiski total ki value choti hogi woh jit jayega
+              min = total;
+            }
+          }
+          dp[i][j] = min;
+        }
+      }
+    }
+    return dp[0][dp[0].length-1];
+  }
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0 ; i  < n; i++) {
+      arr[i] = scn.nextInt();
+    }
+    System.out.println(minScoreTriangulation(arr));
+  }
+}

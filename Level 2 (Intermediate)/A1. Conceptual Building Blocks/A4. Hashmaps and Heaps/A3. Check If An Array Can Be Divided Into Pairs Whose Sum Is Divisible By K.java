67da4f3064305207c66d96a7fc9cae3c5036e6bb
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-23 22:29:45 +0530

    Create A3. Check If An Array Can Be Divided Into Pairs Whose Sum Is Divisible By K.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A4. Hashmaps and Heaps/A3. Check If An Array Can Be Divided Into Pairs Whose Sum Is Divisible By K.java
@@ -0,0 +1,25 @@
+Check If An Array Can Be Divided Into Pairs Whose Sum Is Divisible By K
+
+1. You are given an array(arr) of integers and a number K.
+2. You have to find if the given array can be divided into pairs such that the sum of every pair is divisible by k.
+
+Input Format
+A number N
+arr1
+arr2.. N numbers
+A number K
+Output Format
+true or false.
+
+Constraints
+1 <= N <= 10^6
+1 <= arr[i] <= 10^4
+1 <= K <= 10^4
+Sample Input
+4
+9 7 5 3
+6
+Sample Output
+true
+
+Code:

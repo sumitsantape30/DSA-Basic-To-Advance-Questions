6909f60fd1062d3654455a13d24c438cccdc076c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-27 22:03:51 +0530

    Create A8. Smallest Substring Of A String Containing All Characters Of Another String.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A4. Hashmaps and Heaps/A8. Smallest Substring Of A String Containing All Characters Of Another String.java
@@ -0,0 +1,20 @@
+Smallest Substring Of A String Containing All Characters Of Another String
+
+1. You are given two strings s1 and s2 containing lowercase english alphabets.
+2. You have to find the smallest substring of s1 that contains all the characters of s2.
+3. If no such substring exists, print blank string("").
+Input Format
+Two strings s1 and s2
+Output Format
+A string
+
+Constraints
+1 <= length of string s1 and s2 <= 10000
+Sample Input
+timetopractice
+toc
+Sample Output
+toprac
+
+Code:
+

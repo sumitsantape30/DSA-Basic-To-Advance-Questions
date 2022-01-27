c5aec298f42864852da0715884619b202a0f760a
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-27 19:05:17 +0530

    Create A6. Count Of All Subarrays With Zero Sum.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A4. Hashmaps and Heaps/A6. Count Of All Subarrays With Zero Sum.java
@@ -0,0 +1,22 @@
+Count Of All Subarrays With Zero Sum
+
+1. You are given an array(arr) of integers.
+2. You have to find the count of all subarrays with sum 0.
+Input Format
+A number N
+arr1
+arr2.. N numbers
+Output Format
+A number representing the count of all subarrays with sum 0. 
+
+Constraints
+1 <= N <= 10^6
+1 <= arr[i] <= 10^4
+Sample Input
+8
+15 -2 2 -8 1 7 10 23
+Sample Output
+3
+
+Code:
+

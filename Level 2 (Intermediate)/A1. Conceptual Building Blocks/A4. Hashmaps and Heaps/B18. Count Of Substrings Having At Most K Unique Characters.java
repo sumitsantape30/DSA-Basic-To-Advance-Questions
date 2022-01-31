f8977b16cceb52cbafc313425700e097b5a3147c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-31 22:40:50 +0530

    Create B18. Count Of Substrings Having At Most K Unique Characters.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A4. Hashmaps and Heaps/B18. Count Of Substrings Having At Most K Unique Characters.java
@@ -0,0 +1,20 @@
+Count Of Substrings Having At Most K Unique Characters
+
+1. You are given a string(str) and a number K.
+2. You have to find the count of substrings of the given string that contains at most K unique characters.
+Input Format
+A string
+A number
+Output Format
+A number representing the count of substrings with at most K unique characters.
+
+Constraints
+1 <= length of string <= 10^5
+1 <= K <= 26
+  
+Sample Input
+aabcbcdbca
+2
+Sample Output
+23
+

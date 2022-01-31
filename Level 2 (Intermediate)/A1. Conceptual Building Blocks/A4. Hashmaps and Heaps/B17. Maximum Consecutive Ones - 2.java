252205a559a4edbe2eea214ce3480c1b73e1fd69
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-31 18:32:51 +0530

    Create B17. Maximum Consecutive Ones - 2.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A4. Hashmaps and Heaps/B17. Maximum Consecutive Ones - 2.java
@@ -0,0 +1,24 @@
+Maximum Consecutive Ones - 2
+
+1. You are given an array(arr) which contains only 0's and 1's and a number K.
+2. You have to find the maximum number of consecutive 1's in the given array if you can flip at most K zeroes.
+Input Format
+A number N
+arr1
+arr2... N numbers
+A number K
+Output Format
+A number representing maximum number of consecutive 1's after flipping at most K zeroes.
+
+Constraints
+1 <= N <= 10^5
+0 <= arr[i] <= 1
+1 <= K <= N
+Sample Input
+6
+1 1 0 0 1 1
+1
+Sample Output
+3
+
+Code:

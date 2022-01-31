8d41cb20d0453ae314ed9fd170a85f9ccaa9057c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-01-31 21:10:03 +0530

    Update B16. Maximum Consecutive Ones - 2.java

--- Level 2 (Intermediate)/A1. Conceptual Building Blocks/A4. Hashmaps and Heaps/B16. Maximum Consecutive Ones - 2.java
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A4. Hashmaps and Heaps/B16. Maximum Consecutive Ones - 2.java
@@ -22,3 +22,49 @@ Sample Output
 3
 
 Code:
+
+import java.util.*;
+
+public class Main {
+
+    public static int solution(int[] arr, int k){
+        int ans = 0;
+        
+        int j = -1; //release krne keliye j ko shuruwat mai rakha -1 pr
+        int count = 0;
+        
+        //acquire wala loop chala denge ki acquire to karnahi hai
+        for( int i=0; i< arr.length; i++){
+            // i harbar acquire hota ahi
+            if( arr[i] == 0){ //agar i pe 0 agya to count++
+              count++;
+            }
+            
+            while( count > k){ //jaisehi apka count k se bada hojaye. ab valid nhi hai, jasehi number of 0s k se bade hojayenge to aap j badhake release kardenge
+              j++;
+              if( arr[j] == 0){ //0 ko release krdiya hai to count-- kardenge
+                count--;
+              }
+            }
+            //iss loop se bahar aneka ek hi tarika hai ki apka count k ke equal hojaye, to jab yahape aayenge to count k ke equal ho chuka hoga to iss moment pr length nikalenge
+            int len = i - j;
+            if( len > ans){
+                ans = len;
+            }
+        }
+        
+        return ans;
+    }
+    
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i  < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        int k = scn.nextInt();
+        System.out.println(solution(arr,k));
+	}
+
+}

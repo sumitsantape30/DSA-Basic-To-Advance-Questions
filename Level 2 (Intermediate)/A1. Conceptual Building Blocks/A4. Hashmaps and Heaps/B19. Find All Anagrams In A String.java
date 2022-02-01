ac2b945a7b36c4390a57c8681270560656496414
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-02-01 20:25:54 +0530

    Create B19. Find All Anagrams In A String.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A4. Hashmaps and Heaps/B19. Find All Anagrams In A String.java
@@ -0,0 +1,23 @@
+Find All Anagrams In A String
+
+1. You are given two strings s1 and s2.
+2. You have to find the count of s2's anagrams that are present in s1.
+3. Also, you have to print the start indices of such anagrams in s1.
+
+Note -> Both s1 ad s2 consist of lowercase English letters only.
+Input Format
+Two strings s1 and s2
+Output Format
+A number(x) representing count of s2's anagrams which are present in s1.
+X space separated numbers representing start indices of anagrams.
+
+Constraints
+1 <= length of string <= 20,000
+Sample Input
+cbaebabacd
+abc
+Sample Output
+2
+0 6 
+
+Code:

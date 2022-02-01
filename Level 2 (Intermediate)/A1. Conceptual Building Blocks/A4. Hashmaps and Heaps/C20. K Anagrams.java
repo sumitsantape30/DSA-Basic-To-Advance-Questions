19e7307b10f7a97669658e50f9cb4b809492d257
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-02-01 21:55:47 +0530

    Create C20. K Anagrams.java

---
+++ Level 2 (Intermediate)/A1. Conceptual Building Blocks/A4. Hashmaps and Heaps/C20. K Anagrams.java
@@ -0,0 +1,26 @@
+K Anagrams
+
+1. You are given two strings s1, s2, and a number K.
+2. You have to find if two strings are K-anagrams of each other or not.
+3. Two strings are called K-anagrams if 
+   -> Both s1 and s2 have the same number of characters.
+   -> After changing K characters in any string, s1 and s2 become anagram of each other. 
+
+Note -> Both s1 ad s2 consist of lowercase English letters only.
+Input Format
+Two strings s1 and s2
+A number K
+Output Format
+true/false
+
+Constraints
+1 <= length of string s1 and s2 <= 20,000
+1 <= K <= length of strings
+Sample Input
+fodr 
+gork
+2
+Sample Output
+true
+
+Code:

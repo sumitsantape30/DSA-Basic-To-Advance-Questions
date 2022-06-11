f533392e704808145bb7fe927abda699450a5b76
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-06-11 18:40:50 +0530

    Update A7. Is A Number Prime.java

--- Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/A7. Is A Number Prime.java
+++ Level 1 (Beginner)/A1. Basics of Programming/A1. Getting Started/A7. Is A Number Prime.java
@@ -22,6 +22,34 @@ import java.util.Scanner;
 
 public class printPrime {
 	
+	public static boolean isPrime(int n) {
+		if( n == 0 || n == 1) {
+			return false;
+		}
+		
+		for( int i = 2; i*i <= n; i++) {
+			if(n % i == 0) {
+			   return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	//TO print the prime numbers in 1 to 100
+	public static void printPrimeNumbers( int n) {
+		
+		for( int i=1; i<= 100; i++) {
+			
+			if( isPrime(i) == true) {
+				System.out.println(i);
+			}
+			
+		}
+	}
+	
+	//===============================
+	
 	public static void checkPrime( int n) {
 		
 		//1 se leke n tak loop lagayenge and check karenge kitni bar divide hota hai

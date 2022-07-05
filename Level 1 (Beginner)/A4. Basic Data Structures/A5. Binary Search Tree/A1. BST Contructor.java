c1c99dd6d0461f2d8454e7fb62c18799d06b9f02
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-07-05 06:58:29 +0530

    Update A1. BST Contructor.java

--- Level 1 (Beginner)/A4. Basic Data Structures/A5. Binary Search Tree/A1. BST Contructor.java
+++ Level 1 (Beginner)/A4. Basic Data Structures/A5. Binary Search Tree/A1. BST Contructor.java
@@ -1,1 +1,65 @@
+package practice;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Coding {
+	
+	public static class Node{
+		int data;
+		Node left;
+		Node right;
+		
+		public Node(int data, Node left, Node right) {
+			this.data = data;
+			this.left =left;
+			this.right = right;
+		}
+	}
+	
+	public static Node construct(int arr[], int lo, int hi) { //sorted array hai
+		if( lo > hi) {
+			return null;
+		}
+		
+		int mid = (lo + hi)/2;
+		
+		int data = arr[mid];
+		
+		Node lc = construct(arr, lo, mid-1);
+		Node rc = construct(arr, mid+1, hi);
+		//isse left child aur right child banke ajayega
+		
+		Node node = new Node(data, lc, rc);
+		
+		return node;
+		
+	}
+	
+	public static void display(Node root) {
+		if( root == null) {
+			return;
+		}
+		
+		String str = "";
+		str += root.left == null? "." : root.left.data;
+		str +="<--"+root.data+"-->";
+		str += root.right == null ? ".": root.right.data;
+		
+		System.out.println(str);
+		
+		display(root.left);
+		display(root.right);
+	}
+	
+	
+	public static void main(String[] args) {
+
+		int arr[]= {12,25,37,50,62,75,87};
+		Node root = construct(arr, 0, arr.length-1);
+		display(root);
+		
+	}
+
+}
 

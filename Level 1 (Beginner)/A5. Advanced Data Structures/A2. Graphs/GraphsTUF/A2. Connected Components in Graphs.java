f2ee064bf6425a97eca3ef86f486a837bd4dccf8
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-07-20 20:43:33 +0530

    Create A2. Connected Components in Graphs.java

---
+++ Level 1 (Beginner)/A5. Advanced Data Structures/A2. Graphs/GraphsTUF/A2. Connected Components in Graphs.java
@@ -0,0 +1,10 @@
+= Write code for multiple components whether it is DFS or BFS.
+= Take visited array and initialise as 0
+           for(int i = 0; i <= n; i++) { //start i from 1 if the graph has 1 based indexing
+                if (!visited [ i ] ) {
+                         write code for either BFS or DFS
+                }
+           }
+= How to find the number of components?
+  Count the number of time your code goes inside the if statement.
+            

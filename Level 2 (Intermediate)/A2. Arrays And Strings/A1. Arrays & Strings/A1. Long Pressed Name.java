ce382ab3ffa4b70d0119ae094e6d1654f5ca1a99
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-09-04 15:18:07 +0530

    Update and rename A1. Range Addition.java to A1. Long Pressed Name.java

--- Level 2 (Intermediate)/A2. Arrays And Strings/A1. Arrays & Strings/A1. Long Pressed Name.java
+++ Level 2 (Intermediate)/A2. Arrays And Strings/A1. Arrays & Strings/A1. Long Pressed Name.java
@@ -1,5 +1,7 @@
 Range Addition
 
+Link: https://leetcode.com/problems/long-pressed-name/
+
 1. Assume you have an array of length 'n' initialized with all 0's and are given 'q' queries to update.
 2. Each query is represented as a triplet: [startIndex, endIndex, inc] which increments each element of subarray A[startIndex ... endIndex] (startIndex and endIndex inclusive) with inc.
 3. Return the modified array after all 'q' queries were executed.

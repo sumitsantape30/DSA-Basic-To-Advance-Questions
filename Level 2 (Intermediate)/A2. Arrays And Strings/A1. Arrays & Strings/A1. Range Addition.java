9394309614f138ca2a556ee8c859aa7b151d2051
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-09-04 14:33:00 +0530

    Create A1. Range Addition.java

---
+++ Level 2 (Intermediate)/A2. Arrays And Strings/A1. Arrays & Strings/A1. Range Addition.java
@@ -0,0 +1,40 @@
+Range Addition
+
+1. Assume you have an array of length 'n' initialized with all 0's and are given 'q' queries to update.
+2. Each query is represented as a triplet: [startIndex, endIndex, inc] which increments each element of subarray A[startIndex ... endIndex] (startIndex and endIndex inclusive) with inc.
+3. Return the modified array after all 'q' queries were executed.
+Input Format
+length = 5,
+updates = 
+{
+    {1,  3,  2},
+    {2,  4,  3},
+    {0,  2, -2}
+}
+Output Format
+return {-2, 0, 3, 5, 3}
+Explanation : 
+Initial state:
+{ 0, 0, 0, 0, 0 }
+After applying operation [1, 3, 2]:
+{ 0, 2, 2, 2, 0 }
+After applying operation [2, 4, 3]:
+{ 0, 2, 5, 5, 3 }
+After applying operation [0, 2, -2]:
+{-2, 0, 3, 5, 3 }
+
+Constraints
+0 <= n <= 10^5
+0 <= q <= 10^5
+Sample Input
+12
+4
+2 6 4
+0 5 3
+1 4 -2
+6 9 1
+Sample Output
+3 1 5 5 5 7 5 1 1 1 0 0 
+  
+Code:
+

9799d9bb196dcfa6f85a1f0b6a2306868d213df0
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-09-18 08:21:02 +0530

    Update A1. Set Matrix Zero.java

--- SDE Sheet/A1. Arrays/A1. Set Matrix Zero.java
+++ SDE Sheet/A1. Arrays/A1. Set Matrix Zero.java
@@ -1,1 +1,79 @@
+package Codes;
+
+import java.util.*;
+import java.io.*;
+
+public class Codes {
+	
+  //Brute Force
+	static void setZeroes(int[][] matrix) {
+        int rows = matrix.length, cols = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+
+                	//setting entire row, yeh spot agar 0 hai to iss spot ke upper side wale elements ko -1 set krhe
+                    int ind = i - 1;
+                    while (ind >= 0) {
+
+                        if (matrix[ind][j] != 0) {
+                            matrix[ind][j] = -1;
+                        }
+                        ind--;
+                    }
+                    
+                    //same column mai spot ke  niche wale elements ko -1 set krhee
+                    ind = i + 1;
+                    while (ind < rows) {
+                        if (matrix[ind][j] != 0) {
+                            matrix[ind][j] = -1;
+                        }
+                        ind++;
+                    }
+                    
+                    //same row mai spot ke  left side wale elements ko -1 set krhee
+                    ind = j - 1;
+                    while (ind >= 0) {
+                        if (matrix[i][ind] != 0) {
+                            matrix[i][ind] = -1;
+
+                        }
+                        ind--;
+                    }
+                    
+                    //same row ke spot ke right side wale elements ko -1 set krhe
+                    ind = j + 1;
+                    while (ind < cols) {
+                        if (matrix[i][ind] != 0) {
+                            matrix[i][ind] = -1;
+
+                        }
+                        ind++;
+                    }
+                    
+                }
+            }
+        }
+        //fir jaha jaha -1 rakha the waha 0 kr denge
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] <= 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+	public static void main(String[] args) {
+		int arr[][] = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        setZeroes(arr);
+        System.out.println("The Final Matrix is ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
 

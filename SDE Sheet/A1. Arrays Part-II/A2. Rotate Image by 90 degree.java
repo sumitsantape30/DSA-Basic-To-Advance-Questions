5a3ee0640b9e3ea53f10e8b4dfea9928fb76dcee
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-09-24 21:05:30 +0530

    Update and rename A1. Rotate Image by 90 degree.java to A2. Rotate Image by 90 degree.java

--- SDE Sheet/A1. Arrays Part-II/A2. Rotate Image by 90 degree.java
+++ SDE Sheet/A1. Arrays Part-II/A2. Rotate Image by 90 degree.java
@@ -29,3 +29,32 @@ class TUF {
 
 
 //========================Optimal====================================
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        for( int i=0; i< matrix.length; i++){
+            for( int j=0; j<= i; j++){
+                int temp= matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        int left= 0;
+        int right= matrix[0].length-1;
+        
+        while( left < right){
+            
+            for( int row= 0; row < matrix[0].length; row++){
+                
+                int temp= matrix[row][left];
+                matrix[row][left] = matrix[row][right];
+                matrix[row][right] = temp;
+            }
+            
+            left++;
+            right--;
+            
+        }  
+    }
+}

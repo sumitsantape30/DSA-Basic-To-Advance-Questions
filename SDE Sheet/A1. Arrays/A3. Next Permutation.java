ec6cbaaeb4d3fcd6c281921e3d5f8caa5aae09da
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-10-11 20:33:58 +0530

    Update A3. Next Permutation.java

--- SDE Sheet/A1. Arrays/A3. Next Permutation.java
+++ SDE Sheet/A1. Arrays/A3. Next Permutation.java
@@ -32,3 +32,60 @@ public void reverse(int[] A, int i, int j) {
   }
   
 }
+
+//===========================================================
+
+package Arrays;
+
+import java.util.Arrays;
+
+public class setMatrixZeroes {
+
+	public static void nextPermutation( int arr[]) {
+		
+		int i= arr.length-2;
+		while( i >=0 && arr[i] >= arr[i+1]) {
+			i--;
+		}
+		
+		if( i >= 0) {
+			int j= arr.length-1;
+			
+			while(j>=0 && arr[j] <= arr[i]) { //jabtak index1 se badi values hai piche aate rahenge
+				j--;
+			}
+			swap(arr, i, j);
+		}
+		
+		reverse(arr, i + 1, arr.length - 1);
+	}
+	
+	public static void swap( int arr[], int i, int j) {
+		int temp=  arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	
+	public static void reverse( int arr[], int i, int j) {
+		
+		while( i < j) {
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		int arr[]= {1,3,5,4,2};
+		nextPermutation(arr);
+		for( int i=0; i< arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+}
+

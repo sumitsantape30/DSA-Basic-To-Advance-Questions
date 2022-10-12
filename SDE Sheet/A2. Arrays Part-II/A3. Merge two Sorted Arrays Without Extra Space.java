fe2cb352a7357bc3bb6f0aced57b3aad1f85fa3a
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-10-12 23:49:25 +0530

    Update A3. Merge two Sorted Arrays Without Extra Space.java

--- SDE Sheet/A2. Arrays Part-II/A3. Merge two Sorted Arrays Without Extra Space.java
+++ SDE Sheet/A2. Arrays Part-II/A3. Merge two Sorted Arrays Without Extra Space.java
@@ -84,3 +84,159 @@ public class setMatrixZeroes {
 	}
 
 }
+
+//============================================== my code=========================
+
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class setMatrixZeroes {
+
+	// brute force
+	public static void mergeArrays(int arr1[], int arr2[]) {
+
+		int ans[] = new int[arr1.length + arr2.length];
+		int idx = 0;
+
+		for (int i = 0; i < arr1.length; i++) {
+			ans[idx] = arr1[i];
+			idx++;
+		}
+
+		for (int i = 0; i < arr2.length; i++) {
+			ans[idx] = arr2[i];
+			idx++;
+		}
+
+		Arrays.sort(ans);
+		idx = 0;
+
+		for (int i = 0; i < arr1.length; i++) {
+			arr1[i] = ans[idx];
+			idx++;
+		}
+
+		for (int j = 0; j < arr2.length; j++) {
+			arr2[j] = ans[idx];
+			idx++;
+		}
+
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.print(arr1[i] + " ");
+		}
+		System.out.println();
+		for (int j = 0; j < arr2.length; j++) {
+			System.out.print(arr2[j] + " ");
+		}
+
+	}
+
+	// optimised
+	public static void mergeTwoSorted(int arr1[], int arr2[]) {
+
+		for (int i = 0; i < arr1.length; i++) {
+
+			if (arr1[i] > arr2[0]) {
+				int temp = arr1[i];
+				arr1[i] = arr2[0];
+				arr2[0] = temp;
+			}
+			Arrays.sort(arr2); // can use insertion sort here
+		}
+
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.print(arr1[i] + " ");
+		}
+
+		System.out.println();
+
+		for (int j = 0; j < arr2.length; j++) {
+			System.out.print(arr2[j] + " ");
+		}
+
+	}
+
+	// most optimised- gap method
+	public static void mergeTwoSorted2(int arr1[], int arr2[]) {
+		int n = arr1.length;
+		int m = arr2.length;
+		int gap = (int) Math.ceil((double) (n + m) / 2.0);
+
+		while(gap > 0) {
+
+			int i = 0;
+			int j = gap;
+
+			while (j < (m + n)) {
+
+				if (j < n  && arr1[i] > arr1[j]) {
+					swap(arr1[i], arr1[j]);
+				} else if (j >= n && i < n && arr1[i] > arr2[j - n]) {
+					swap(arr1[i], arr2[j - n]);
+				} else if (j >= n && i >= n && arr2[i - n] > arr2[j - n]) {
+					swap(arr2[i - n], arr2[j - n]);
+				}
+				i++;
+				j++;
+			}
+			if (gap == 1) {
+				gap = 0;
+			} else {
+				gap = (int) Math.ceil((double) gap / 2.0);
+			}
+		}
+	}
+
+	public static void swap(int a,int b)
+	    {
+	        int temp=a;
+	        a=b;
+	        b=temp;
+	    }
+	
+	public static void merge(int ar1[], int ar2[], int n, int m) {
+		  // code here 
+		  int gap =(int) Math.ceil((double)(n + m) / 2.0);
+		  while (gap > 0) {
+		    int i = 0;
+		    int j = gap;
+		    while (j < (n + m)) {
+		      if (j < n && ar1[i] > ar1[j]) {
+		        swap(ar1[i], ar1[j]);
+		      } else if (j >= n && i < n && ar1[i] > ar2[j - n]) {
+		        swap(ar1[i], ar2[j - n]);
+		      } else if (j >= n && i >= n && ar2[i - n] > ar2[j - n]) {
+		        swap(ar2[i - n], ar2[j - n]);
+		      }
+		      j++;
+		      i++;
+		    }
+		    if (gap == 1) {
+		      gap = 0;
+		    } else {
+		      gap =(int) Math.ceil((double) gap / 2.0);
+		    }
+		  }
+	}
+
+	public static void main(String[] args) {
+		int arr1[] = { 1, 4, 7, 8, 10 };
+		int arr2[] = { 2, 3, 9 };
+		merge(arr1, arr2, 5, 3);
+		
+		System.out.println("After merge:");
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.print(arr1[i] + " ");
+		}
+		System.out.println();
+		for (int i = 0; i < arr2.length; i++) {
+			System.out.print(arr2[i] + " ");
+		}
+
+	}
+
+}
+

a33ab98762751b0bd51fe289964a67fec0196c1e
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-10-13 22:04:16 +0530

    Create A5. Find the repeating and missing numbers.java

---
+++ SDE Sheet/A2. Arrays Part-II/A5. Find the repeating and missing numbers.java
@@ -0,0 +1,14 @@
+Problem Statement: You are given a read-only array of N integers with values also in the range [1, N] both inclusive. Each integer appears exactly once except A which appears twice and B which is missing. The task is to find the repeating and missing numbers A and B where A repeats twice and B is missing.
+
+Example 1:
+Input Format:  array[] = {3,1,2,5,3}
+Result: {3,4)
+Explanation: A = 3 , B = 4 
+Since 3 is appearing twice and 4 is missing
+  
+Example 2:
+Input Format: array[] = {3,1,2,5,4,6,7,5}
+Result: {5,8)
+Explanation: A = 5 , B = 8 
+Since 5 is appearing twice and 8 is missing
+  

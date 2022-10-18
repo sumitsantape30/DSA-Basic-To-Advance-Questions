23c68be3468730230e90fff49d9ad8349d0942d0
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-10-18 11:07:58 +0530

    Create A5. Grid Unique Paths.java

---
+++ SDE Sheet/A3. Arrays Part-III/A5. Grid Unique Paths.java
@@ -0,0 +1,8 @@
+Problem Statement: Given a matrix m X n, count paths from left-top to the right bottom of a matrix with the constraints that from each cell you can either only move to the rightward direction or the downward direction.
+
+Example 1:
+
+Input Format: m = 2, n= 2
+Output: 2
+Explanation: From the top left corner there are total 2 ways to reach the bottom right corner
+

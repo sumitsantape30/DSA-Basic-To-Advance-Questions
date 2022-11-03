f4b21583489dede4bf76dc2b35cf98d9b688fa21
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-11-03 14:32:06 +0530

    Create A2. Find middle element in a Linked List.java

---
+++ SDE Sheet/A5. LinkedList/A2. Find middle element in a Linked List.java
@@ -0,0 +1,39 @@
+Problem Statement: Given the head of a singly linked list, return the middle node of the linked list. If there are two middle nodes, return the second middle node.
+
+Examples:
+
+Input Format: 
+( Pointer / Access to the head of a Linked list )
+head = [1,2,3,4,5]
+
+Result: [3,4,5]
+( As we will return the middle of Linked list the further linked list will be still available )
+
+Explanation: The middle node of the list is node 3 as in the below image.
+  
+Code:
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while( fast != null && fast.next != null){
+            slow=  slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}

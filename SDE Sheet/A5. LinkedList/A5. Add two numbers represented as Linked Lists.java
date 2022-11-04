d3f2dec95f82d533a6bd00bc0665d0dbb6ab8cfd
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-11-04 07:59:23 +0530

    Update and rename A5. to A5. Add two numbers represented as Linked Lists.java

--- SDE Sheet/A5. LinkedList/A5. Add two numbers represented as Linked Lists.java
+++ SDE Sheet/A5. LinkedList/A5. Add two numbers represented as Linked Lists.java
@@ -1,3 +1,20 @@
+Problem Statement: Given the heads of two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+Examples:
+
+Input Format: 
+(Pointer/Access to the head of the two linked lists)
+
+num1  = 243, num2 = 564
+
+l1 = [2,4,3]
+l2 = [5,6,4]
+
+Result: sum = 807; L = [7,0,8]
+
+Explanation: Since the digits are stored in reverse order, reverse the numbers first to get the or 
+original number and then add them as → 342 + 465 = 807. Refer to the image below.
 
 Code:
 

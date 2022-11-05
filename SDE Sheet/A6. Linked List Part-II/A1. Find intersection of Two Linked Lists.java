14176a5cac7e386151a36849bcd8fb0ec705f92c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-11-05 07:04:45 +0530

    Create A1. Find intersection of Two Linked Lists.java

---
+++ SDE Sheet/A6. Linked List Part-II/A1. Find intersection of Two Linked Lists.java
@@ -0,0 +1,63 @@
+Problem Statement: Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. 
+  If the two linked lists have no intersection at all, return null.
+
+ Example 1:
+Input:
+List 1 = [1,3,1,2,4], List 2 = [3,2,4]
+Output:
+2
+Explanation: Here, both lists intersecting nodes start from node 2.
+  
+Code:
+
+//============================= Brute Force =================================
+
+//utility function to check presence of intersection
+static Node intersectionPresent(Node head1,Node head2) {
+    while(head2 != null) {
+        Node temp = head1;
+        while(temp != null) {
+            //if both nodes are same
+            if(temp == head2) return head2;
+            temp = temp.next;
+        }
+        head2 = head2.next;
+    }
+    //intersection is not present between the lists return null
+    return null;
+}
+
+//============================= better ====================================
+
+//utility function to check presence of intersection
+static Node intersectionPresent(Node head1,Node head2) {
+     HashSet<Node> st=new HashSet<>();
+    while(head1 != null) {
+       st.add(head1);
+       head1 = head1.next;
+    }
+    while(head2 != null) {
+        if(st.contains(head2)) return head2;
+        head2 = head2.next;
+    }
+    return null;
+
+}
+
+//================================= optimal 1 =============================
+
+static Node intersectionPresent(Node head1,Node head2) {
+  if( head1 == null || head2 == null){
+    return null;
+  }
+  //take two pointers and point to the head of both the LL
+    Node d1 = head1;
+    Node d2 = head2;
+    
+    while(d1 != d2) { //i'll keep traversing until they become equal
+        d1 = d1 == null? head2:d1.next; //if d1 became null then it directly moves to the head of the other LL and if d1 is not null it'll move to the next node
+        d2 = d2 == null? head1:d2.next; //if d2 became null then it directly move to the head of opposite LL or if d2 is not null it'll move to the next node
+    }
+    
+    return d1;
+}

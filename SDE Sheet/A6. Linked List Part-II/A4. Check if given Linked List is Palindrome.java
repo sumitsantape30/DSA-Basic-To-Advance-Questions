28443c3c9fb5b4dbd3b666025eb223dc68bf0d61
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-11-05 20:54:13 +0530

    Update A4. Check if given Linked List is Palindrome.java

--- SDE Sheet/A6. Linked List Part-II/A4. Check if given Linked List is Palindrome.java
+++ SDE Sheet/A6. Linked List Part-II/A4. Check if given Linked List is Palindrome.java
@@ -23,6 +23,7 @@ Code:
  * }
  */
 class Solution {
+ 
     public boolean isPalindrome(ListNode head) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         while( head != null){
@@ -43,3 +44,44 @@ class Solution {
 
 //===================================== Optimal Approach ==================================
 
+static Node reverse(Node ptr) {
+    Node pre=null;
+    Node nex=null;
+    while(ptr!=null) {
+        nex = ptr.next;
+        ptr.next = pre;
+        pre=ptr;
+        ptr=nex;
+    }
+    return pre;
+}
+
+static boolean isPalindrome(Node head) {
+    if(head==null||head.next==null) return true; //if LL is null or if its consisting of single element
+        
+    Node slow = head;
+    Node fast = head;
+    
+   //finding the middle of LL
+    while(fast.next!=null&&fast.next.next!=null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+ //then reverse the right half
+    slow.next = reverse(slow.next);
+ 
+ //then move slow to right half
+    slow = slow.next; // slow is standing at the starting position of right half
+ 
+ //then create a dummy node to check the left half and right half is equal or not
+    Node dummy = head;
+        
+    while(slow!=null) {
+        if(dummy.num != slow.num) return false;
+     
+        dummy = dummy.next;
+        slow = slow.next;
+    }
+    return true;
+}

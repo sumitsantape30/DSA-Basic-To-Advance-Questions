c138c33e63d4173749e47f08024504f2e2c710fd
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-11-07 22:24:38 +0530

    Create A5. Find minimum number of coins.java

---
+++ SDE Sheet/A8. Greedy Algorithm/A5. Find minimum number of coins.java
@@ -0,0 +1,17 @@
+Problem Statement: Given a value V, if we want to make a change for V Rs, 
+and we have an infinite supply of each of the denominations in Indian currency, i.e., we have an infinite supply of { 1, 2, 5, 10, 20, 50, 100, 500, 1000} 
+valued coins/notes, what is the minimum number of coins and/or notes needed to make the change.
+  
+Examples:
+Example 1:
+Input: V = 70
+
+Output: 2
+Explaination: We need a 50 Rs note and a 20 Rs note.
+Example 2:
+
+Input: V = 121
+Output: 3
+Explaination: We need a 100 Rs note, a 20 Rs note and a 1 Rs coin.
+  
+Code:

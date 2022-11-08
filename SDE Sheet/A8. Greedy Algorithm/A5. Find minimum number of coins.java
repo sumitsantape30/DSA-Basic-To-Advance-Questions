02b3632e1d9c4c5f193131e068f58f33cbcc54bb
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-11-08 08:33:46 +0530

    Update A5. Find minimum number of coins.java

--- SDE Sheet/A8. Greedy Algorithm/A5. Find minimum number of coins.java
+++ SDE Sheet/A8. Greedy Algorithm/A5. Find minimum number of coins.java
@@ -15,3 +15,28 @@ Output: 3
 Explaination: We need a 100 Rs note, a 20 Rs note and a 1 Rs coin.
   
 Code:
+
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+
+    int V = 49;
+    ArrayList < Integer > ans = new ArrayList < > ();
+    int coins[] = {1, 2, 5, 10, 20, 50, 100, 500, 1000};
+    int n = coins.length;
+    //pichese traverse krte hai
+    for (int i = n - 1; i >= 0; i--) {
+      while (V >= coins[i]) {//pichese aate hue agar woh coin mere V se chote hai tabhi mai woh coin use kr skta hu
+        V -= coins[i];
+        ans.add(coins[i]);
+      }
+    }
+    
+    System.out.println("The minimum number of coins is "+ans.size());
+    System.out.println("The coins are ");
+    for (int i = 0; i < ans.size(); i++) {
+      System.out.print(" " + ans.get(i));
+    }
+
+  }
+}

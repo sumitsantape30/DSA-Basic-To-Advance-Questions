107f8b42c6911a2ffdd53e3e35fd0dc8e40e0723
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-11-08 20:24:50 +0530

    Update A1. Subset Sums.java

--- SDE Sheet/A9. Recursion/A1. Subset Sums.java
+++ SDE Sheet/A9. Recursion/A1. Subset Sums.java
@@ -1,1 +1,64 @@
+Given a list arr of N integers, print sums of all subsets in it.
 
+Example 1:
+
+Input:
+N = 2
+arr[] = {2, 3}
+Output:
+0 2 3 5
+Explanation:
+When no elements is taken then Sum = 0.
+When only 2 is taken then Sum = 2.
+When only 3 is taken then Sum = 3.
+When element 2 and 3 are taken then 
+Sum = 2+3 = 5.
+  
+Code:
+
+import java.util.*;
+class TUF {
+    static void subsetSumsHelper(int ind, int sum, ArrayList < Integer > arr, int N, ArrayList < Integer > sumSubset) {
+        if (ind == N) {
+            sumSubset.add(sum);
+            return;
+        }
+
+        // pick the element 
+        subsetSumsHelper(ind + 1, sum + arr.get(ind), arr, N, sumSubset);
+
+        // Do-not pick the element
+        subsetSumsHelper(ind + 1, sum, arr, N, sumSubset);
+    }
+
+    static ArrayList < Integer > subsetSums(ArrayList < Integer > arr, int N) {
+
+        ArrayList < Integer > sumSubset = new ArrayList < > ();
+        subsetSumsHelper(0, 0, arr, N, sumSubset);
+        Collections.sort(sumSubset);
+        return sumSubset;
+    }
+
+    public static void main(String args[]) {
+
+
+        ArrayList < Integer > arr = new ArrayList < > ();
+        arr.add(3);
+        arr.add(1);
+        arr.add(2);
+        ArrayList < Integer > ans = subsetSums(arr, arr.size());
+        Collections.sort(ans);
+        System.out.println("The sum of each subset is ");
+        for (int i = 0; i < ans.size(); i++)
+            System.out.print(ans.get(i) + " ");
+    }
+
+}
+
+Output:
+
+The sum of each subset is
+0 1 2 3 3 4 5 6
+
+Time Complexity: O(2^n)+O(2^n log(2^n)). Each index has two ways. You can either pick it up or not pick it. So for n index time complexity for O(2^n) and for sorting it will take (2^n log(2^n)).
+Space Complexity: O(2^n) for storing subset sums, since 2^n subsets can be generated for an array of size n.

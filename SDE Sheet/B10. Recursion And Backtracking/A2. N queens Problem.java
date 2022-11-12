87b5d519ecffc30e2d2b8910af9684d637b1a836
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-11-12 18:26:51 +0530

    Create A2. N queens Problem.java

---
+++ SDE Sheet/B10. Recursion And Backtracking/A2. N queens Problem.java
@@ -0,0 +1,185 @@
+Problem Statement: The n-queens is the problem of placing n queens on n × n chessboard such that no two queens can attack each other. Given an integer n, return all distinct solutions to the n -queens puzzle. Each solution contains a distinct boards configuration of the queen’s placement, where ‘Q’ and ‘.’ indicate queen and empty space respectively.
+
+Examples:
+
+Input: n = 4
+
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+
+Code:
+
+//======================================== Approach 1 =================================
+
+import java.util.*;
+class TUF {
+    public static List < List < String >> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List < List < String >> res = new ArrayList < List < String >> ();
+        dfs(0, board, res);
+        return res;
+    }
+
+    static boolean validate(char[][] board, int row, int col) {
+        int duprow = row;
+        int dupcol = col;
+        while (row >= 0 && col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+        while (col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+        while (col >= 0 && row < board.length) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+            row++;
+        }
+        return true;
+    }
+
+    static void dfs(int col, char[][] board, List < List < String >> res) {
+        if (col == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (validate(board, row, col)) {
+                board[row][col] = 'Q';
+                dfs(col + 1, board, res);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    static List < String > construct(char[][] board) {
+        List < String > res = new LinkedList < String > ();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+    public static void main(String args[]) {
+        int N = 4;
+        List < List < String >> queen = solveNQueens(N);
+        int i = 1;
+        for (List < String > it: queen) {
+            System.out.println("Arrangement " + i);
+            for (String s: it) {
+                System.out.println(s);
+            }
+            System.out.println();
+            i += 1;
+        }
+        
+    }
+}
+Output:
+
+Arrangement 1
+..Q.
+Q…
+…Q
+.Q..
+
+Arrangement 2
+.Q..
+…Q
+Q…
+..Q.
+
+Time Complexity: Exponential in nature since we are trying out all ways, to be precise its O(N! * N).
+
+Space Complexity: O( N2 )
+  
+//================================================== Approach 2 =======================================================
+  
+import java.util.*;
+class TUF {
+    public static List < List < String >> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List < List < String >> res = new ArrayList < List < String >> ();
+        int leftRow[] = new int[n];
+        int upperDiagonal[] = new int[2 * n - 1];
+        int lowerDiagonal[] = new int[2 * n - 1];
+        solve(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
+        return res;
+    }
+
+    static void solve(int col, char[][] board, List < List < String >> res, int leftRow[], int lowerDiagonal[], int upperDiagonal[]) {
+        if (col == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[board.length - 1 + col - row] == 0) {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[board.length - 1 + col - row] = 1;
+                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDiagonal);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[board.length - 1 + col - row] = 0;
+            }
+        }
+    }
+
+    static List < String > construct(char[][] board) {
+        List < String > res = new LinkedList < String > ();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+    public static void main(String args[]) {
+        int N = 4;
+        List < List < String >> queen = solveNQueens(N);
+        int i = 1;
+        for (List < String > it: queen) {
+            System.out.println("Arrangement " + i);
+            for (String s: it) {
+                System.out.println(s);
+            }
+            System.out.println();
+            i += 1;
+        }
+
+    }
+}
+Output:
+
+Arrangement 1
+..Q.
+Q…
+…Q
+.Q..
+
+Arrangement 2
+.Q..
+…Q
+Q…
+..Q.
+
+Time Complexity: Exponential in nature since we are trying out all ways, to be precise its O(N! * N).
+
+Space Complexity: O(N)
+ 

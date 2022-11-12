fc07474eb05620a50762ca0be0a3762a7191e38d
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2022-11-12 14:17:20 +0530

    Update A1. Print all permutations of a string or array.java

--- SDE Sheet/A10. Recursion And Backtracking/A1. Print all permutations of a string or array.java
+++ SDE Sheet/A10. Recursion And Backtracking/A1. Print all permutations of a string or array.java
@@ -67,3 +67,65 @@ All Permutations are
 Time Complexity:  N! x N
 
 Space Complexity:  O(N)
+
+//============================================================== Approach 2 - Backtracking =============================================
+  
+  import java.io.*;
+import java.util.*;
+class Solution {
+    private void recurPermute(int index, int[] nums, List < List < Integer >> ans) {
+        if (index == nums.length) { //whenever my index reaches at nums.length then means uss array mai sari swapping vagere hogyi hai aur ek permutation generate hogya hai so iske sare elements ko ds mai dalenge aur fir main answer mai
+            // copy the ds to ans
+            List < Integer > ds = new ArrayList < > ();
+            for (int i = 0; i < nums.length; i++) {
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList < > (ds));
+            return;
+        }
+      
+        for (int i = index; i < nums.length; i++) {
+            swap(i, index, nums);//swapping i with index
+            recurPermute(index + 1, nums, ans);
+            swap(i, index, nums);//reswap it, backtrack it
+        }
+    }
+    private void swap(int i, int j, int[] nums) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+    public List < List < Integer >> permute(int[] nums) {
+        List < List < Integer >> ans = new ArrayList < > ();
+        recurPermute(0, nums, ans);//calling the recursion with 0th index, passing array and ans that'll store all the permutations
+        return ans;
+    }
+};
+
+class TUF {
+    public static void main(String[] args) {
+        int nums[] = {1,2,3};
+        Solution sol = new Solution();
+        List < List < Integer >> ls = sol.permute(nums);
+        System.out.println("All Permutations are");
+        for (int i = 0; i < ls.size(); i++) {
+            for (int j = 0; j < ls.get(i).size(); j++) {
+                System.out.print(ls.get(i).get(j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+Output:
+
+All Permutations are
+1 2 3
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1
+
+Time Complexity:  N! x N
+
+Space Complexity:  O(N)

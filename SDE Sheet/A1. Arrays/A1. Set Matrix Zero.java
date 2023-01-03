3b8c60f0d8eda497c4141f05a77ebb429ca16978
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-01-03 19:23:07 +0530

    Update A1. Set Matrix Zero.java

--- SDE Sheet/A1. Arrays/A1. Set Matrix Zero.java
+++ SDE Sheet/A1. Arrays/A1. Set Matrix Zero.java
@@ -89,6 +89,26 @@ public class Codes {
 		    }
 		  }
 	}
+	
+	//optimised approach
+	public static void setZeroes(int[][] matrix) {
+        int col0 = 1, rows = matrix.length, cols = matrix[0].length; //set col0 to 1 or true
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) col0 = 0; //if value at 0th column of any given row i is 0 then marks col0 as false
+		
+            for (int j = 1; j < cols; j++) //then we traverse from first column coz operation for 0th column is done then we check if matrix[i][j] is 0 then i assign leftmost and topmost value as 0
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }//onece i completely travel 2d matrix i linearly travel backwards
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) //i check for any index i,j if the leftmost is 0 or topmost is 0 then I assign 0 to i,j
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0; //if col0 is false that means that means the 0th column is actually set to 0s so I simply set it to 0s
+        }
+    }
 
 	public static void main(String[] args) {
 		int arr[][] = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};

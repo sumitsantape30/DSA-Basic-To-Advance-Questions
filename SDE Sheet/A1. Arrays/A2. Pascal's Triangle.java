2cfd8aae26e8d978999fb906fdb9957edd45f8dc
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-01-03 20:55:50 +0530

    Update A2. Pascal's Triangle.java

--- SDE Sheet/A1. Arrays/A2. Pascal's Triangle.java
+++ SDE Sheet/A1. Arrays/A2. Pascal's Triangle.java
@@ -43,21 +43,23 @@ class Solution {
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-		List<Integer> row, pre = null; 
-
-		 for (int i = 0; i < numRows; ++i) {
-             
-			row = new ArrayList<Integer>();
-			for (int j = 0; j <= i; ++j){
-				if (j == 0 || j == i){
-					row.add(1);
-                }else{
-					row.add(pre.get(j - 1) + pre.get(j));
-                }
-            }
-            res.add(row); 
-			pre = row; 
+       List<List<Integer>> res=  new ArrayList<List<Integer>>();
+		List<Integer> curr, prev = null;
+		
+		for( int i=0; i< numRows; i++) {
+			
+			curr = new ArrayList<Integer>();
+			
+			for( int j=0; j<= i; j++) {
+				
+				if( j == 0 || j == i) {
+					curr.add(1);
+				}else {
+					curr.add(prev.get(j-1) + prev.get(j));
+				}
+			}
+			res.add(curr);
+			prev = curr;
 		}
 		return res;
     }

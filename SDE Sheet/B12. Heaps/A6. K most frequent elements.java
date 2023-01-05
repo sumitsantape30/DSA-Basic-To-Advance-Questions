cc2f021f8c7983b4f2fd48900e03d901883d43e2
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-01-05 20:41:08 +0530

    Update A6. K most frequent elements.java

--- SDE Sheet/B12. Heaps/A6. K most frequent elements.java
+++ SDE Sheet/B12. Heaps/A6. K most frequent elements.java
@@ -11,3 +11,47 @@ Output: [1]
 
 Code:
 
+//======================================= Optimised - o(logk)===================
+class Solution {
+
+    public static class Pair implements Comparable<Pair>{
+        int data;
+        int count;
+
+        public Pair(int data, int count){
+            this.data = data;
+            this.count =  count;
+        }
+
+        public int compareTo(Pair o){
+            return this.count- o.count;
+        }
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        
+        HashMap<Integer, Integer> map= new HashMap<>();
+        //creating map takes o(n)
+        for(int i=0; i< nums.length; i++)map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+
+        PriorityQueue<Pair> pq= new PriorityQueue<>(Collections.reverseOrder());
+
+        for( int i=0; i< nums.length; i++){
+            if( map.containsKey(nums[i])){
+                pq.add(new Pair(nums[i], map.get(nums[i])));
+                map.remove(nums[i]);
+            }
+        }
+
+        int ans[]= new int[k];
+        int idx=0 ;
+        
+      //removing k elements from PQ takes O(logk)
+        while( k > 0){
+            ans[idx]= pq.remove().data;
+            idx++;
+            k--;
+        }
+        return ans;
+    }
+}

e3969af4874c1e9fa235f8f131458fd983b9c08f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-01-09 20:33:30 +0530

    Create A3. Implement Stack using Queue (using single queue).java

---
+++ SDE Sheet/B13. Stack and Queue/A3. Implement Stack using Queue (using single queue).java
@@ -0,0 +1,45 @@
+Implement Stack using single Queue
+Problem Statement: Implement a Stack using a single Queue.
+
+Note: Stack is a data structure that follows the Last In First Out (LIFO) rule.
+
+Note: Queue is a data structure that follows the First In First Out (FIFO) rule.
+
+Code:
+
+import java.util.*;
+
+public class tuf {
+
+    public static void main(String[] args) {
+        stack s = new stack();
+        s.push(3);
+        s.push(2);
+        s.push(4);
+        s.push(1);
+        System.out.println("Top of the stack: " + s.top());
+        System.out.println("Size of the stack before removing element: " + s.size());
+        System.out.println("The deleted element is: " + s.pop());
+        System.out.println("Top of the stack after removing element: " + s.top());
+        System.out.println("Size of the stack after removing element: " + s.size());
+    }
+
+}
+class stack {
+    Queue < Integer > q = new LinkedList < > ();
+    void push(int x) {
+        q.add(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.add(q.remove());
+        }
+    }
+    int pop() {
+        return q.remove();
+    }
+    int top() {
+        return q.peek();
+    }
+    int size() {
+        return q.size();
+    }
+}

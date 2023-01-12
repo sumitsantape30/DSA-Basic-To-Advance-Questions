042acf828b71943ce2533b0d72423b762a6ad171
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-01-12 19:29:05 +0530

    Create A4. Implement ATOI-STRSTR.java

---
+++ SDE Sheet/B15. String/A4. Implement ATOI-STRSTR.java
@@ -0,0 +1,93 @@
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+The algorithm for myAtoi(string s) is as follows:
+
+Read in and ignore any leading whitespace.
+Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+Return the integer as the final result.
+Note:
+
+Only the space character ' ' is considered a whitespace character.
+Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.
+ 
+Example 1:
+
+Input: s = "42"
+Output: 42
+Explanation: The underlined characters are what is read in, the caret is the current reader position.
+Step 1: "42" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "42" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "42" ("42" is read in)
+           ^
+The parsed integer is 42.
+Since 42 is in the range [-231, 231 - 1], the final result is 42.
+Example 2:
+
+Input: s = "   -42"
+Output: -42
+Explanation:
+Step 1: "   -42" (leading whitespace is read and ignored)
+            ^
+Step 2: "   -42" ('-' is read, so the result should be negative)
+             ^
+Step 3: "   -42" ("42" is read in)
+               ^
+The parsed integer is -42.
+Since -42 is in the range [-231, 231 - 1], the final result is -42.
+Example 3:
+
+Input: s = "4193 with words"
+Output: 4193
+Explanation:
+Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
+             ^
+The parsed integer is 4193.
+Since 4193 is in the range [-231, 231 - 1], the final result is 4193.
+ 
+Code:
+      
+ class Solution {
+    public int myAtoi(String s) {
+        int sign = 1;//input string mai sign konsa hai dekhenge fir end mai answer ko uss sign se multiple krdenge
+		int n = s.length();
+		int result = 0;
+		
+		//pehle starting mai jo space honge unko ignore krenge
+		int i=0;
+		while( i < n && s.charAt(i) == ' ')i++;
+		
+		//starting spaces ke bad ayega sign it can be - or +
+		if(i < n && s.charAt(i) == '+') {
+			sign = 1;
+			i++;
+		}else if( i < n && s.charAt(i) == '-') {
+			sign = -1;
+			i++;
+		}
+		
+		//ab i jis index pe hoga wahase actual number start hoga to dekhenge kya woh range mai hai
+		while(i < n && (0 <= s.charAt(i) - '0') && (s.charAt(i) - '0' <= 9)) {
+			int digit = s.charAt(i)- '0';
+			//ab overflow check karenge
+			if(result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE/10 && digit > Integer.MAX_VALUE % 10)) {
+				//yeh conditions true hui means overflow
+				return (sign == 1)? Integer.MAX_VALUE: Integer.MIN_VALUE; //agar positive number hai to max number return karo. negative number hai to min value return krdo
+			}
+			
+			//aur agar yeh overflow nhi hota hai to digit ko result mai daldo
+			result = result*10 + digit;
+			i++;
+		}
+		
+		return result*sign;
+    }
+}                                                                                            

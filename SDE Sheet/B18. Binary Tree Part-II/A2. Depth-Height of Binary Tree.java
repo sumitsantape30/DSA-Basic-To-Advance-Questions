311d038f00e5127600cf9e2249eeb54957323c10
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-01-20 16:37:15 +0530

    Create A2. Depth-Height of Binary Tree.java

---
+++ SDE Sheet/B18. Binary Tree Part-II/A2. Depth-Height of Binary Tree.java
@@ -0,0 +1,47 @@
+Problem Statement: Find the Maximum Depth of Binary Tree. Maximum Depth is the count of nodes of the longest path from the root node to the leaf node.
+
+Code:
+//======================== Levelorder traversal =========================================
+public class tUf {
+    private static int levelOrder( TreeNode root ){
+        if( root == null ){
+            return 0;
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.addLast(root);
+
+        int level = 0;
+
+        while( queue.size() > 0 ){
+            int size = queue.size();
+
+            while( size-- > 0 ){
+                TreeNode remNode = queue.removeFirst();
+                if( remNode.left != null ){
+                    queue.addLast( remNode.left );
+                }
+                if( remNode.right != null ){
+                        queue.addLast( remNode.right );
+                }
+            }
+
+            level++;
+        }
+
+        return level;
+    }  
+}
+
+//==================================== Recursive ================================
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0; 
+        
+        int lh = maxDepth(root.left); 
+        int rh = maxDepth(root.right); 
+        
+        return 1 + Math.max(lh, rh); 
+    }
+}

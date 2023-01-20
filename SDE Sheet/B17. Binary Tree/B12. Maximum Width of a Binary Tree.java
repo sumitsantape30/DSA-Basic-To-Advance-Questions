bbe9cb69e9a71114ee9a46d9dd65e41ba69051dd
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-01-20 16:19:42 +0530

    Update B12. Maximum Width of a Binary Tree.java

--- SDE Sheet/B17. Binary Tree/B12. Maximum Width of a Binary Tree.java
+++ SDE Sheet/B17. Binary Tree/B12. Maximum Width of a Binary Tree.java
@@ -35,25 +35,33 @@ class Pair {
         node = _node; 
     }
 }
+
 class Solution {
     public static int widthOfBinaryTree(TreeNode root) {
-        if(root == null) return 0;
+        if(root == null) return 0; //if root is null there'll be no width so returning 0
+      
         int ans = 0;
-        Queue<Pair> q = new LinkedList<>(); 
-        q.offer(new Pair(root, 0)); 
+        Queue<Pair> q = new LinkedList<>();  //we'll have queue of Pair
+      
+        q.offer(new Pair(root, 0)); //we're adding node and index
+      
         while(!q.isEmpty()){
-            int size = q.size();
-            int mmin = q.peek().num;    //to make the id starting from zero
+          
+            int size = q.size();//now currently whatever maybe the size of queue we'll iterate on all the elements
+            int mmin = q.peek().num; //whichever is the first guy i took that index so i have minimum index possible on that level  
             int first = 0,last = 0;
             for(int i=0; i<size; i++){
-                int cur_id = q.peek().num-mmin;
+                int cur_id = q.peek().num- mmin; //the current index will always be substracted by min
                 TreeNode node = q.peek().node;
                 q.poll();
+                
+              //store the first guy and the last guy
                 if(i==0) first = cur_id;
                 if(i==size-1) last = cur_id;
-                if(node.left != null)
-                    q.offer(new Pair(node.left, cur_id*2+1));
-                if(node.right != null) 
+              
+                if(node.left != null) //if there's left you take the left node
+                    q.offer(new Pair(node.left, cur_id*2+1)); //after substraction index*2 + 1
+                if(node.right != null) //if right exists
                     q.offer(new Pair(node.right, cur_id*2+2));
             }
             ans = Math.max(ans, last-first+1);

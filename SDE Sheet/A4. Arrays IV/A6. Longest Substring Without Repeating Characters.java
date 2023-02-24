1d04e58bafe68a630f603ae98895b281ce7330a7
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-02-24 09:35:34 +0530

    Update A6. Longest Substring Without Repeating Characters.java

--- SDE Sheet/A4. Arrays IV/A6. Longest Substring Without Repeating Characters.java
+++ SDE Sheet/A4. Arrays IV/A6. Longest Substring Without Repeating Characters.java
@@ -21,17 +21,20 @@ Code:
 
 import java.util.*;
 public class Main {
+ 
+ 
     static int solve(String str) {
+     //check karenge kyawoh character present hai set mai agar already present hai means yeh current character repeated hai aur jo characters set mai hai woh unique hai to unki length count karo aur loop break karo
 
         if(str.length()==0)
              return 0;
         int maxans = Integer.MIN_VALUE;
         for (int i = 0; i < str.length(); i++) // outer loop for traversing the string
         {
-            Set < Character > se = new HashSet < > ();
+            Set < Character > se = new HashSet < > ();//hum yahape set use krhe to store unique substring
             for (int j = i; j < str.length(); j++) // nested loop for getting different string starting with str[i]
             {
-                if (se.contains(str.charAt(j))) // if element if found so mark it as ans and break from the loop
+                if (se.contains(str.charAt(j))) // if element if found so mark it as ans and break from the loop. agar j already present hai means set mai ek substring hai to uski length count krke break karo
                 {
                     maxans = Math.max(maxans, j - i);
                     break;
@@ -61,7 +64,8 @@ public class Main {
         Set < Character > set = new HashSet < > ();
         int l = 0;
         for (int r = 0; r < str.length(); r++) // outer loop for traversing the string
-        {
+        { 
+         //agar yeh character present nhi hoga to dal denge aur length update krdenge
             if (set.contains(str.charAt(r))) //if duplicate element is found. duplicate element hai iss range mai to l ko aage badhao, then jo new range hogi usme repeating character nhi hoga
             {
                 while (l < r && set.contains(str.charAt(r))) {  //jabtak yeh r present hai set mai means duplicate hai to l ko remove krte rahenge aur l ko aage badhate rahenge
@@ -97,8 +101,9 @@ public class Main {
         while (right < n) {
             if (mpp.containsKey(s.charAt(right))) // we check if our map contains the character at index right, if it contains I'm going to update my left if that's in the range [l-r] 
                left = Math.max(mpp.get(s.charAt(right)) + 1, left);//so I am doing comparison of left and whatever character I have seen at last + 1
+               //right mai yahi character dikh gya hai to left ko update krna hai so last time jaha right dikha tha uske +1 pe shift karenge
 
-            mpp.put(s.charAt(right), right);// here I update the last seen
+            mpp.put(s.charAt(right), right);// here I update the last seen. //map mai hum dal rhe hai yeh character aur uska index
 
             len = Math.max(len, right - left + 1);// then I compute the length
             right++;// then I'll move right pointer by one

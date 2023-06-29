6569f21a1088a1cd0fceecf01654de5044b4c8e5
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-06-29 15:56:07 +0530

    Create A8. Topological Sort Using DFS.java

---
+++ SDE Sheet/C23. Graph/A8. Topological Sort Using DFS.java
@@ -0,0 +1,79 @@
+
+//Summary
+
+
+
+Code:
+
+package Codes;
+
+import java.util.*;
+import java.io.*;
+
+public class ques {
+
+	public static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+
+		Stack<Integer> st = new Stack<>();
+		int visited[] = new int[V];
+
+		for (int i = 0; i < V; i++) {
+
+			if (visited[i] == 0) {
+				findTopoSort(i, adj, visited, st);
+			}
+		}
+
+		int topo[] = new int[V];
+		int ind = 0;
+		while (!st.isEmpty()) {
+			topo[ind++] = st.pop();
+		}
+		// for(int i = 0;i<N;i++) System.out.println(topo[i] + " ");
+		return topo;
+
+	}
+
+	public static void findTopoSort(int node, ArrayList<ArrayList<Integer>> adj, int visited[], Stack<Integer> st) {
+
+		visited[node] = 1;
+
+		for (Integer adjacent : adj.get(node)) {
+
+			if (visited[adjacent] == 0) {
+				findTopoSort(adjacent, adj, visited, st);
+			}
+
+		}
+
+		st.push(node);
+	}
+
+	public static void main(String[] args) {
+
+		 ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+	        int n=6;
+	        for(int i=0;i<n;i++)
+	        {
+	            ArrayList<Integer> arr=new ArrayList<>();
+	            adj.add(arr);
+	        }
+	        
+	        adj.get(5).add(2);
+	        adj.get(5).add(0);
+	        adj.get(4).add(0);
+	        adj.get(4).add(1);
+	        adj.get(2).add(3);
+	        adj.get(3).add(1);
+	        
+	        int res[] = topoSort(6, adj);
+	   
+	        System.out.println("Toposort of the given graph is:" );
+	        for (int i = 0; i < res.length; i++) {
+	        	
+	        System.out.print(res[i]+" ");
+	        
+	        }
+	}
+
+}

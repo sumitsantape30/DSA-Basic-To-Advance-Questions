a4ea46df4c05c1e660b4b8e714952e92f2fd9791
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-08-01 07:16:42 +0530

    Update A1. Set Matrix Zero.java

--- SDE Sheet/A1. Arrays/A1. Set Matrix Zero.java
+++ SDE Sheet/A1. Arrays/A1. Set Matrix Zero.java
@@ -1,3 +1,5 @@
+Problem Statement: Given a matrix if an element in the matrix is 0 then you will have to set its entire column and row to 0 and then return the matrix.
+
 package Codes;
 
 import java.util.*;

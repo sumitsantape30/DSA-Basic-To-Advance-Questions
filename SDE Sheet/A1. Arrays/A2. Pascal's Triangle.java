a1b2394ec1b3fe3f6a15a9bd139dbc371d7c7671
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-08-01 13:16:34 +0530

    Update A2. Pascal's Triangle.java

--- SDE Sheet/A1. Arrays/A2. Pascal's Triangle.java
+++ SDE Sheet/A1. Arrays/A2. Pascal's Triangle.java
@@ -64,3 +64,38 @@ class Solution {
 		return res;
     }
 }
+
+
+Variation 1: In this case, we are given the row number r and the column number c, and we need to find out the element at position (r,c). 
+
+In this case, we are given the row number r and the column number c, and we need to find out the element at position (r,c). 
+
+Code:
+
+import java.util.*;
+
+public class tUf {
+    public static long nCr(int n, int r) {
+        long res = 1;
+
+        // calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+
+    public static int pascalTriangle(int r, int c) {
+        int element = (int) nCr(r - 1, c - 1);
+        return element;
+    }
+
+    public static void main(String[] args) {
+        int r = 5; // row number
+        int c = 3; // col number
+        int element = pascalTriangle(r, c);
+        System.out.println("The element at position (r,c) is: " + element);
+    }
+}  
+        

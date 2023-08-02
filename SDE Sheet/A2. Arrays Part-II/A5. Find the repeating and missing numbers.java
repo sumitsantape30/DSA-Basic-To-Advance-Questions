2d90e550025a3690bcf5f9b502c841731ba62f34
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-08-02 16:53:25 +0530

    Update A5. Find the repeating and missing numbers.java

--- SDE Sheet/A2. Arrays Part-II/A5. Find the repeating and missing numbers.java
+++ SDE Sheet/A2. Arrays Part-II/A5. Find the repeating and missing numbers.java
@@ -12,3 +12,63 @@ Result: {5,8)
 Explanation: A = 5 , B = 8 
 Since 5 is appearing twice and 8 is missing
   
+Code:
+
+//================================= Brute Force========================================
+
+package Codes;
+
+import java.util.*;
+import java.io.*;
+
+public class ques {
+
+	public static int[] findMissingRepeatingNumbers( int arr[]) {
+		
+		int freq[]= new int[arr.length+1];
+		
+		for( int i=0; i< arr.length; i++) {
+			freq[arr[i]]++;
+		}
+		
+		for( int i=0; i< freq.length; i++) {
+			System.out.println(freq[i]);
+		}
+		
+		int repeating = -1;
+		int missing= -1;
+		
+		for( int i=1; i< freq.length; i++) {
+			
+			if( freq[i] == 2) {
+				repeating = i;
+			}
+			
+		}
+		
+		for( int i=1; i< freq.length; i++) {
+			
+			if( freq[i] == 0) {
+				missing= i;
+			}
+		}
+		
+		int ans[]= new int[2];
+		ans[0]= repeating;
+		ans[1]= missing;
+		
+		return ans;
+	}
+	
+	public static void main(String[] args) {
+		
+		 int[] a = {3, 1, 2, 5, 4, 6, 7, 5};
+	        int[] ans = findMissingRepeatingNumbers(a);
+	        System.out.println("The repeating and missing numbers are: {"
+	                           + ans[0] + ", " + ans[1] + "}");
+
+	}
+}
+
+//========================================================Optimised ========================
+

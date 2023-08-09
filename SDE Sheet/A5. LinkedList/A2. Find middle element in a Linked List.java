fbaf5ed8e36013bd05febf89ded671247edf1b26
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2023-08-09 09:38:20 +0530

    Update A2. Find middle element in a Linked List.java

--- SDE Sheet/A5. LinkedList/A2. Find middle element in a Linked List.java
+++ SDE Sheet/A5. LinkedList/A2. Find middle element in a Linked List.java
@@ -10,7 +10,10 @@ Result: [3,4,5]
 ( As we will return the middle of Linked list the further linked list will be still available )
 
 Explanation: The middle node of the list is node 3 as in the below image.
-  
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
 Code:
 
 /**
